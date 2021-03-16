8e02d47f7fc566e35f694b3caf4cfe3d588076d8
user <user@Dmitry>
2021-03-16 15:45:19 +0300

    Исправления.

---
+++ erp/roll/pdfprint.php
@@ -0,0 +1,25 @@
+<?php
+include '../include/topscripts.php';
+
+// Если не задано значение id, перенаправляем на список
+$id = filter_input(INPUT_GET, 'id');
+if(empty($id)) {
+    header('Location: '.APPLICATION.'/roll/');
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body>
+        <div style="margin-left: 30px;">
+            <div style="margin-bottom: 20px; margin-top: 30px;">
+                <a href="<?=APPLICATION ?>/roll/roll.php?id=<?=$id ?>"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+            </div>
+            <embed src="pdf.php?id=<?=$id ?>" type="application/pdf" width="100%" height="500px">
+        </div>
+    </body>
+</html>
