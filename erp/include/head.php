21b02e8880787c6f0e0c4b2dfd30fc679c5d81a1
user <user@Dmitry>
2021-03-22 13:32:48 +0300

    Стикер.

--- erp/include/head.php
+++ erp/include/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <title>Принт-дизайн. Управление ресурсами предприятия</title>
 <link href="<?=APPLICATION ?>/css/bootstrap.min.css" rel="stylesheet">
-<link href="<?=APPLICATION ?>/fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
+<link href="<?=APPLICATION ?>/../fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/css/main.css?version=20210312" rel="stylesheet">
 <link rel="icon" type="image/png" href="<?=APPLICATION ?>/favicon.ico" />
