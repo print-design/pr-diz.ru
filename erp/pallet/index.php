95ab9723497fba18d10ee4cf9490d2f0384dc5e3
user <user@Dmitry>
2021-03-23 15:31:56 +0300

    Фильтр.

--- erp/pallet/index.php
+++ erp/pallet/index.php
@@ -64,10 +64,16 @@ $total_weight = $row['total_weight'];
                 </div>
                 <div class="p-1">
                     <?php if(IsInRole(array('technologist', 'dev', 'storekeeper'))): ?>
-                    <a href="new.php" class="btn btn-outline-dark" style="padding-top: 14px; padding-bottom: 14px; padding-left: 30px; width: 200px; text-align: left;"><i class="fas fa-plus" style="font-size: 10px; margin-right: 18px;"></i>Новый паллет</a>
+                    <a href="new.php" class="btn btn-outline-dark" style="padding-top: 14px; padding-bottom: 14px; padding-left: 30px; width: 222px; text-align: left; font-size: 14px;"><i class="fas fa-plus" style="font-size: 10px; margin-right: 18px;"></i>Новый паллет</a>
                     <?php endif; ?>
-                    <button class="btn btn-outline-dark disabled" data-toggle="modal" data-target="#filterModal" data-text="Фильтр"><img src="../images/icons/filter.svg" style="margin-right: 20px;" />Фильтр</button>
-                    <div style="display: inline; position: relative;"><img src="../images/icons/filter.svg" style="margin-right: 20px;" /></div>
+                    <button class="btn btn-outline-dark disabled d-none" data-toggle="modal" data-target="#filterModal" data-text="Фильтр"><img src="../images/icons/filter.svg" style="margin-right: 20px;" />Фильтр</button>
+                    <div style="display: inline-block; position: relative; margin-right: 55px; margin-left: 80px;">
+                        <a href="javascript: void(0);"><img src="../images/icons/filter1.svg" /></a>
+                        <div id="filter_params_counter_round" style="position: absolute; top: -7px; right: 0;">
+                            <img src="../images/icons/filter_params_counter.svg" />
+                            <div id="filter_params_counter" style="position: absolute; top: 1px; left: 8px; color: white;">0</div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <table class="table" id="content_table">
