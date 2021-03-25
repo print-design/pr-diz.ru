bf7e92ff845f60fa6be40fb372593415c8e94e8b
user <user@Dmitry>
2021-03-25 09:57:34 +0300

    Фильтр.

--- erp/pallet/index.php
+++ erp/pallet/index.php
@@ -59,7 +59,7 @@ while ($row = $fetcher->Fetch()) {
                         <tr>
                             <td><h1 style="font-size: 32px; line-height: 48px; font-weight: 600;">Паллеты</h1></td>
                             <td style="padding-left: 20px; padding-right: 20px; font-weight: bold;">(<?= number_format($total_weight, 0, ',', ' ') ?> кг)</td>
-                            <td style='padding-left: 35px; padding-right: 30px;' class="d-none">
+                            <td class="d-none" style='padding-left: 35px; padding-right: 30px;'>
                                 <a class="btn btn-dark disabled" id="btn-cut-request" style="padding-left: 40px; padding-right: 60px; padding-bottom: 8px; padding-top: 9px;">
                                     <div style="float:left; padding-top: 8px; padding-right: 30px; font-size: 12px;"><i class="fas fa-plus"></i></div>
                                     &nbsp;Заявка на<br/>раскрой
@@ -77,12 +77,22 @@ while ($row = $fetcher->Fetch()) {
                     <?php if(IsInRole(array('technologist', 'dev', 'storekeeper'))): ?>
                     <a href="new.php" class="btn btn-outline-dark" style="padding-top: 14px; padding-bottom: 14px; padding-left: 30px; width: 222px; text-align: left; font-size: 14px;"><i class="fas fa-plus" style="font-size: 10px; margin-right: 18px;"></i>Новый паллет</a>
                     <?php endif; ?>
-                    <div style="display: inline-block; position: relative; margin-right: 55px; margin-left: 80px;" class="d-none">
+                    <div style="display: inline-block; position: relative; margin-right: 55px; margin-left: 80px;">
                         <a href="javascript: void(0);"><img src="../images/icons/filter1.svg" data-toggle="modal" data-target="#filterModal" data-text="Фильтр" /></a>
+                        <?php
+                        $get_count = 0;
+                        foreach ($_GET as $get_key=>$get_value) {
+                            if(!empty($get_value) && $get_value != PAGE) {
+                                $get_count++;
+                            }
+                        }
+                        if($get_count > 0):
+                        ?>
                         <div id="filter_params_counter_round" style="position: absolute; top: -7px; right: 0;">
                             <img src="../images/icons/filter_params_counter.svg" />
-                            <div id="filter_params_counter" style="position: absolute; top: 1px; left: 8px; color: white;">0</div>
+                            <div id="filter_params_counter" style="position: absolute; top: 1px; left: 8px; color: white;"><?=$get_count ?></div>
                         </div>
+                        <?php endif; ?>
                     </div>
                 </div>
             </div>
@@ -222,6 +232,28 @@ while ($row = $fetcher->Fetch()) {
             include '../include/pager_bottom.php';
             ?>
         </div>
+        
+        <?php
+        $film_brand_name = addslashes(filter_input(INPUT_GET, 'film_brand_name'));
+        $thicknesses = array();
+        $slider_value = 0;
+        $slider_index = 0;
+        
+        if(!empty($film_brand_name)) {
+            $grabber = (new Grabber("select distinct fbv.thickness from film_brand_variation fbv inner join film_brand fb on fbv.film_brand_id = fb.id where fb.name='$film_brand_name' order by thickness"))->result;
+            
+            foreach ($grabber as $row) {
+                $slider_index++;
+                array_push($thicknesses, $row['thickness']);
+                
+                if(filter_input(INPUT_GET, 'thickness') == $row['thickness']) {
+                    $slider_value = $slider_index;
+                }
+            }
+        }
+        
+        $json_thicknesses = json_encode($thicknesses);
+        ?>
         <div class="modal fixed-left fade" id="filterModal" tabindex="-1" role="dialog">
             <div class="modal-dialog modal-dialog-aside" role="document">
                 <div class="modal-content" style="padding-left: 35px; padding-right: 35px; width: 521px;">
@@ -229,58 +261,53 @@ while ($row = $fetcher->Fetch()) {
                     <h1 style="margin-top: 53px; margin-bottom: 20px; font-size: 32px; line-height: 48px; font-weight: 600;">Фильтр</h1>
                     <form method="get">
                         <div class="form-group">
-                            <select id="film_brand_id" name="film_brand_id" class="form-control" style="margin-top: 30px; margin-bottom: 30px;">
+                            <select id="film_brand_name" name="film_brand_name" class="form-control" style="margin-top: 30px; margin-bottom: 30px;">
                                 <option value="">МАРКА ПЛЕНКИ</option>
                                 <?php
                                 $film_brands = (new Grabber("select distinct fb.id, fb.name from pallet p inner join film_brand fb on p.film_brand_id = fb.id order by fb.name"))->result;
                                 foreach ($film_brands as $film_brand) {
-                                    $id = $film_brand['id'];
                                     $name = $film_brand['name'];
                                     $selected = '';
-                                    if(filter_input(INPUT_GET, 'film_brand_id') == $film_brand['id']) $selected = " selected='selected'";
-                                    echo "<option value='$id'$selected>$name</option>";
+                                    if(filter_input(INPUT_GET, 'film_brand_name') == $film_brand['name']) $selected = " selected='selected'";
+                                    echo "<option value='$name'$selected>$name</option>";
                                 }
                                 ?>
                             </select>
                         </div>
                         <h2 style="font-size: 24px; line-height: 32px; font-weight: 600;">Толщина</h2>
                         <div id="width_slider" style="width: 465px;">
-                            <div id="width_slider_values" style="height: 50px; position: relative; font-size: 14px; line-height: 18px;">
-                                <div style="position: absolute; bottom: 10px; left: 0;">8 мкм</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 10 / 72) + 6 ?>px;">20</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 20 / 72) + 6 ?>px;">30</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 30 / 72) + 6 ?>px;">40</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 40 / 72) + 6 ?>px;">50</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 50 / 72) + 6 ?>px;">60</div>
-                                <div style="position: absolute; bottom: 10px;  left: <?=(465 * 60 / 72) + 6 ?>px;">70</div>
-                                <div style="position: absolute; bottom: 10px; right: -7px;">80</div>
-                                <div style="position: absolute; bottom: 10px; right: -34px;">мкм</div>
+                            <div id="width_slider_values" style="height: 30px; position: relative; font-size: 14px; line-height: 18px;" class="d-flex justify-content-between mb-auto">
+                                <div class="p-1">все</div>
+                                <?php
+                                foreach ($thicknesses as $thickness) {
+                                    echo "<div class='p-1'>$thickness</div>";
+                                }
+                                ?>
                             </div>
                             <div id="slider"></div>
                         </div>
-                        <input type="hidden" id="thickness_from" name="thickness_from" />
-                        <input type="hidden" id="thickness_to" name="thickness_to" />
+                        <input type="hidden" id="thickness" name="thickness" value="<?= filter_input(INPUT_GET, 'thickness') ?>" />
                         <h2 style="font-size: 24px; line-height: 32px; font-weight: 600; margin-top: 43px; margin-bottom: 18px;">Ширина</h2>
                         <table style="margin-bottom: 30px;">
                             <tr>
                                 <td>
                                     <div style="display: inline; width: 120px;">
                                         <div style="width: 100%; text-align: center; font-size: 14px; line-height: 18px; padding-bottom: 5px;">От</div>
-                                        <input type="number" min="1" id="width_from" name="width_from" class="form-control" style="width: 100px;" value="<?= filter_input(INPUT_GET, 'width_from') ?>" />
+                                        <input type="text" id="width_from" name="width_from" class="form-control int-only" style="width: 100px;" value="<?= filter_input(INPUT_GET, 'width_from') ?>" />
                                     </div>
                                 </td>
                                 <td style="font-weight: bold; padding-top: 20px; padding-left: 5px; padding-right: 5px;">-</td>
                                 <td>
                                     <div style="display: inline; width: 120px;">
                                         <div style="width: 100%; text-align: center; font-size: 14px; line-height: 18px; padding-bottom: 5px;">До</div>
-                                        <input type="number" min="1" id="width_to" name="width_to" class="form-control" style="width: 100px;" value="<?= filter_input(INPUT_GET, 'width_to') ?>" />
+                                        <input type="text" id="width_to" name="width_to" class="form-control int-only" style="width: 100px;" value="<?= filter_input(INPUT_GET, 'width_to') ?>" />
                                     </div>
                                 </td>
                             </tr>
                         </table>
-                        <button type="button" class="btn" id="filter_clear" name="filter_clear" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #E4E1ED;"><img src="../images/icons/white-times.svg" />&nbsp;&nbsp;Очистить</button>
-                        <button type="button" class="btn" id="filter_cancel" name="filter_cancel" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #FFFFFF;">Отменить</button>
-                        <button type="submit" class="btn" id="filter_submit" name="filter_submit" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #CECACA;">Применить</button>
+                        <a href="<?=APPLICATION ?>/pallet/" type="button" class="btn" name="filter_clear" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #E4E1ED;"><img src="../images/icons/white-times.svg" />&nbsp;&nbsp;Очистить</a>
+                        <button type="button" class="btn" data-dismiss="modal" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #EEEEEE;">Отменить</button>
+                        <button type="submit" class="btn" style="margin-top: 20px; margin-bottom: 35px; padding: 10px; border-radius: 8px; background-color: #CECACA;">Применить</button>
                     </form>
                 </div>
             </div>
@@ -290,15 +317,67 @@ while ($row = $fetcher->Fetch()) {
         ?>
         <script src="<?=APPLICATION ?>/js/jquery-ui.js"></script>
         <script>
-            var slider_start_from = <?= null === filter_input(INPUT_GET, 'thickness_from') ? "20" : filter_input(INPUT_GET, 'thickness_from') ?>;
-            var slider_start_to = <?= null === filter_input(INPUT_GET, 'thickness_to') ? "50" : filter_input(INPUT_GET, 'thickness_to') ?>;
+            var thicknesses = JSON.parse('<?=$json_thicknesses ?>');
             
-            $( "#slider" ).slider({
-                    step: 10
+            $("#slider").slider({
+                range: false,
+                min: 0,
+                max: <?= count($thicknesses) ?>,
+                step: 1,
+                value: <?=$slider_value ?>,
+                slide: function(event, ui) {
+                    if(ui.value == '') {
+                        $("#thickness").val('');
+                    }
+                    else {
+                        $("#thickness").val(thicknesses[ui.value - 1]);
+                    }
+                }
             });
             
-            $("#thickness_from").val(slider_start_from);
-            $("#thickness_to").val(slider_start_to);
+            $('#film_brand_name').change(function(){
+                if($(this).val() == '') {
+                    $('#width_slider_values').html("<div class='p-1'>все</div>");
+                    $("#slider").slider({
+                        range: false,
+                        min: 0,
+                        max: 0,
+                        step: 1
+                    });
+                    $("#thickness").val('');
+                }
+                else {
+                    $.ajax({ url: "../ajax/thickness.php?film_brand_name="+$(this).val() })
+                            .done(function(data){
+                                var thicknesses = JSON.parse(data);
+                        
+                                var slider_labels = "<div class='p-1'>все</div>";
+                                thicknesses.forEach(thickness => slider_labels = slider_labels + "<div class='p-1'>" + thickness + "</div>");
+                                $('#width_slider_values').html(slider_labels);
+                                
+                                $("#slider").slider({
+                                    range: false,
+                                    min: 0,
+                                    max: thicknesses.length,
+                                    step: 1,
+                                    value: 0,
+                                    slide: function(event, ui) {
+                                        if(ui.value == '') {
+                                            $("#thickness").val('');
+                                        }
+                                        else {
+                                            $("#thickness").val(thicknesses[ui.value - 1]);
+                                        }
+                                    }
+                                });
+                                
+                                $("#thickness").val('');
+                            })
+                            .fail(function(){
+                                alert("Ошибка при получении толщины по названию.");
+                            });
+                }
+            });
             
             $('#chkMain').change(function(){
                 if($(this).is(':checked')) {
