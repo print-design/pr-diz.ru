3f563126b342be30312920205f60778ccde48ee1
user <user@Dmitry>
2021-04-01 08:57:00 +0300

    Рулоны в паллете.

--- erp/pallet/print.php
+++ erp/pallet/print.php
@@ -44,6 +44,9 @@ $fetcher = new Fetcher($sql);
 if($row = $fetcher->Fetch()) {
     $ud_ves = $row[0];
 }
+
+// Вертикальное положение стикера
+$sticker_top = 0;
 ?>
 <!DOCTYPE html>
 <html>
@@ -63,7 +66,7 @@ if($row = $fetcher->Fetch()) {
         </style>
     </head>
     <body class="print">
-        <div class="w-100" style="height: 1400px;">
+        <div class="w-100" style="height: 1400px; position: absolute; left: 30px; top: <?=$sticker_top ?>px;">
             <div style="margin-bottom: 20px; margin-top: 30px;">
                 <a href="<?=APPLICATION ?>/pallet/new.php"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
                 <div style="display: inline; margin-left: 300px; font-size: 30px;">
@@ -122,9 +125,11 @@ if($row = $fetcher->Fetch()) {
             </table>
         </div>
         
-        <div style="height: 250px; text-align: center; padding-top: 10px; font-size: 100px;"></div>
+        <?php
+        $sticker_top = 1850;
+        ?>
         
-        <div class="w-100" style="height: 1400px;">
+        <div class="w-100" style="height: 1400px; position: absolute; top: <?=$sticker_top ?>px">
             <table class="table table-bordered print w-100" style="writing-mode: vertical-rl; margin-left: 50px;">
                 <tbody>
                     <tr>
@@ -176,6 +181,100 @@ if($row = $fetcher->Fetch()) {
                 </tbody>
             </table>
         </div>
+        
+        <?php
+        $sql = "select id pallet_roll_id, weight, length, ordinal from pallet_roll where pallet_id = ". filter_input(INPUT_GET, 'id');
+        $pallet_rolls = (new Grabber($sql))->result;
+        foreach ($pallet_rolls as $pallet_roll):
+        $pallet_roll_id = $pallet_roll['pallet_roll_id'];
+        $weight = $pallet_roll['weight'];
+        $length = $pallet_roll['length'];
+        $ordinal = $pallet_roll['ordinal'];
+        
+        switch ($ordinal) {
+            case 1:
+                $sticker_top = 3530;
+                break;
+            
+            case 2:
+                $sticker_top = 5300;
+                break;
+                
+            case 3:
+                $sticker_top = 7000;
+                break;
+            
+            case 4:
+                $sticker_top = 8700;
+                break;
+                
+            case 5:
+                $sticker_top = 10450;
+                break;
+                
+            case 6:
+                $sticker_top = 12200;
+                break;
+
+            default:
+                break;
+        }
+        ?>
+        <div class="w-100" style="height: 1400px; position: absolute; left: 30px; top: <?=$sticker_top ?>px;">
+            <table class="table table-bordered print w-100" style="writing-mode: vertical-rl; margin-left: 50px;">
+                <tbody>
+                    <tr>
+                        <td colspan="2" class="font-weight-bold font-italic text-center">ООО &laquo;Принт-дизайн&raquo;</td>
+                        <td class="text-center text-nowrap">Рулон <span class="font-weight-bold"><?="П".$id."Р".$ordinal ?></span> от <?=$date ?></td>
+                    </tr>
+                    <tr>
+                        <td>Поставщик<br /><strong><?=$supplier ?></strong></td>
+                        <td>Ширина<br /><strong><?=$width ?> мм</strong></td>
+                        <td rowspan="6" class="qr" style="height: 20%;">
+                            <?php
+                            //include '../qr/qrlib.php';
+                            $errorCorrectionLevel = 'M'; // 'L','M','Q','H'
+                            $data = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].APPLICATION.'/pallet/roll.php?id='.$pallet_roll_id;
+                            $current_date_time = date("dmYHis");
+                            $filename = "../temp/$current_date_time.png";
+                            QRcode::png(addslashes($data), $filename, $errorCorrectionLevel, 10, 4, true);
+                            echo "<img src='$filename' />";
+                            
+                            // Удаление всех файлов, кроме текущего (чтобы диск не переполнился).
+                            $files = scandir("../temp/");
+                            foreach ($files as $file) {
+                                if($file != "$current_date_time.png" && !is_dir($file)) {
+                                    unlink("../temp/$file");
+                                }
+                            }
+                            ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>ID от поставщика<br /><strong><?=$id_from_supplier ?></strong></td>
+                        <td>Толщина, уд.вес<br /><strong><?=$thickness ?> мкм,<br /> <?=$ud_ves ?> г/м<sup style="top: 2px;">2</sup></strong></td>
+                    </tr>
+                    <tr>
+                        <td>Кладовщик<br /><strong><?=$storekeeper ?></strong></td>
+                        <td>Длина<br /><strong><?=$length ?> м</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Марка пленки<br /><strong><?=$film_brand ?></strong></td>
+                        <td>Масса нетто<br /><strong><?=$weight ?> кг</strong></td>
+                    </tr>
+                    <tr>
+                        <td>Статус<br /><strong><?=$status ?></strong></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="white-space: normal;">Комментарий<br /><strong><?= $comment ?></strong></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        endforeach;
+        ?>
     </body>
     <script>
         let shareData = {
