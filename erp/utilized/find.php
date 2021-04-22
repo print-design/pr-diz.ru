87090da6bdf4f79ca2bd0393383ff5850d136238
user <user@Dmitry>
2021-04-22 14:03:36 +0300

    Поиск.

--- erp/utilized/find.php
+++ erp/utilized/find.php
@@ -13,17 +13,17 @@ if(filter_input(INPUT_GET, "find") != '') {
     $placeholder = "";
 }
 ?>
-<button type="button" class="btn btn-light ml-auto<?=$append_class ?>" id="find-append" style="border-top-right-radius: 5px; border-bottom-right-radius: 5px;"><i class="fas fa-search"></i></button>
+<button type="button" class="btn btn-link nav-link ml-auto<?=$append_class ?>" id="find-append" style="border-top-right-radius: 5px; border-bottom-right-radius: 5px; color: black;"><i class="fas fa-search"></i></button>
 <form class="form-inline ml-auto mr-3<?=$find_class ?>" method="get" id="find-form" action="<?=APPLICATION.'/utilized/' ?>">
     <div class="input-group<?=$group_class ?>" id="find-group">
         <input type="text" class="form-control" id="find" name="find" placeholder="<?=$placeholder ?>" />
         <div class="input-group-append">
             <button type="submit" class="btn btn-outline-dark form-control" id="find-submit" style="border-top-right-radius: 5px; border-bottom-right-radius: 5px;">Найти</button>
         </div>
-        <div class="position-absolute pl-2 pr-2 pt-1 <?=$string_class ?>" style="top: 3px; left: 5px; bottom: 3px; background-color: gray; color: white;">
+        <div class="position-absolute px-2 align-text-bottom <?=$string_class ?>" style="top: 3px; left: 5px; bottom: 3px; background-color: gray; color: white; border-radius: 4px; padding-top: .4rem;">
         <?= filter_input(INPUT_GET, "find") ?>
             &nbsp;&nbsp;
-            <a href="<?=APPLICATION.'/utilized/' ?>"><i class="fas fa-window-close" style="color: white;"></i></a>
+            <a href="<?=APPLICATION.'/utilized/' ?>"><i class="fas fa-times" style="color: white;"></i></a>
         </div>
     </div>
 </form>
