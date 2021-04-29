35c0d4ebd24a96d5a4e77986b76f910f325326e7
user <user@Dmitry>
2021-04-29 11:48:13 +0300

    Объединение склада и графика.

---
+++ erp/include/header_grafik.php
@@ -0,0 +1,11 @@
+<?php
+include 'left_bar.php';
+?>
+<div class="container-fluid header">
+    <nav class="navbar navbar-expand-sm justify-content-end">
+        <?php
+        include 'header_right.php';
+        ?>
+    </nav>
+</div>
+<div id="topmost"></div>
--- erp/include/left_bar.php
+++ erp/include/left_bar.php
@@ -2,4 +2,5 @@
     <a href="<?=APPLICATION ?>/" class="left_bar_item logo" title="На главную" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/logo.svg" /></a>
     <a href="javascript: void(0);" class="left_bar_item"><img src="<?=APPLICATION ?>/images/nav_clock.svg" /></a>
     <a href="<?=APPLICATION ?>/pallet/" class="left_bar_item" title="Склад" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_sklad.svg" /></a>
+    <a href="<?=APPLICATION ?>/grafik/" class="left_bar_item" title="График" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
 </div>
