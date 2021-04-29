76a7daff062d3e8a91554ad7544da422267a4b47
user <user@Dmitry>
2021-04-29 12:08:36 +0300

    Объединение склада и графика.

--- erp/include/head.php
+++ erp/include/head.php
@@ -1,6 +1,6 @@
 <meta charset="UTF-8">
 <title>Принт-дизайн. Управление ресурсами предприятия</title>
 <link href="<?=APPLICATION ?>/css/bootstrap.min.css" rel="stylesheet">
-<link href="<?=APPLICATION ?>/../fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
+<link href="<?=APPLICATION ?>/fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/css/main.css?version=20210327" rel="stylesheet">
 <link rel="shortcut icon" type="image/x-icon" href="<?=APPLICATION ?>/favicon.ico" />
