b55d39ff76e6ef3fdc2f6bbc2b1acbf018a1d9b2
user <user@Dmitry>
2021-05-04 10:23:16 +0300

    Перенос проекта в корень сайта.

--- include/left_bar.php
+++ include/left_bar.php
@@ -3,6 +3,10 @@
     <?php if(LoggedIn()): ?>
     <a href="javascript: void(0);" class="left_bar_item"><img src="<?=APPLICATION ?>/images/nav_clock.svg" /></a>
     <a href="<?=APPLICATION ?>/pallet/" class="left_bar_item" title="Склад" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_sklad.svg" /></a>
+    <?php if(IsInRole(array('technologist', 'dev', 'manager'))): ?>
     <a href="<?=APPLICATION ?>/grafik/comiflex.php" class="left_bar_item" title="График" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
-    <?php endif; ?>
+    <?php
+    endif;
+    endif;
+    ?>
 </div>
