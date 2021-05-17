0baf2d10e4c97245f236429fe228752632cdb8de
user <user@Dmitry>
2021-05-17 13:25:22 +0300

    subheader_norm

--- include/subheader_norm.php
+++ include/subheader_norm.php
@@ -1,27 +1,17 @@
 <?php
-$form_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/form.php')) == APPLICATION.'/norm/form.php' ? " class='active'" : "";
-$paint_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/paint.php')) == APPLICATION.'/norm/paint.php' ? " class='active'" : "";
-$glue_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/glue.php')) == APPLICATION.'/norm/glue.php' ? " class='active'" : "";
-$machine_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/machine.php')) == APPLICATION.'/norm/machine.php' ? " class='active'" : "";
-$fitting_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/fitting.php')) == APPLICATION.'/norm/fitting.php' ? " class='active'" : "";
+$form_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/form.php')) == APPLICATION.'/norm/form.php' ? " active" : "";
+$paint_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/paint.php')) == APPLICATION.'/norm/paint.php' ? " active" : "";
+$glue_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/glue.php')) == APPLICATION.'/norm/glue.php' ? " active" : "";
+$machine_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/machine.php')) == APPLICATION.'/norm/machine.php' ? " active" : "";
+$fitting_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/norm/fitting.php')) == APPLICATION.'/norm/fitting.php' ? " active" : "";
 ?>
 <hr />
 <div class="d-flex justify-content-start nav2">
-    <div class="p-1 row">
-        <div class="col-2 text-nowrap">
-            <a<?=$form_class ?> href="<?=APPLICATION ?>/norm/form.php">Стоимость форм</a>
-        </div>
-        <div class="col-2 text-nowrap">
-            <a<?=$paint_class ?> href="<?=APPLICATION ?>/norm/paint.php">Стоимость краски</a>
-        </div>
-        <div class="col-2 text-nowrap">
-            <a<?=$glue_class ?> href="<?=APPLICATION ?>/norm/glue.php">Стоимость клея</a>
-        </div>
-        <div class="col-2 text-nowrap">
-            <a<?=$machine_class ?> href="<?=APPLICATION ?>/norm/machine.php">Нормы работы оборудования</a>
-        </div>
-        <div class="col-2 text-nowrap">
-            <a<?=$fitting_class ?> href="<?=APPLICATION ?>/norm/fitting.php">Приладка</a>
-        </div>
+    <div class="text-nowrap">
+        <a href="<?=APPLICATION ?>/norm/form.php" class="mr-3<?=$form_class ?>">Стоимость форм</a>
+        <a href="<?=APPLICATION ?>/norm/paint.php" class="mr-3<?=$paint_class ?>">Стоимость краски</a>
+        <a href="<?=APPLICATION ?>/norm/glue.php" class="mr-3<?=$glue_class ?>">Стоимость клея</a>
+        <a href="<?=APPLICATION ?>/norm/machine.php" class="mr-3<?=$machine_class ?>">Нормы работы оборудования</a>
+        <a href="<?=APPLICATION ?>/norm/fitting.php" class="mr-3<?=$fitting_class ?>">Приладка</a>
     </div>
 </div>
