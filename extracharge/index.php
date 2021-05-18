88a4c726463acf6c5c0acbd6f0d9ea0a0d50cb3e
user <user@Dmitry>
2021-05-18 17:05:13 +0300

    extracharge

--- extracharge/index.php
+++ extracharge/index.php
@@ -5,6 +5,17 @@ include '../include/topscripts.php';
 if(!IsInRole(array('technologist', 'dev'))) {
     header('Location: '.APPLICATION.'/unauthorized.php');
 }
+
+// Список типов наценки
+$sql = "select id, name from extracharge_type";
+$grabber = new Grabber($sql);
+$error_message = $grabber->error;
+
+$extracharge_types = array();
+
+foreach ($grabber->result as $row) {
+    $extracharge_types[$row['id']] = $row['name'];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -28,6 +39,31 @@ if(!IsInRole(array('technologist', 'dev'))) {
                     <?php
                     include '../include/subheader_admin.php';
                     ?>
+                </div>
+            </div>
+            <hr />
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <?php
+                    $extracharge_type_id = 1;
+                    ?>
+                    <h2><?=$extracharge_types[$extracharge_type_id] ?></h2>
+                    <form method="post">
+                        <button type="submit" class="btn btn-light"><i class="fas fa-plus"></i>&nbsp;&nbsp;Добавить</button>
+                    </form>
+                    <br />
+                    <?php
+                    $extracharge_type_id = 2;
+                    ?>
+                    <h2><?=$extracharge_types[$extracharge_type_id] ?></h2>
+                    <?php
+                    $extracharge_type_id = 3;
+                    ?>
+                    <h2><?=$extracharge_types[$extracharge_type_id] ?></h2>
+                    <?php
+                    $extracharge_type_id = 4;
+                    ?>
+                    <h2><?=$extracharge_types[$extracharge_type_id] ?></h2>
                 </div>
             </div>
         </div>
