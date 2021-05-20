68817b8f7cfd4813cc8947fffcba6e2abb766b05
user <user@Dmitry>
2021-05-20 15:59:22 +0300

    Новый дизайн.

--- calculation/calculation.php
+++ calculation/calculation.php
@@ -117,30 +117,78 @@ $work_type = $row['work_type'];
                     <!-- Расчёт -->
                     <div id="calculation">
                         <h1>Расчет</h1>
-                        <div class="mt-3 mb-1">Наценка</div>
-                        <div class="font-weight-bold mt-1 mb-1" style="font-size: large;">10%</div>
-                        <div class="mt-3 mb-1">Себестоимость</div>
-                        <div class="font-weight-bold mt-1 mb-1" style="font-size: large;"><?= number_format(1200000, 0, ",", " ") ?> руб.</div>
-                        <div class="mt-3 mb-1">Отгрузочная стоимость</div>
-                        <div class="font-weight-bold mt-1 mb-1" style="font-size: large;"><?= number_format(800000, 0, ",", " ") ?> руб.</div>
+                        <div class="d-table w-100">
+                            <div class="d-table-row">
+                                <div class="d-table-cell pb-2 pt-2">
+                                    <div style="font-size: x-small;">Наценка</div>
+                                    10%
+                                </div>
+                                <div class="d-table-cell pb-2 pt-2 pl-3" style="color: gray; border: solid 1px gray; border-radius: 10px;">
+                                    <div style="font-size: x-small;">Курс евро</div>
+                                    93
+                                </div>
+                            </div>
+                            <div class="d-table-row">
+                                <div class="d-table-cell pb-2 pt-2">
+                                    Себестоимость
+                                    <div class="font-weight-bold" style="font-size: large;">1 200 000 руб.</div>
+                                </div>
+                                <div class="d-table-cell pb-2 pt-2 pl-3">
+                                    За 1 кг
+                                    <div class="font-weight-bold" style="font-size: large;">765 руб.</div>
+                                </div>
+                            </div>
+                            <div class="d-table-row">
+                                <div class="d-table-cell pb-2 pt-2">
+                                    Отгрузочная стоимость
+                                    <div class="font-weight-bold" style="font-size: large;">800 000 руб.</div>
+                                </div>
+                                <div class="d-table-cell pb-2 pt-2 pl-3">
+                                    За 1 кг
+                                    <div class="font-weight-bold" style="font-size: large;">978 руб.</div>
+                                </div>
+                            </div>
+                        </div>
                         <button type="button" class="btn btn-light" id="show_costs" onclick="javascript: ShowCosts();"><i class="fa fa-chevron-down"></i>&nbsp;Показать расходы</button>
                         <div id="costs" class="d-none">
                             <button type="button" class="btn btn-light" id="hide_costs" onclick="javascript: HideCosts();"><i class="fa fa-chevron-up"></i>&nbsp;Скрыть расходы</button>
-                            <div class="mt-3 mb-1">Отходы</div>
-                            <div class="font-weight-bold mt-1 mb-1" style="font-size: large;"><?= number_format(200280, 0, ",", " ") ?> руб.&nbsp;&nbsp;&nbsp;<?= number_format(24.5, 1, ",", " ") ?> кг.</div>
-                            <div class="mt-3 mb-1">Клей</div>
-                            <div class="font-weight-bold mt-1 mb-3" style="font-size: large;"><?= number_format(800000, 0, ",", " ") ?> руб.</div>
+                            <div class="d-table w-100">
+                                <div class="d-table-row">
+                                    <div class="d-table-cell pb-2 pt-2">
+                                        Отходы
+                                        <div class="font-weight-bold" style="font-size: large;">1 280 руб.</div>
+                                    </div>
+                                    <div class="d-table-cell pb-2 pt-2 pl-3">
+                                        <br />
+                                        <div class="font-weight-bold" style="font-size: large;">4,5 кг.</div>
+                                    </div>
+                                </div>
+                                <div class="d-table-row">
+                                    <div class="d-table-cell pb-2 pt-2">
+                                        Клей
+                                        <div class="font-weight-bold" style="font-size: large;">800 000 руб.</div>
+                                    </div>
+                                    <div class="d-table-cell pb-2 pt-2 pl-3">
+                                        <br />
+                                        <div class="font-weight-bold" style="font-size: large;">1,0 кг.</div>
+                                    </div>
+                                </div>
+                                <div class="d-table-row">
+                                    <div class="d-table-cell pb-2 pt-2">
+                                        Работа ламинатора
+                                        <div class="font-weight-bold">230 руб.</div>
+                                    </div>
+                                    <div class="d-table-cell pb-2 pt-2 pl-3">
+                                        <br />
+                                        <div class="font-weight-bold" style="font-size: large;">1,5 ч.</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <form method="post">
                             <input type="hidden" id="id" name="id" value="<?= filter_input(INPUT_GET, 'id') ?>" />
                             <input type="hidden" id="change_status_submit" name="change_status_submit" />
-                            <?php if($status_id == 1): ?>
-                            <button type="submit" id="status_id" name="status_id" value="2" class="btn btn-outline-dark mt-3">Отправить КП</button>
-                            <?php elseif($status_id == 2): ?>
-                            <button type="submit" id="status_id" name="status_id" value="3" class="btn btn-outline-dark mt-3">Отправить в работу</button>
-                            <?php elseif ($status_id == 4): ?>
-                            <button type="submit" id="status_id" name="status_id" value="6" class="btn btn-outline-dark mt-3">Составить тех. карту</button>
-                            <?php endif; ?>
+                            <button type="submit" id="status_id" name="status_id" value="2" class="btn btn-outline-dark mt-3">Сделать КП</button>
                         </form>
                     </div>
                 </div>
