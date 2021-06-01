0dd7e60edfaaf0e91217d7937415787c67d0e169
user <user@Dmitry>
2021-06-01 15:30:24 +0300

    Нормы для каждой машины.

--- admin/machine.php
+++ admin/machine.php
@@ -14,19 +14,14 @@ define('ISINVALID', ' is-invalid');
 $form_valid = true;
 $error_message = '';
 
-$lamination_valid = '';
+$price_valid = '';
 $lam_speed_valid = '';
 $speed_valid = '';
 
 // Сохранение введённых значений
 if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
-    if(empty(filter_input(INPUT_POST, 'lamination'))) {
-        $lamination_valid = ISINVALID;
-        $form_valid = false;
-    }
-    
-    if(empty(filter_input(INPUT_POST, 'lam_speed'))) {
-        $lam_speed_valid = ISINVALID;
+    if(empty(filter_input(INPUT_POST, 'price'))) {
+        $price_valid = ISINVALID;
         $form_valid = false;
     }
     
@@ -39,27 +34,24 @@ if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
     
     if($form_valid) {
         // Старый объект
-        $old_lamination = '';
-        $old_lam_speed = '';
+        $old_price = '';
         $old_speed = '';
         
-        $sql = "select lamination, lam_speed, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
+        $sql = "select price, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
         $fetcher = new Fetcher($sql);
         $error_message = $fetcher->error;
         
         if($row = $fetcher->Fetch()) {
-            $old_lamination = $row['lamination'];
-            $old_lam_speed = $row['lam_speed'];
+            $old_price = $row['price'];
             $old_speed = $row['speed'];
         }
         
         // Новый объект
-        $new_lamination = filter_input(INPUT_POST, 'lamination');
-        $new_lam_speed = filter_input(INPUT_POST, 'lam_speed');
+        $new_price = filter_input(INPUT_POST, 'price');
         $new_speed = filter_input(INPUT_POST, 'speed');
         
-        if($old_lamination != $new_lamination || $old_lam_speed != $new_lam_speed || $old_speed != $new_speed) {
-            $sql = "insert into norm_machine (machine_id, lamination, lam_speed, speed) values ($machine_id, $new_lamination, $new_lam_speed, $new_speed)";
+        if($old_price != $new_price || $old_speed != $new_speed) {
+            $sql = "insert into norm_machine (machine_id, price, speed) values ($machine_id, $new_price, $new_speed)";
             $executer = new Executer($sql);
             $error_message = $executer->error;
         }
@@ -70,19 +62,17 @@ if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
 }
 
 // Получение объекта
-$lamination = '';
-$lam_speed = '';
+$price = '';
 $speed = '';
 
-$sql = "select lamination, lam_speed, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
+$sql = "select price, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
 $fetcher = new Fetcher($sql);
 if(empty($error_message)) {
     $error_message = $fetcher->error;
 }
 
 if($row = $fetcher->Fetch()) {
-    $lamination = $row['lamination'];
-    $lam_speed = $row['lam_speed'];
+    $price = $row['price'];
     $speed = $row['speed'];
 }
 ?>
@@ -125,19 +115,14 @@ if($row = $fetcher->Fetch()) {
                     <form method="post">
                         <input type="hidden" id="machine_id" name="machine_id" value="<?= filter_input(INPUT_GET, 'machine_id') ?>" />
                         <div class="form-group">
-                            <label for="speed">Стоимость работы печатной машины (руб/час)</label>
-                            <input type="text" class="form-control float-only" id="speed" name="speed" value="<?= empty($speed) ? "" : floatval($speed) ?>" placeholder="Стоимость, час" required="required" />
-                            <div class="invalid-feedback">Стоимость работы печатной машины обязательно</div>
+                            <label for="price">Стоимость работы оборудования (руб/час)</label>
+                            <input type="text" class="form-control float-only" id="price" name="price" value="<?= empty($price) ? "" : floatval($price) ?>" placeholder="Стоимость, час" required="required" />
+                            <div class="invalid-feedback">Стоимость обязательно</div>
                         </div>
                         <div class="form-group">
-                            <label for="lamination">Стоимость работы ламинации (руб/час)</label>
-                            <input type="text" class="form-control float-only" id="lamination" name="lamination" value="<?= empty($lamination) ? "" : floatval($lamination) ?>" placeholder="Стоимость, час" required="required" />
-                            <div class="invalid-feedback">Стоимость работы ламинации обязательно</div>
-                        </div>
-                        <div class="form-group">
-                            <label for="lam_speed">Скорость работы ламинатора (м/мин)</label>
-                            <input type="text" class="form-control float-only" id="lam_speed" name="lam_speed" value="<?= empty($lam_speed) ? "" : floatval($lam_speed) ?>" placeholder="Скорость, м/мин" required="required" />
-                            <div class="invalid-feedback">Скорость работы ламинатора обязательно</div>
+                            <label for="speed">Скорость работы оборудования (м/мин)</label>
+                            <input type="text" class="form-control float-only" id="speed" name="speed" value="<?= empty($speed) ? "" : floatval($speed) ?>" placeholder="Скорость, м/мин" required="required" />
+                            <div class="invalid-feedback">Скорость обязательно</div>
                         </div>
                         <button type="submit" id="norm_machine_submit" name="norm_machine_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
                     </form>
