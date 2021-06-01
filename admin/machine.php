b0f5b17de22235c47b17cec370021075c83a0966
user <user@Dmitry>
2021-06-01 11:47:43 +0300

    Нормы для каждой машины.

--- admin/machine.php
+++ admin/machine.php
@@ -6,6 +6,9 @@ if(!IsInRole(array('technologist', 'dev'))) {
     header('Location: '.APPLICATION.'/unauthorized.php');
 }
 
+// Печатная машина
+$machine_id = filter_input(INPUT_GET, 'machine_id');
+
 // Валидация формы
 define('ISINVALID', ' is-invalid');
 $form_valid = true;
@@ -13,9 +16,7 @@ $error_message = '';
 
 $lamination_valid = '';
 $lam_speed_valid = '';
-$zbs6_valid = '';
-$zbs8_valid = '';
-$comiflex_valid = '';
+$speed_valid = '';
 
 // Сохранение введённых значений
 if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
@@ -29,50 +30,36 @@ if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
         $form_valid = false;
     }
     
-    if(empty(filter_input(INPUT_POST, 'zbs6'))) {
-        $zbs6_valid = ISINVALID;
+    if(empty(filter_input(INPUT_POST, 'speed'))) {
+        $speed_valid = ISINVALID;
         $form_valid = false;
     }
     
-    if(empty(filter_input(INPUT_POST, 'zbs8'))) {
-        $zbs8_valid = ISINVALID;
-        $form_valid = false;
-    }
-    
-    if(empty(filter_input(INPUT_POST, 'comiflex'))) {
-        $comiflex_valid = ISINVALID;
-        $form_valid = false;
-    }
+    $machine_id = filter_input(INPUT_POST, 'machine_id');
     
     if($form_valid) {
         // Старый объект
         $old_lamination = '';
         $old_lam_speed = '';
-        $old_zbs6 = '';
-        $old_zbs8 = '';
-        $old_comiflex = '';
+        $old_speed = '';
         
-        $sql = "select lamination, lam_speed, zbs6, zbs8, comiflex from norm_machine order by date desc limit 1";
+        $sql = "select lamination, lam_speed, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
         $fetcher = new Fetcher($sql);
         $error_message = $fetcher->error;
         
         if($row = $fetcher->Fetch()) {
             $old_lamination = $row['lamination'];
             $old_lam_speed = $row['lam_speed'];
-            $old_zbs6 = $row['zbs6'];
-            $old_zbs8 = $row['zbs8'];
-            $old_comiflex = $row['comiflex'];
+            $old_speed = $row['speed'];
         }
         
         // Новый объект
         $new_lamination = filter_input(INPUT_POST, 'lamination');
         $new_lam_speed = filter_input(INPUT_POST, 'lam_speed');
-        $new_zbs6 = filter_input(INPUT_POST, 'zbs6');
-        $new_zbs8 = filter_input(INPUT_POST, 'zbs8');
-        $new_comiflex = filter_input(INPUT_POST, 'comiflex');
+        $new_speed = filter_input(INPUT_POST, 'speed');
         
-        if($old_lamination != $new_lamination || $old_lam_speed != $new_lam_speed || $old_zbs6 != $new_zbs6 || $old_zbs8 != $new_zbs8 || $old_comiflex != $new_comiflex) {
-            $sql = "insert into norm_machine (lamination, lam_speed, zbs6, zbs8, comiflex) values ($new_lamination, $new_lam_speed, $new_zbs6, $new_zbs8, $new_comiflex)";
+        if($old_lamination != $new_lamination || $old_lam_speed != $new_lam_speed || $old_speed != $new_speed) {
+            $sql = "insert into norm_machine (machine_id, lamination, lam_speed, speed) values ($machine_id, $new_lamination, $new_lam_speed, $new_speed)";
             $executer = new Executer($sql);
             $error_message = $executer->error;
         }
@@ -85,11 +72,9 @@ if(null !== filter_input(INPUT_POST, 'norm_machine_submit')) {
 // Получение объекта
 $lamination = '';
 $lam_speed = '';
-$zbs6 = '';
-$zbs8 = '';
-$comiflex = '';
+$speed = '';
 
-$sql = "select lamination, lam_speed, zbs6, zbs8, comiflex from norm_machine order by date desc limit 1";
+$sql = "select lamination, lam_speed, speed from norm_machine where machine_id = $machine_id order by date desc limit 1";
 $fetcher = new Fetcher($sql);
 if(empty($error_message)) {
     $error_message = $fetcher->error;
@@ -98,9 +83,7 @@ if(empty($error_message)) {
 if($row = $fetcher->Fetch()) {
     $lamination = $row['lamination'];
     $lam_speed = $row['lam_speed'];
-    $zbs6 = $row['zbs6'];
-    $zbs8 = $row['zbs8'];
-    $comiflex = $row['comiflex'];
+    $speed = $row['speed'];
 }
 ?>
 <!DOCTYPE html>
@@ -140,6 +123,12 @@ if($row = $fetcher->Fetch()) {
             <div class="row">
                 <div class="col-12 col-md-4 col-lg-2">
                     <form method="post">
+                        <input type="hidden" id="machine_id" name="machine_id" value="<?= filter_input(INPUT_GET, 'machine_id') ?>" />
+                        <div class="form-group">
+                            <label for="speed">Стоимость работы печатной машины (руб/час)</label>
+                            <input type="text" class="form-control float-only" id="speed" name="speed" value="<?= empty($speed) ? "" : floatval($speed) ?>" placeholder="Стоимость, час" required="required" />
+                            <div class="invalid-feedback">Стоимость работы печатной машины обязательно</div>
+                        </div>
                         <div class="form-group">
                             <label for="lamination">Стоимость работы ламинации (руб/час)</label>
                             <input type="text" class="form-control float-only" id="lamination" name="lamination" value="<?= empty($lamination) ? "" : floatval($lamination) ?>" placeholder="Стоимость, час" required="required" />
@@ -149,21 +138,6 @@ if($row = $fetcher->Fetch()) {
                             <label for="lam_speed">Скорость работы ламинатора (м/мин)</label>
                             <input type="text" class="form-control float-only" id="lam_speed" name="lam_speed" value="<?= empty($lam_speed) ? "" : floatval($lam_speed) ?>" placeholder="Скорость, м/мин" required="required" />
                             <div class="invalid-feedback">Скорость работы ламинатора обязательно</div>
-                        </div>
-                        <div class="form-group">
-                            <label for="zbs6">Стоимость работы печатной машины ZBS 6 цветов (руб/час)</label>
-                            <input type="text" class="form-control float-only" id="zbs6" name="zbs6" value="<?= empty($zbs6) ? "" : floatval($zbs6) ?>" placeholder="Стоимость, час" required="required" />
-                            <div class="invalid-feedback">Стоимость работы печатной машины ZBS 6 цветов обязательно</div>
-                        </div>
-                        <div class="form-group">
-                            <label for="zbs8">Стоимость работы печатной машины ZBS 8 цветов (руб/час)</label>
-                            <input type="text" class="form-control float-only" id="zbs8" name="zbs8" value="<?= empty($zbs8) ? "" : floatval($zbs8) ?>" placeholder="Стоимость, час" required="required" />
-                            <div class="invalid-feedback">Стоимость работы печатной машины ZBS 8 цветов обязательно</div>
-                        </div>
-                        <div class="form-group">
-                            <label for="comiflex">Стоимость работы печатной машины Comiflex (руб/час)</label>
-                            <input type="text" class="form-control float-only" id="comiflex" name="comiflex" value="<?= empty($comiflex) ? "" : floatval($comiflex) ?>" placeholder="Стоимость, час" required="required" />
-                            <div class="invalid-feedback">Стоимость работы печатной машины Comiflex обязательно</div>
                         </div>
                         <button type="submit" id="norm_machine_submit" name="norm_machine_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
                     </form>
