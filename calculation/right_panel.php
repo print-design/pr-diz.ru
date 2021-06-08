15c2aaf16606f63fb8455ffa856909c3f6e0cd8f
user <user@Dmitry>
2021-06-08 13:04:02 +0300

    Переделка 8.07.2021.

--- calculation/right_panel.php
+++ calculation/right_panel.php
@@ -76,11 +76,11 @@ if(isset($create_calculation_submit_class) && empty($create_calculation_submit_c
                     <div>Закупочная стоимость</div>
                     <div class="value mb-2">800 000 &#8381;</div>
                     <div>Минимальная ширина</div>
-                    <div class="value mb-2">800 000 &#8381;</div>
+                    <div class="value mb-2">800 000 м</div>
                     <div>Масса с приладкой</div>
-                    <div class="value mb-2">8 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
+                    <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
                     <div>Масса без приладки</div>
-                    <div class="value mb-2">7 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+                    <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
                 </div>
                 <div class="d-table-cell pb-2 pt-2 pl-3" style="width: 33%;">
                     <?php if(!empty($lamination1_brand_name)): ?>
@@ -88,11 +88,11 @@ if(isset($create_calculation_submit_class) && empty($create_calculation_submit_c
                     <div>Закупочная стоимость</div>
                     <div class="value mb-2">800 000 &#8381;</div>
                     <div>Минимальная ширина</div>
-                    <div class="value mb-2">800 000 &#8381;</div>
+                    <div class="value mb-2">800 000 м</div>
                     <div>Масса с приладкой</div>
-                    <div class="value mb-2">8 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
+                    <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
                     <div>Масса без приладки</div>
-                    <div class="value mb-2">7 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+                    <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
                     <?php endif; ?>
                 </div>
                 <div class="d-table-cell pb-2 pt-2 pl-3" style="width: 33%;">
@@ -101,11 +101,11 @@ if(isset($create_calculation_submit_class) && empty($create_calculation_submit_c
                     <div>Закупочная стоимость</div>
                     <div class="value mb-2">800 000 &#8381;</div>
                     <div>Минимальная ширина</div>
-                    <div class="value mb-2">800 000 &#8381;</div>
+                    <div class="value mb-2">800 000 м</div>
                     <div>Масса с приладкой</div>
-                    <div class="value mb-2">8 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
+                    <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
                     <div>Масса без приладки</div>
-                    <div class="value mb-2">7 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+                    <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
                     <?php endif; ?>
                 </div>
             </div>
