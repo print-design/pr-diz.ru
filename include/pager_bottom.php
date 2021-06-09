98a2c8a9008c34cb6f213b9d5a20ec3c589bc188
user <user@Dmitry>
2021-06-09 14:50:27 +0300

    Переделка 9.07.2021.

--- include/pager_bottom.php
+++ include/pager_bottom.php
@@ -31,7 +31,7 @@ if($pager_pages_count > 1) {
                 echo "<li class='page-item".$disabled."'><a class='page-link' href='?".http_build_query($pager_get_params)."'>".$i."</a></li>";
             }
         }
-        elseif ($pager_page <= $max_head_page) {    
+        elseif ($pager_page <= $max_head_page - 2) {    
             for($i = 1; $i <= $max_head_page; $i++) {
                 $pager_get_params[PAGE] = $i;
                 $disabled = "";
@@ -43,7 +43,7 @@ if($pager_pages_count > 1) {
             $pager_get_params[PAGE] = $pager_pages_count;
             echo "<li class='page-item'><a class='page-link' href='?". http_build_query($pager_get_params)."'>".$pager_pages_count."</a>";
         }
-        elseif ($pager_page >= $min_tail_page) {
+        elseif ($pager_page >= $min_tail_page + 2) {
             $pager_get_params[PAGE] = 1;
             echo "<li class='page-item'><a class='page-link' href='?". http_build_query($pager_get_params)."'>1</a>";
             echo "<li class='page-item disabled'><a class='page-link' href='javascript:void(0);'>...</a></li>";
@@ -59,8 +59,9 @@ if($pager_pages_count > 1) {
             echo "<li class='page-item'><a class='page-link' href='?". http_build_query($pager_get_params)."'>1</a>";
             echo "<li class='page-item disabled'><a class='page-link' href='javascript:void(0);'>...</a></li>";
             
-            $min_page = intval($pager_page - (LINKS_VISIBLE / 2));
-            $max_page = $min_page + LINKS_VISIBLE;
+            $floor = floor(LINKS_VISIBLE / 2);
+            $min_page = $pager_page - $floor;
+            $max_page = $pager_page + $floor;
             for($i = $min_page; $i <= $max_page; $i++) {
                 $pager_get_params[PAGE] = $i;
                 $disabled = "";
