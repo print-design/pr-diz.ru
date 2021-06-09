eab45fb19778ec80e98f02009d598e95da35ae76
user <user@Dmitry>
2021-06-09 15:58:39 +0300

    Переделка 9.07.2021.

--- admin/characteristics.php
+++ admin/characteristics.php
@@ -23,11 +23,17 @@ $form_valid = true;
 $error_message = '';
 
 $colorfulness_valid = '';
+$max_width_valid = '';
 
 // Сохранение введённых значений
-if(null !== filter_input(INPUT_POST, 'colorfulness_submit')) {
+if(null !== filter_input(INPUT_POST, 'characteristics_submit')) {
     if(empty(filter_input(INPUT_POST, 'colorfulness'))) {
         $colorfulness_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    if(empty(filter_input(INPUT_POST, 'max_width'))) {
+        $max_width_valid = ISINVALID;
         $form_valid = false;
     }
     
@@ -36,20 +42,23 @@ if(null !== filter_input(INPUT_POST, 'colorfulness_submit')) {
     if($form_valid) {
         // Старый объект
         $old_colorfulness = '';
+        $old_max_width = '';
         
-        $sql = "select colorfulness from machine where id = $machine_id";
+        $sql = "select colorfulness, max_width from machine where id = $machine_id";
         $fetcher = new Fetcher($sql);
         $error_message = $fetcher->error;
         
         if($row = $fetcher->Fetch()) {
-            $old_colorfulness = $row[0];
+            $old_colorfulness = $row['colorfulness'];
+            $old_max_width = $row['max_width'];
         }
         
         // Новый объект
         $new_colorfulness = filter_input(INPUT_POST, 'colorfulness');
+        $new_max_width = filter_input(INPUT_POST, 'max_width');
         
-        if($old_colorfulness != $new_colorfulness) {
-            $sql = "update machine set colorfulness = $new_colorfulness where id = $machine_id";
+        if($old_colorfulness != $new_colorfulness || $old_max_width != $new_max_width) {
+            $sql = "update machine set colorfulness = $new_colorfulness, max_width = $new_max_width where id = $machine_id";
             $executer = new Executer($sql);
             $error_message = $executer->error;
         }
@@ -61,15 +70,17 @@ if(null !== filter_input(INPUT_POST, 'colorfulness_submit')) {
 
 // Получение объекта
 $colorfulness = '';
+$max_width = '';
 
-$sql = "select colorfulness from machine where id = $machine_id";
+$sql = "select colorfulness, max_width from machine where id = $machine_id";
 $fetcher = new Fetcher($sql);
 if(empty($error_message)) {
     $error_message = $fetcher->error;
 }
 
 if($row = $fetcher->Fetch()) {
-    $colorfulness = $row[0];
+    $colorfulness = $row['colorfulness'];
+    $max_width = $row['max_width'];
 }
 ?>
 <!DOCTYPE html>
@@ -89,7 +100,7 @@ if($row = $fetcher->Fetch()) {
                echo "<div class='alert alert-danger'>$error_message</div>";
             }
             
-            if(null !== filter_input(INPUT_POST, 'colorfulness_submit') && empty($error_message)):
+            if(null !== filter_input(INPUT_POST, 'characteristics_submit') && empty($error_message)):
             ?>
             <div class="alert alert-success">Данные сохранены</div>
             <?php
@@ -126,7 +137,23 @@ if($row = $fetcher->Fetch()) {
                             </select>
                             <div class="invalid-feedback">Красочность обязательно</div>
                         </div>
-                        <button type="submit" id="colorfulness_submit" name="colorfulness_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
+                        <div class="form-group">
+                            <label for="max_width">Максимальная ширина материала</label>
+                            <input type="text" 
+                                   class="form-control float-only" 
+                                   id="max_width" 
+                                   name="max_width" 
+                                   value="<?= empty($max_width) || $max_width == 0.0 ? "" : floatval($max_width) ?>" 
+                                   placeholder="Максимальная ширина материала" 
+                                   required="required" 
+                                   onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
+                                   onmouseup="javascript: $(this).attr('id', 'max_width'); $(this).attr('name', 'max_width'); $(this).attr('placeholder', 'Максимальная ширина материала');" 
+                                   onkeydown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
+                                   onkeyup="javascript: $(this).attr('id', 'max_width'); $(this).attr('name', 'max_width'); $(this).attr('placeholder', 'Максимальная ширина материала');" 
+                                   onfocusout="javascript: $(this).attr('id', 'max_width'); $(this).attr('name', 'max_width'); $(this).attr('placeholder', 'Максимальная ширина материала');" />
+                            <div class="invalid-feedback">Максимальная ширина материала обязательно</div>
+                        </div>
+                        <button type="submit" id="characteristics_submit" name="characteristics_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
                     </form>
                 </div>
             </div>
