8759e355ecf30ec6c0d28e7a8abc5f525d135bfc
user <user@Dmitry>
2021-06-10 13:37:15 +0300

    10.07.2021.

--- ajax/raport.php
+++ ajax/raport.php
@@ -6,13 +6,15 @@ $machine_id = filter_input(INPUT_GET, 'machine_id');
 echo "<option value='' hidden='hidden'>Рапорт...</option>";
 
 if(!empty($machine_id)) {
-    $sql = "select value from raport where machine_id = $machine_id order by value"; echo $sql;
+    $sql = "select name, value from raport where machine_id = $machine_id order by value";
     $grabber = new Grabber($sql);
     $result = $grabber->result;
     
     foreach ($result as $row) {
-        $raport = $row['value'];
-        echo "<option value='$raport'>".floatval($raport)."</option>";
+        $name = $row['name'];
+        $value = $row['value'];
+        $raport = empty($name) ? $value : "$name ($value)";
+        echo "<option value='$value'>$raport</option>";
     }
 }
 ?>
