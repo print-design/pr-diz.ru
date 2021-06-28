65cef593c857a2534b3684bb201cda4d85f95f07
user <user@Dmitry>
2021-06-28 11:52:37 +0300

    28.05.2021

--- pallet/pallet.php
+++ pallet/pallet.php
@@ -216,7 +216,7 @@ if(null !== filter_input(INPUT_POST, 'change-status-submit')) {
             }
             
             // Стирать старый комментарий может только технолог, остальные - только добавлять новый комментарий к старому
-            if(IsInRole(array('dev', 'technologist'))) {
+            if(IsInRole(array('dev', 'technologist', 'storekeeper'))) {
                 $sql .= "comment = '$comment' where id=$id";
             }
             else {
@@ -478,12 +478,12 @@ if(null === $comment) $comment = $row['comment'];
                         }
                         
                         $comment_value = htmlentities($comment);
-                        if(!IsInRole(array('technologist', 'dev'))) {
+                        if(!IsInRole(array('technologist', 'dev', 'storekeeper'))) {
                             $comment_value = "";
                         }
                         ?>
                         <label for="comment">Комментарий</label>
-                        <?php if(!IsInRole(array('technologist', 'dev'))): ?>
+                        <?php if(!IsInRole(array('technologist', 'dev', 'storekeeper'))): ?>
                         <p><?= htmlentities($comment) ?></p>
                         <?php endif; ?>
                         <textarea id="comment" name="comment" rows="4" class="form-control no-latin"<?=$comment_disabled ?>><?=$comment_value ?></textarea>
