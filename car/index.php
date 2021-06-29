e513439d99aa15c71b50f4d741f93351586a2030
user <user@Dmitry>
2021-06-29 14:18:34 +0300

    29.05.2021 карщик

--- car/index.php
+++ car/index.php
@@ -39,10 +39,47 @@ if(!IsInRole(array('technologist', 'dev', 'electrocarist'))) {
                echo "<div class='alert alert-danger'>$error_message</div>";
             }
             ?>
-            <h1>Карщик</h1>
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-4">
+                    <form method="get">
+                        <div class="form-group">
+                            <label for="id">Введите ID</label>
+                            <div class="d-flex">
+                                <input type="text"
+                                       id="id"
+                                       name="id"
+                                       class="form-control int-only"
+                                       required="required" 
+                                       onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name');" 
+                                       onmouseup="javascript: $(this).attr('id', 'id'); $(this).attr('name', 'id');" 
+                                       onkeydown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name');" 
+                                       onkeyup="javascript: $(this).attr('id', 'id'); $(this).attr('name', 'id');" 
+                                       onfocusout="javascript: $(this).attr('id', 'id'); $(this).attr('name', 'id');" />
+                                <button type="submit" class="btn btn-info ml-1 d-none" id="car-submit">Найти</button>
+                            </div>
+                            <div class="invalid-feedback">ID обязательно</div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
         <?php
         include '../include/footer.php';
         ?>
+        <script>
+            function SetSubmitVisibility(input) {
+                val = input.val();
+                if(val == '') {
+                    $('#car-submit').addClass('d-none');
+                }
+                else {
+                    $('#car-submit').removeClass('d-none');
+                }
+            }
+            
+            $('input#id').keypress(function (){ SetSubmitVisibility($(this)) });
+            $('input#id').keyup(function (){ SetSubmitVisibility($(this)) });
+            $('input#id').change(function (){ SetSubmitVisibility($(this)) });
+        </script>
     </body>
 </html>
