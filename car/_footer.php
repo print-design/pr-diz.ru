ed3a1881c0bf870b47ea87920937393634b80c07
user <user@Dmitry>
2021-06-30 14:29:10 +0300

    30.05.2021 карщик начальная страница - не мобильная

--- car/_footer.php
+++ car/_footer.php
@@ -1,20 +1,5 @@
 <script>
-    function SetSubmitVisibility(input, e) {
-        val = input.val();
-        if(val == '') {
-            $('#car-submit').addClass('d-none');
-        }
-        else {
-            $('#car-submit').removeClass('d-none');
-        }
-        
-        return true;
-        
-        input.val(val.replace(/[^0-9а-яА-Я]/g, ''));
-        return true;
-    }
-    
-    $('input#id').keydown(function (e){ return SetSubmitVisibility($(this), e) });
-    $('input#id').keyup(function (e){ return SetSubmitVisibility($(this), e) });
-    $('input#id').change(function (e){ return SetSubmitVisibility($(this), e) });
+    $('input#id').focusin(function (){
+        $('#car-submit').removeClass('d-none');
+    });
 </script>
