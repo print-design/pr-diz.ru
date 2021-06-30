a78bc2f016698e391a494690a9526b3ba4a5d06c
user <user@Dmitry>
2021-06-30 15:03:25 +0300

    30.05.2021 карщик начальная страница - не мобильная

--- include/head.php
+++ include/head.php
@@ -2,5 +2,6 @@
 <title>Принт-дизайн. Управление ресурсами предприятия</title>
 <link href="<?=APPLICATION ?>/css/bootstrap.min.css" rel="stylesheet">
 <link href="<?=APPLICATION ?>/fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
-<link href="<?=APPLICATION ?>/css/main.css?version=4" rel="stylesheet">
+<link href="<?=APPLICATION ?>/css/jquery-ui.css" rel="stylesheet"/>
+<link href="<?=APPLICATION ?>/css/main.css?version=43" rel="stylesheet">
 <link rel="shortcut icon" type="image/x-icon" href="<?=APPLICATION ?>/favicon.ico" />
