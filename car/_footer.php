16c5976badf6e346ca89a86de2e15c89f82ba6fe
user <user@Dmitry>
2021-07-01 12:55:35 +0300

    Revert "Revert "1.06.2021 карщик""
    This reverts commit 65e4f57e5ae8f52db2fa90f6d14f78c5c3db3b20.

--- car/_footer.php
+++ car/_footer.php
@@ -2,4 +2,9 @@
     $('input#id').focusin(function (){
         $('#car-submit').removeClass('d-none');
     });
+    
+    $('#find-clear').click(function() {
+        $('input#id').val('');
+        $('input#id').focus();
+    });
 </script>
