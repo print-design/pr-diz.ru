5509d293a9f172afdaa60f1fd4bb5c509fd1bf4c
user <user@Dmitry>
2021-07-06 10:58:54 +0300

    6.06.2021 перенос дизайна из master в release

--- include/left_bar.php
+++ include/left_bar.php
@@ -27,7 +27,7 @@ else if($folder == "grafik") {
     <?php
     if(IsInRole(array('technologist', 'dev'))):
     ?>
-    <a href="javascript: void(0);" class="left_bar_item<?=$zakaz_class ?>" title="Заказы"><img src="<?=APPLICATION ?>/images/nav_clock.svg" /></a>
+    <a href="javascript: void(0);" class="left_bar_item<?=$zakaz_class ?>"><img src="<?=APPLICATION ?>/images/nav_clock.svg" /></a>
     <?php
     endif;
     if(IsInRole(array('technologist', 'storekeeper', 'dev', 'manager'))):
