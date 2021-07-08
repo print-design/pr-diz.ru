b72ea80ca90d3ccb1f56ae58c70b7e0cd8dcfb8e
user <user@Dmitry>
2021-07-08 13:04:17 +0300

    8.06.2021 grafik удаление пустой смены

--- grafik/ajax/delete_edition.php
+++ grafik/ajax/delete_edition.php
@@ -2,5 +2,25 @@
 include '../include/topscripts.php';
 
 $id = filter_input(INPUT_GET, 'id');
-$error_message = (new Executer("delete from edition where id=$id"))->error;
+
+// Определяем смену
+$sql = "select workshift_id from edition where id = $id";
+$fetcher = new Fetcher($sql);
+$error_message = $fetcher->error;
+
+if($row = $fetcher->Fetch()) {
+    $workshift_id = $row[0];
+    
+    $sql = "delete from edition where id=$id";
+    $executer = new Executer($sql);
+    $error_message = $executer->error;
+    
+    if(empty($error_message)) {
+        $count = (new Fetcher("select count(id) from edition where workshift_id = $workshift_id"))->Fetch()[0];
+        
+        if($count == 0) {
+            $error_message = (new Executer("delete from workshift where id = $workshift_id"))->error;
+        }
+    }
+}
 ?>
