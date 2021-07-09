0741eec63946e3e845f491ac674e2cb72469b711
user <user@Dmitry>
2021-07-09 10:35:19 +0300

    9.06.2021 просмотр графика - сокращено до одного файла

--- grafik.php
+++ grafik.php
@@ -1,53 +1,46 @@
 <?php
-include '../include/topscripts.php';
-include '../include/grafik_readonly.php';
+include 'include/topscripts.php';
+include 'include/grafik_readonly.php';
 
 // Авторизация
 if(!IsInRole(array('technologist', 'storekeeper', 'dev', 'manager'))) {
     header('Location: '.APPLICATION.'/unauthorized.php');
 }
 
+// Если не указан параметр id, переводим на начальную страницу
+if(empty(filter_input(INPUT_GET, 'id'))) {
+    header('Location: '.APPLICATION);
+}
+
 $date_from = null;
 $date_to = null;
 GetDateFromDateTo(filter_input(INPUT_GET, 'from'), filter_input(INPUT_GET, 'to'), $date_from, $date_to);
 
-$grafik = new GrafikReadonly($date_from, $date_to, 6);
-$grafik->name = 'Ламинатор 1';
-$grafik->user1Name = 'Ламинаторщик';
-$grafik->userRole = 4;
-
-$grafik->hasEdition = true;
-$grafik->hasOrganization = true;
-$grafik->hasLength = true;
-$grafik->hasLamination = true;
-$grafik->hasManager = true;
-$grafik->hasComment = true;
-
+$grafik = new GrafikReadonly($date_from, $date_to, filter_input(INPUT_GET, 'id'));
 $error_message = $grafik->error_message;
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>График - Ламинатор 1</title>
         <?php
-        include '../include/head.php';
+        include 'include/head.php';;
         ?>
     </head>
     <body>
         <?php
-        include '../include/header_grafik.php';
+        include 'include/header_grafik.php';
         ?>
         <div style="position: fixed; top: 100px; left: 100px; z-index: 1000;" id="waiting"></div>
         <div class="container-fluid" id="maincontent">
             <?php
-            if(isset($error_message) && $error_message != '') {
-                echo "<div class='alert alert-danger'>$error_message</div>";
+            if(!empty($error_message)) {
+               echo "<div class='alert alert-danger'>$error_message</div>";
             }
             $grafik->ShowPage();
             ?>
         </div>
         <?php
-        include '../include/footer.php';
+        include 'include/footer.php';
         ?>
     </body>
 </html>
