1b0b1ae0d6404257b869ddbab27c39cd87464c42
user <user@Dmitry>
2021-07-09 10:12:57 +0300

    9.06.2021 просмотр графика - сокращено до одного файла

--- include/header_grafik.php
+++ include/header_grafik.php
@@ -5,19 +5,19 @@ include 'left_bar.php';
     <nav class="navbar navbar-expand-sm justify-content-end">
         <ul class="navbar-nav">
             <?php
-            $comiflex_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/comiflex.php' ? ' disabled' : '';
-            $zbs1_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/zbs1.php' ? ' disabled' : '';
-            $zbs2_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/zbs2.php' ? ' disabled' : '';
-            $zbs3_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/zbs3.php' ? ' disabled' : '';
-            $atlas_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/atlas.php' ? ' disabled' : '';
-            $laminators1_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/laminators1.php' ? ' disabled' : '';
-            $laminators2_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/laminators2.php' ? ' disabled' : '';
-            $cutters1_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters1.php' ? ' disabled' : '';
-            $cutters2_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters2.php' ? ' disabled' : '';
-            $cutters3_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters3.php' ? ' disabled' : '';
-            $cutters4_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters4.php' ? ' disabled' : '';
-            $cutters_atlas_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters_atlas.php' ? ' disabled' : '';
-            $cutters_soma_status = filter_input(INPUT_SERVER, 'PHP_SELF') == APPLICATION.'/grafik/cutters_soma.php' ? ' disabled' : '';
+            $comiflex_status = filter_input(INPUT_GET, 'id') == 1 ? ' disabled' : '';
+            $zbs1_status = filter_input(INPUT_GET, 'id') == 2 ? ' disabled' : '';
+            $zbs2_status = filter_input(INPUT_GET, 'id') == 3 ? ' disabled' : '';
+            $zbs3_status = filter_input(INPUT_GET, 'id') == 4 ? ' disabled' : '';
+            $atlas_status = filter_input(INPUT_GET, 'id') == 5 ? ' disabled' : '';
+            $laminators1_status = filter_input(INPUT_GET, 'id') == 6 ? ' disabled' : '';
+            $laminators2_status = filter_input(INPUT_GET, 'id') == 13 ? ' disabled' : '';
+            $cutters1_status = filter_input(INPUT_GET, 'id') == 7 ? ' disabled' : '';
+            $cutters2_status = filter_input(INPUT_GET, 'id') == 9 ? ' disabled' : '';
+            $cutters3_status = filter_input(INPUT_GET, 'id') == 10 ? ' disabled' : '';
+            $cutters4_status = filter_input(INPUT_GET, 'id') == 14 ? ' disabled' : '';
+            $cutters_atlas_status = filter_input(INPUT_GET, 'id') == 11 ? ' disabled' : '';
+            $cutters_soma_status = filter_input(INPUT_GET, 'id') == 12 ? ' disabled' : '';
             
             $query_string = '';
             $period = array();
@@ -36,43 +36,43 @@ include 'left_bar.php';
             if(IsInRole(array('technologist', 'dev', 'manager', 'storekeeper'))):
             ?>
             <li class="nav-item">
-                <a class="nav-link<?=$comiflex_status ?>" href="<?=APPLICATION ?>/grafik/comiflex.php<?=$query_string ?>">Comiflex</a>
+                <a class="nav-link<?=$comiflex_status ?>" href="<?=APPLICATION ?>/grafik.php?id=1<?=$query_string ?>">Comiflex</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$zbs1_status ?>" href="<?=APPLICATION ?>/grafik/zbs1.php<?=$query_string ?>">ZBS-1</a>
+                <a class="nav-link<?=$zbs1_status ?>" href="<?=APPLICATION ?>/grafik.php?id=2<?=$query_string ?>">ZBS-1</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$zbs2_status ?>" href="<?=APPLICATION ?>/grafik/zbs2.php<?=$query_string ?>">ZBS-2</a>
+                <a class="nav-link<?=$zbs2_status ?>" href="<?=APPLICATION ?>/grafik.php?id=3<?=$query_string ?>">ZBS-2</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$zbs3_status ?>" href="<?=APPLICATION ?>/grafik/zbs3.php<?=$query_string ?>">ZBS-3</a>
+                <a class="nav-link<?=$zbs3_status ?>" href="<?=APPLICATION ?>/grafik.php?id=4<?=$query_string ?>">ZBS-3</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$atlas_status ?>" href="<?=APPLICATION ?>/grafik/atlas.php<?=$query_string ?>">Атлас</a>
+                <a class="nav-link<?=$atlas_status ?>" href="<?=APPLICATION ?>/grafik.php?id=5<?=$query_string ?>">Атлас</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$laminators1_status ?>" href="<?=APPLICATION ?>/grafik/laminators1.php<?=$query_string ?>">Ламинатор 1</a>
+                <a class="nav-link<?=$laminators1_status ?>" href="<?=APPLICATION ?>/grafik.php?id=6<?=$query_string ?>">Ламинатор 1</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$laminators2_status ?>" href="<?=APPLICATION ?>/grafik/laminators2.php<?=$query_string ?>">Ламинатор 2</a>
+                <a class="nav-link<?=$laminators2_status ?>" href="<?=APPLICATION ?>/grafik.php?id=13<?=$query_string ?>">Ламинатор 2</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters1_status ?>" href="<?=APPLICATION ?>/grafik/cutters1.php<?=$query_string ?>">Резка 1</a>
+                <a class="nav-link<?=$cutters1_status ?>" href="<?=APPLICATION ?>/grafik.php?id=7<?=$query_string ?>">Резка 1</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters2_status ?>" href="<?=APPLICATION ?>/grafik/cutters2.php<?=$query_string ?>">Резка 2</a>
+                <a class="nav-link<?=$cutters2_status ?>" href="<?=APPLICATION ?>/grafik.php?id=9<?=$query_string ?>">Резка 2</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters3_status ?>" href="<?=APPLICATION ?>/grafik/cutters3.php<?=$query_string ?>">Резка 3</a>
+                <a class="nav-link<?=$cutters3_status ?>" href="<?=APPLICATION ?>/grafik.php?id=10<?=$query_string ?>">Резка 3</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters4_status ?>" href="<?=APPLICATION ?>/grafik/cutters4.php<?=$query_string ?>">Резка 4</a>
+                <a class="nav-link<?=$cutters4_status ?>" href="<?=APPLICATION ?>/grafik.php?id=14<?=$query_string ?>">Резка 4</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters_atlas_status ?>" href="<?=APPLICATION ?>/grafik/cutters_atlas.php<?=$query_string ?>">Резка &laquo;Атлас&raquo;</a>
+                <a class="nav-link<?=$cutters_atlas_status ?>" href="<?=APPLICATION ?>/grafik.php?id=11<?=$query_string ?>">Резка &laquo;Атлас&raquo;</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link<?=$cutters_soma_status ?>" href="<?=APPLICATION ?>/grafik/cutters_soma.php<?=$query_string ?>">Резка &laquo;Сома&raquo;</a>
+                <a class="nav-link<?=$cutters_soma_status ?>" href="<?=APPLICATION ?>/grafik.php?id=12<?=$query_string ?>">Резка &laquo;Сома&raquo;</a>
             </li>
             <?php
             endif;
--- include/left_bar.php
+++ include/left_bar.php
@@ -3,9 +3,11 @@ $php_self = $_SERVER['PHP_SELF'];
 $substrings = mb_split("/", $php_self);
 $count = count($substrings);
 $folder = '';
+$file = '';
 
 if($count > 2) {
     $folder = $substrings[$count - 2];
+    $file = $substrings[$count - 1];
 }
 
 $zakaz_class = '';
@@ -18,9 +20,10 @@ if($folder == "calculation") {
 else if($folder == "pallet" || $folder == "roll" || $folder == "utilized") {
     $sklad_class = " active";
 }
-else if($folder == "grafik") {
+else if($file == "grafik.php") {
     $grafik_class = " active";
 }
+
 ?>
 <div id="left_bar">
     <a href="<?=APPLICATION ?>/" class="left_bar_item logo" title="На главную" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/logo.svg" /></a>
@@ -33,7 +36,7 @@ else if($folder == "grafik") {
     if(IsInRole(array('technologist', 'storekeeper', 'dev', 'manager'))):
     ?>
     <a href="<?=APPLICATION ?>/pallet/" class="left_bar_item<?=$sklad_class ?>" title="Склад" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_sklad.svg" /></a>
-    <a href="<?=APPLICATION ?>/grafik/comiflex.php" class="left_bar_item<?=$grafik_class ?>" title="График" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
+    <a href="<?=APPLICATION ?>/grafik.php?id=1" class="left_bar_item<?=$grafik_class ?>" title="График" data-toggle="tooltip" data-placement="right"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
     <?php
     endif;
     ?>
