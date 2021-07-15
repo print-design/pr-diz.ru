b7ace0407b8b7237a56f36c3d86cae1fc44bb51b
user <user@Dmitry>
2021-07-15 11:39:55 +0300

    15.06.2021 резчик раскрой новая версия

--- cutter/wind.php
+++ cutter/wind.php
@@ -21,6 +21,7 @@ $form_valid = true;
 $error_message = '';
 
 $length_valid = '';
+$length_message = "Число, макс. 30000";
 $radius_valid = '';
 
 // Обработка отправки формы
@@ -67,6 +68,17 @@ if(null !== filter_input(INPUT_POST, 'next-submit')) {
     $net_weight = floatval($ud_ves) * floatval($length) * floatval($width) / 1000.0 / 1000.0;
     $cell = "Цех";
     $comment = "";
+    
+    // Валидация длины: длина+-20% от (0,15*R*R+11,3961*R-176,4427)/(толщина пленки/20)
+    $normal_length = (0.15 * floatval($radius) * floatval($radius) + 11.3961 * floatval($radius) - 176.4427) / (floatval($thickness) / 20.0);
+    $max_length = $normal_length * 1.2;
+    $min_length = $normal_length * 0.8;
+    
+    if($length > $max_length || $length < $min_length) {
+        $length_valid = ISINVALID;
+        $length_message = "Длина не соответствует радиусу";
+        $form_valid = false;
+    }
     
     if($form_valid) {
         $sql = "insert into cut (supplier_id, film_brand_id, thickness, width) values($supplier_id, $film_brand_id, $thickness, $width)";
@@ -172,7 +184,7 @@ if(null !== filter_input(INPUT_POST, 'next-submit')) {
                     <div class="input-group">
                         <input type="text" class="form-control int-only int-format<?=$length_valid ?>" id="length" name="length" value="<?= filter_input(INPUT_POST, 'length') ?>" required="required" />
                         <div class="input-group-append"><span class="input-group-text">м</span></div>
-                        <div class="invalid-feedback">Число, макс. 30000</div>
+                        <div class="invalid-feedback"><?=$length_message ?></div>
                     </div>
                 </div>
                 <div class="form-group">
