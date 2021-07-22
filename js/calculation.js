c1b342573165884a98892fba2164cf6dd39d7d8b
user <user@Dmitry>
2021-07-22 09:02:33 +0300

    22.06.2021 оптимизация расчёта

--- js/calculation.js
+++ js/calculation.js
@@ -1,3 +1,12 @@
+// ВЫЧИСЛЕНИЕ МАССЫ ПЛЁНКИ ПО ДЛИНЕ, ШИРИНЕ, УДЕЛЬНОМУ ВЕСУ
+// length - длина
+// width - ширина
+// density - удельный вес
+function GetFilmWeightByLengthWidth(length, width, density) {
+    var weight = (length * density * width) / 1000 / 1000;
+    return weight;
+}
+
 // ВЫЧИСЛЕНИЕ ДЛИНЫ И МАССЫ ПЛЁНКИ ПО ШПУЛЕ, ТОЛЩИНЕ, РАДИУСУ, ШИРИНЕ, УДЕЛЬНОМУ ВЕСУ
 // spool - шпуля
 // thickness - толщина
@@ -10,21 +19,12 @@ function GetFilmLengthWeightBySpoolThicknessRadiusWidth(spool, thickness, radius
     
     if(spool == 76) {
         length = (0.15 * radius * radius + 11.3961 * radius - 176.4427) * 20 / thickness;
-        weight = (length * density * width) / 1000 / 1000;
+        weight = GetFilmWeightByLengthWidth(length, width, density);
     }
     else if(spool == 152) {
         length = (0.1524 * radius * radius + 23.1245 * radius - 228.5017) * 20 / thickness;
-        weight = (length * density * width) / 1000 / 1000;
+        weight = GetFilmWeightByLengthWidth(length, width, density);
     }
     
     return { length: length, weight: weight };
-}
-
-// ВЫЧИСЛЕНИЕ МАССЫ ПЛЁНКИ ПО ДЛИНЕ, ШИРИНЕ, УДЕЛЬНОМУ ВЕСУ
-// length - длина
-// width - ширина
-// density - удельный вес
-function GetFilmWeightByLengthWidth(length, width, density) {
-    var weight = (length * density * width) / 1000 / 1000;
-    return weight;
 }
