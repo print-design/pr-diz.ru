8190cec623def455a08857250313cef02d0b1644
user <user@Dmitry>
2021-07-26 09:11:18 +0300

    26.06.2021 удаление истории

--- cutter/_close.php
+++ cutter/_close.php
@@ -4,10 +4,7 @@ $request_uri = mb_substr($_SERVER['REQUEST_URI'], mb_strlen(APPLICATION.'/cutter
 $user_id = GetUserId();
 $sql = "update user set request_uri='$request_uri' where id=$user_id";
 $error_message = (new Executer($sql))->error;
-if(empty($error_message)) {
-    $sql = "insert into history (user, request_uri) values($user_id, '$request_uri')";
-    $error_message = (new Executer($sql))->error;    
-}
+
 if(!empty($error_message)) {
     exit($error_message);
 }
