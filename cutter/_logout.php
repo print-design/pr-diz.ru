9d30f01c8e73b625cc501c1f9449e5aa0e824fc0
user <user@Dmitry>
2021-07-26 11:37:49 +0300

    26.06.2021 Добавление проверки текущей странице к ссылкам "назад"

--- cutter/_logout.php
+++ cutter/_logout.php
@@ -1,11 +1,19 @@
 <?php
 include_once '../include/topscripts.php';
+$request_uri = mb_substr($_SERVER['REQUEST_URI'], mb_strlen(APPLICATION.'/cutter/'));
+$user_id = GetUserId();
+$sql = "update user set request_uri='$request_uri' where id=$user_id";
+$error_message = (new Executer($sql))->error;
+
+if(!empty($error_message)) {
+    exit($error_message);
+}
 ?>
 <div class="container-fluid header">
     <nav class="navbar navbar-expand-sm justify-content-start">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <button class="btn btn-link nav-link goto_index"><i class="fas fa-chevron-left"></i>&nbsp;Назад</button>
+                <button class="btn btn-link nav-link" id="index-submit"><i class="fas fa-chevron-left"></i>&nbsp;Назад</button>
             </li>
         </ul>
     </nav>
@@ -41,3 +49,42 @@ include_once '../include/topscripts.php';
         <button type="submit" class="btn btn-outline-danger form-control" id="logout_submit" name="logout_submit">Выйти</button>
     </form>
 </div>
+<script>
+    // Выход из системы
+    $('#logout_submit').click(function() {
+        $.ajax({ url: "_check_db_uri.php?uri=<?= urlencode($request_uri) ?>" })
+                .done(function(data) {
+                    if(data == "OK") {
+                        return true;
+                    }
+                    else {
+                        OpenAjaxPage(data);
+                        return false;
+                    }
+                })
+                .fail(function() {
+                    alert('Ошибка при переходе на страницу.');
+                    return false;
+                });
+    });
+    
+    // Возврат на главную страницу
+    function Index() {
+        OpenAjaxPage("_index.php");
+    }
+    
+    $('#index-submit').click(function() {
+        $.ajax({ url: "_check_db_uri.php?uri=<?= urlencode($request_uri) ?>" })
+                .done(function(data) {
+                    if(data == "OK") {
+                        Index();
+                    }
+                    else {
+                        OpenAjaxPage(data);
+                    }
+                })
+                .fail(function() {
+                    alert('Ошибка при переходе на страницу.');
+                });
+    });
+</script>
