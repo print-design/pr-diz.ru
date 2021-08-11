26958e9c0aff09df9321b403c9c0ae19c8afef7c
user <user@Dmitry>
2021-08-11 13:15:29 +0300

    Revert "11.08.2021 проверка на наличие нарезок, не имеющих исходных роликов"
    This reverts commit 6550f9fcf4bfef00c3f540bd6c60c3e04ebc5b28.

--- cutter/_next.php
+++ cutter/_next.php
@@ -291,7 +291,3 @@ while ($row = $fetcher->Fetch()) {
                 });
     });
 </script>
-<?php
-// Проверка, имеются ли нарезки, у которых нет исходного ролика
-include '_check_no_unclosed_cut.php';
-?>
