8b02b79b7dca74cec904d390463cf912f86a626d
user <user@Dmitry>
2021-08-16 12:14:06 +0300

    16.08.2021 cut_history

--- cutter/_cut_history.php
+++ cutter/_cut_history.php
@@ -1,12 +1,12 @@
 <?php
-$sql = "insert into cut_history (user_id, page_db) values($user_id, '$request_uri')";
+$sql = "update cut_history set datetime = now(), page_real = '$request_uri' where user_id = $user_id and page_real = ''";
 $error_message = (new Executer($sql))->error;
 
 if(!empty($error_message)) {
     exit($error_message);
 }
 
-$sql = "update cut_history set datetime = now(), page_real = '$request_uri' where user_id = $user_id and page_real = ''";
+$sql = "insert into cut_history (user_id, page_db) values($user_id, (select request_uri from user where id = $user_id))";
 $error_message = (new Executer($sql))->error;
 
 if(!empty($error_message)) {
