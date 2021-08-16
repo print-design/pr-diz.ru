c3ce79aab8db223ac6311e4ce6dd3ee135e1e224
user <user@Dmitry>
2021-08-16 13:24:07 +0300

    16.08.2021 История раскроя

--- history/cut.php
+++ history/cut.php
@@ -1,5 +1,6 @@
 <?php
 include '../include/topscripts.php';
+include '../include/pager_top.php';
 
 // Авторизация
 if(!IsInRole(array('technologist', 'dev'))) {
@@ -33,8 +34,45 @@ if(!IsInRole(array('technologist', 'dev'))) {
             <?php
             include '../include/subheader_history.php';
             ?>
-            <hr />
-            <h1>Раскрой</h1>
+            <hr class="pb-0 mb-0" />
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Пользователь</th>
+                        <th>Дата</th>
+                        <th>Предыдущая</th>
+                        <th>Текущая</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $sql = "select count(id) from cut_history";
+                    $fetcher = new Fetcher($sql);
+                    
+                    if($row = $fetcher->Fetch()) {
+                        $pager_total_count = $row[0];
+                    }
+                    
+                    $sql = "select u.first_name, u.last_name, ch.datetime, ch.page_db, ch.page_real from cut_history ch inner join user u on ch.user_id = u.id order by ch.id desc limit $pager_skip, $pager_take";
+                    $fetcher = new Fetcher($sql);
+                    while($row = $fetcher->Fetch()) {
+                        ?>
+                    <tr>
+                        <td><?=$row['first_name'].' '.$row['last_name'] ?></td>
+                        <td><?=$row['datetime'] ?></td>
+                        <td><?=$row['page_db'] ?></td>
+                        <td><?=$row['page_real'] ?></td>
+                        <td style="background-color: green; color: white; font-weight: bold;">OK</td>
+                    </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+            include '../include/pager_bottom.php';
+            ?>
         </div>
         <?php
         include '../include/footer.php';
