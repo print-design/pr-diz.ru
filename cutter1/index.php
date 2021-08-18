b366761be5e4696f5a8701ea080bb62be8ef49b8
user <user@Dmitry>
2021-08-18 12:18:19 +0300

    18.08.2021 резчик новая версия

---
+++ cutter1/index.php
@@ -0,0 +1,53 @@
+<?php
+include_once '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('technologist', 'dev', 'cutter'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+include_once '_redirects.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php
+        include '../include/style_mobile.php';
+        ?>
+        <script src='<?=APPLICATION ?>/js/jquery-3.5.1.min.js'></script>
+        <script src='<?=APPLICATION ?>/js/bootstrap.min.js'></script>
+        <script src="<?=APPLICATION ?>/js/jquery-ui.js"></script>
+        <script src="<?=APPLICATION ?>/js/popper.min.js"></script>
+        <script src="<?=APPLICATION ?>/js/jquery.maskedinput.js"></script>
+        <script src="<?=APPLICATION ?>/js/calculation.js?version=100"></script>
+    </head>
+    <body>
+        <div class="container-fluid header">
+            <nav class="navbar navbar-expand-sm justify-content-between">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Склад</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown no-dropdown-arrow-after">
+                        <a class="nav-link mr-0" id="logout-submit" href="logout.php"><i class="fa fa-cog" aria-hidden="true""></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div id="topmost"></div>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            ?>
+            <button id="mat-submit" class="btn btn-dark w-100 mt-4">Приступить к раскрою</button>
+        </div>
+    </body>
+</html>
---
+++ cutter1/logout.php
@@ -0,0 +1,70 @@
+<?php
+include_once '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('technologist', 'dev', 'cutter'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+include_once '_redirects.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php
+        include '../include/style_mobile.php';
+        ?>
+        <script src='<?=APPLICATION ?>/js/jquery-3.5.1.min.js'></script>
+        <script src='<?=APPLICATION ?>/js/bootstrap.min.js'></script>
+        <script src="<?=APPLICATION ?>/js/jquery-ui.js"></script>
+        <script src="<?=APPLICATION ?>/js/popper.min.js"></script>
+        <script src="<?=APPLICATION ?>/js/jquery.maskedinput.js"></script>
+        <script src="<?=APPLICATION ?>/js/calculation.js?version=100"></script>
+    </head>
+    <body>
+        <div class="container-fluid header">
+            <nav class="navbar navbar-expand-sm justify-content-start">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link"><i class="fas fa-chevron-left"></i>&nbsp;Назад</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div id="topmost"></div>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            
+            $position = "Работник";
+            if(IsInRole('electrocarist')) {
+                $position = "Водитель погрузчика";
+            }
+            elseif(IsInRole('cutter')) {
+                $position = "Резчик раскрой";
+            }
+            ?>
+            <p class="mt-4" style="font-size: 18px; line-height: 24px; font-weight: 600;"><?=$position ?>:</p>
+                <?php
+                $sql = "select last_name, first_name from user where id=". GetUserId();
+                $fetcher = new Fetcher($sql);
+                if($row = $fetcher->Fetch()):
+                    $last_name = $row['last_name'];
+                    $first_name = $row['first_name'];
+                ?>
+            <p class="mt-2 mb-5" style="font-size: 24px; line-height: 32px; font-weight: 600;"><?=$last_name.' '.$first_name ?></p>
+                <?php
+                endif;
+                ?>
+            <form method="post" id="form_logout">
+                <button type="submit" class="btn btn-outline-danger form-control" id="logout_submit" name="logout_submit">Выйти</button>
+            </form>
+        </div>
+    </body>
+</html>
