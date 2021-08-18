66ac946aa75f3f255767f5b84ca704616bd57c24
user <user@Dmitry>
2021-08-18 13:20:28 +0300

    18.08.2021 grafik

--- grafik/include/head.php
+++ grafik/include/head.php
@@ -3,5 +3,5 @@
 <link href="<?=APPLICATION ?>/css/bootstrap.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/../fontawesome-free-5.15.1-web/css/all.min.css" rel="stylesheet" />
 <link href="<?=APPLICATION ?>/css/jquery-ui.css" rel="stylesheet"/>
-<link href="<?=APPLICATION ?>/css/main.css?version=1" rel="stylesheet" />
+<link href="<?=APPLICATION ?>/css/main.css?version=3" rel="stylesheet" />
 <link rel="icon" type="image/png" href="<?=APPLICATION ?>/favicon.ico" />
