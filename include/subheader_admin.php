5a450beba6cf5d4af2ab2bdc24838e36922a4254
user <user@Dmitry>
2021-08-18 08:58:52 +0300

    17.08.2021 grafik исправление

--- include/subheader_admin.php
+++ include/subheader_admin.php
@@ -3,12 +3,10 @@ $user_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATI
 $supplier_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/supplier')) == APPLICATION.'/supplier' ? " active" : "";
 $price_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/price')) == APPLICATION.'/price' ? " active" : "";
 $norm_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/admin')) == APPLICATION.'/admin' ? " active" : "";
-$history_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATION.'/history')) == APPLICATION.'/history' ? " active" : "";
 ?>
 <div class="text-nowrap nav2">
     <a class="mr-4<?=$user_class ?>" href="<?=APPLICATION ?>/user/">Сотрудники</a>
     <a class="mr-4<?=$supplier_class ?>" href="<?=APPLICATION ?>/supplier/">Поставщики</a>
     <a class="mr-4<?=$price_class ?>" href="<?=APPLICATION ?>/price/">Цены</a>
     <a class="mr-4<?=$norm_class ?>" href="<?=APPLICATION ?>/admin/characteristics.php<?= BuildQuery('machine_id', 1) ?>">Нормы</a>
-    <a class="mr-4<?=$history_class ?>" href="<?=APPLICATION ?>/history/cut.php">История</a>
 </div>
