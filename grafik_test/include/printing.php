ff35e735c132616dd18d90cef3b107310374bfe3
user <user@Dmitry>
2021-08-27 16:56:32 +0300

    27.08.2021 рациональный раскрой

--- grafik_test/include/printing.php
+++ grafik_test/include/printing.php
@@ -1,10 +1,25 @@
 <?php
+include_once 'database.php';
+include_once 'user.php';
+
 class Printing {
     private $fields;
-    
+    private ArrayObject $Users;
+
     // .....................................................................
     function __construct() {
         $this->fields = array();
+        
+        $this->Users = new ArrayObject();
+        
+        $sql = "select date, username, password, fio, quit from user";
+        $db = new Database($sql);
+        
+        while ($row = $db->Fetch()) {
+            $user = new User();
+            $user->Load($row);
+            $this->Users->append($user);
+        }
     }
     
     //.....................................................................
@@ -16,14 +31,25 @@ class Printing {
     function Append(string $value) {
         array_push($this->fields, $value);
     }
-    
+        
     //.....................................................................
     function Show() {
 ?>
 <tr>
-    <?php foreach ($this->fields as $field): ?>
-    <td style="padding-right: 20px;"><?= $field ?></td>
-    <?php endforeach; ?>
+    <td style="padding-right: 20px;">
+        <select>
+        <?php
+        foreach ($this->Users as $user) {
+            $user->ShowSelectOption();
+        }
+        ?>
+        </select>
+    </td>
+    <td style="padding-right: 20px;"><?= $this->fields['name'] ?></td>
+    <td style="padding-right: 20px;"><?= $this->fields['organization'] ?></td>
+    <td style="padding-right: 20px;"><?= $this->fields['length'] ?></td>
+    <td style="padding-right: 20px;"><?= $this->fields['width'] ?></td>
+    <td style="padding-right: 20px;"><?= $this->fields['square'] ?></td>
 </tr>
 <?php
     }
--- grafik_test/include/test_grafik.php
+++ grafik_test/include/test_grafik.php
@@ -3,25 +3,18 @@ include_once 'database.php';
 include_once 'printing.php';
 
 class Test_Grafik {
-    private $PrintingColumnNames = null;
     private ArrayObject $Printings;
 
     // .............................................................
     public function __construct() {
-        $this->PrintingRows = array();
         $this->Printings = new ArrayObject();
         
-        $sql = "select * from printing_table";
+        $sql = "select name, organization, length, width, square from printing_table";
         $db = new Database($sql);
-        $firstRow = true;
         
         while ($row = $db->Fetch()) {
-            if($this->PrintingColumnNames == null) {
-                $this->PrintingColumnNames = array_keys($row);
-            }
-            
             $printing = new Printing();
-            $printing->Load(array_values($row));
+            $printing->Load($row);
             $this->Printings->append($printing);
         }
     }
@@ -30,34 +23,36 @@ class Test_Grafik {
     public function Show() {
 ?>
 <table>
-    <?php if($this->Printings->count() > 0): ?>
     <tr>
-        <?php foreach($this->PrintingColumnNames as $columnName): ?>
-        <th><?=$columnName ?></th>
-        <?php endforeach; ?>
+        <th>Работники</th>
+        <th>Тираж</th>
+        <th>Заказчик</th>
+        <th>Длина</th>
+        <th>Ширина</th>
+        <th>Площадь</th>
     </tr>
     <?php
-    endif;
     foreach($this->Printings as $printing) {
         $printing->Show();
     }
     ?>
 </table>
 <?php
-    }   
+    }
     // .............................................................
     public function ShowRange($begin, $end) {
         if($begin < $end && $this->Printings->offsetExists($begin)) {
 ?>
 <table>
-    <?php if($this->Printings->count() > 0): ?>
     <tr>
-        <?php foreach($this->PrintingColumnNames as $columnName): ?>
-        <th><?=$columnName ?></th>
-        <?php endforeach; ?>
+        <th>Работники</th>
+        <th>Тираж</th>
+        <th>Заказчик</th>
+        <th>Длина</th>
+        <th>Ширина</th>
+        <th>Площадь</th>
     </tr>
     <?php
-    endif;
     for ($i=$begin; $i<=$end; $i++) {
         if($this->Printings->offsetExists($i)) {
             $this->Printings->offsetGet($i)->Show();
