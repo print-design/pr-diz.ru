9451e60a265960f37bf829ba79439251b9180fec
user <user@Dmitry>
2021-08-27 11:50:47 +0300

    27.08.2021 рациональный раскрой

--- analytics/rational_cut.php
+++ analytics/rational_cut.php
@@ -1,9 +1,13 @@
 <?php
 include '../include/topscripts.php';
 
+// Статус "СВОБОДНЫЙ"
+$free_status_id = 1;
+
 $brand_name = "";
 $thickness = null;
 $max_width = null;
+$combinations = array();
 
 // Обработка отправки формы
 if(null !== filter_input(INPUT_POST, 'rational_cut_submit')) {
@@ -13,14 +17,56 @@ if(null !== filter_input(INPUT_POST, 'rational_cut_submit')) {
     // Толщина
     $thickness = filter_input(INPUT_POST, 'thickness');
     
+    // Создаём список конечных плёнок
+    $targets = array();
+    $i = 0;
+    while (null !== filter_input(INPUT_POST, 'width_'.(++$i)) && null !== filter_input(INPUT_POST, 'length_'.$i)) {
+        $target = array();
+        $target['width'] = filter_input(INPUT_POST, 'width_'.$i);
+        $target['length'] = filter_input(INPUT_POST, 'length_'.$i);
+        array_push($targets, $target);
+    }
+    
     // Получаем наибольшую ширину плёнки данного типа
-    $sql = "select max(r.width) from roll r "
+    $sql = "select ifnull(("
+            . "select max(r.width) from roll r "
             . "inner join film_brand fb on r.film_brand_id = fb.id "
-            . "where trim(fb.name) = '$brand_name' and r.thickness = $thickness";
+            . "left join (select * from roll_status_history where id in (select max(id) from roll_status_history group by roll_id)) rsh on rsh.roll_id = r.id "
+            . "where trim(fb.name) = '$brand_name' and r.thickness = $thickness and (rsh.status_id is null or rsh.status_id = $free_status_id)"
+            . "), 0)";
+    
     $fetcher = new Fetcher($sql);
     $row = $fetcher->Fetch();
-    $max_width = $row[0];
+    $max_width_roll = $row[0];
     
+    $sql = "select ifnull(("
+            . "select max(p.width) from pallet_roll pr "
+            . "inner join pallet p on pr.pallet_id = p.id "
+            . "inner join film_brand fb on p.film_brand_id = fb.id "
+            . "left join (select * from pallet_roll_status_history where id in (select max(id) from pallet_roll_status_history group by pallet_roll_id)) prsh on prsh.pallet_roll_id = pr.id "
+            . "where trim(fb.name) = '$brand_name' and p.thickness = $thickness and (prsh.status_id is null or prsh.status_id = $free_status_id)"
+            . "), 0)";
+    $fetcher = new Fetcher($sql);
+    $row = $fetcher->Fetch();
+    $max_width_pallet = $row[0];
+    
+    $max_width = max($max_width_roll, $max_width_pallet);
+    
+    // Перебираем все возможные сочетания ширин, чтобы их сумма была не больше максимальной
+    $combination = array();
+    WalkTargets($combinations, $combination, $targets);
+}
+
+function WalkTargets(&$combinations, &$combination, &$targets) {
+    foreach ($targets as $target) {
+        $current_combination = $combination;
+        array_push($current_combination, $target);
+        array_push($combinations, $current_combination);
+    }
+}
+
+function GetWidthsSum($combination) {
+    return 0;
 }
 ?>
 <!DOCTYPE html>
@@ -121,7 +167,13 @@ if(null !== filter_input(INPUT_POST, 'rational_cut_submit')) {
                 </div>
                 <div class="col-12 col-md-6 col-lg-8">
                     <h2>Результаты</h2>
-                    <p><?=$max_width ?></p>
+                    <p>Наибольшая возможная ширина: <?=$max_width ?></p>
+                    <?php
+                    foreach($combinations as $combination) {
+                        echo '<br />';
+                        print_r($combination);
+                    }
+                    ?>
                 </div>
             </div>
         </div>
