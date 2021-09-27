db819cfdcb8517fe094224d204bf25f664f97327
user <user@Dmitry>
2021-09-27 16:51:03 +0300

    2.09.2021 резчик исправление

--- cutter/_check_cuts.php
+++ cutter/_check_cuts.php
@@ -14,44 +14,43 @@ function CheckCuts($user_id) {
         if($row = $fetcher->Fetch()) {
             $cut_id = $row[0];
             $remain = $row['remain'];
-        }
-            
-        // Если незакрытых нарезок нет, то 
-        // если страница не "Остаточный ролик"
-        // берём последнюю закрытую нарезку и проверяем есть ли остаточный ролик и равна ли сумма длин исходных роликов сумме длин намоток (+-300 м)
-        // и если их разница больше 300 м и нет остаточного ролика, перекидываем на страницу "остаточный ролик"
-        if(mb_substr_count($php_self, '/remain.php') == 0) { // remain.php, но не print_remain.php
-            if(empty($remain))
-            {
-                $sum_source = 0;
-                $sql = "select "
-                        . "ifnull((select sum(r.length) "
-                        . "from cut_source cs "
-                        . "inner join roll r on cs.roll_id = r.id "
-                        . "where cs.cut_id = $cut_id "
-                        . "and cs.is_from_pallet = 0), 0) "
-                        . "+ "
-                        . "ifnull((select sum(pr.length) "
-                        . "from cut_source cs "
-                        . "inner join pallet_roll pr on cs.roll_id = pr.id "
-                        . "where cs.cut_id = $cut_id "
-                        . "and cs.is_from_pallet = 1), 0) "
-                        . "sum_source";
-                $fetcher = new Fetcher($sql);
-                if($row = $fetcher->Fetch()) {
-                    $sum_source = $row['sum_source'];
-                }
         
-                $sum_wind = 0;
-                $sql = "select ifnull((select sum(length) from cut_wind where cut_id = $cut_id), 0) sum_length";
-                $fetcher = new Fetcher($sql);
-                if($row = $fetcher->Fetch()) {
-                    $sum_wind = $row['sum_length'];
-                }
+            // Если незакрытых нарезок нет, то 
+            // если страница не "Остаточный ролик"
+            // берём последнюю закрытую нарезку и проверяем есть ли остаточный ролик и равна ли сумма длин исходных роликов сумме длин намоток (+-300 м)
+            // и если их разница больше 300 м и нет остаточного ролика, перекидываем на страницу "остаточный ролик"
+            if(mb_substr_count($php_self, '/remain.php') == 0) { // remain.php, но не print_remain.php
+                if(empty($remain)) {
+                    $sum_source = 0;
+                    $sql = "select "
+                            . "ifnull((select sum(r.length) "
+                            . "from cut_source cs "
+                            . "inner join roll r on cs.roll_id = r.id "
+                            . "where cs.cut_id = $cut_id "
+                            . "and cs.is_from_pallet = 0), 0) "
+                            . "+ "
+                            . "ifnull((select sum(pr.length) "
+                            . "from cut_source cs "
+                            . "inner join pallet_roll pr on cs.roll_id = pr.id "
+                            . "where cs.cut_id = $cut_id "
+                            . "and cs.is_from_pallet = 1), 0) "
+                            . "sum_source";
+                    $fetcher = new Fetcher($sql);
+                    if($row = $fetcher->Fetch()) {
+                        $sum_source = $row['sum_source'];
+                    }
         
-                $sum_diff = intval($sum_source) - intval($sum_wind);
-                if($sum_diff > 300) {
-                    header("Location: ".APPLICATION."/cutter/remain.php");
+                    $sum_wind = 0;
+                    $sql = "select ifnull((select sum(length) from cut_wind where cut_id = $cut_id), 0) sum_length";
+                    $fetcher = new Fetcher($sql);
+                    if($row = $fetcher->Fetch()) {
+                        $sum_wind = $row['sum_length'];
+                    }
+        
+                    $sum_diff = intval($sum_source) - intval($sum_wind);
+                    if($sum_diff > 300) {
+                        header("Location: ".APPLICATION."/cutter/remain.php");
+                    }
                 }
             }
         }
