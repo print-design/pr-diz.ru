cd5bea1462ebca74276034a236062f44b9000e47
user <user@Dmitry>
2021-11-15 16:24:22 +0300

    15.11.2021 переименование

--- include/subheader_admin.php
+++ include/subheader_admin.php
@@ -7,6 +7,6 @@ $norm_class = substr(filter_input(INPUT_SERVER, 'PHP_SELF'), 0, strlen(APPLICATI
 <div class="text-nowrap nav2">
     <a class="mr-4<?=$user_class ?>" href="<?=APPLICATION ?>/user/">Сотрудники</a>
     <a class="mr-4<?=$supplier_class ?>" href="<?=APPLICATION ?>/supplier/">Поставщики</a>
-    <a class="mr-4<?=$price_class ?>" href="<?=APPLICATION ?>/price/">Цены</a>
+    <a class="mr-4<?=$price_class ?>" href="<?=APPLICATION ?>/price/">Цены пленки, бумаги</a>
     <a class="mr-4<?=$norm_class ?>" href="<?=APPLICATION ?>/admin/machine.php<?= BuildQuery('machine_id', 1) ?>">Нормы</a>
 </div>
