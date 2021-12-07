821e551b985c8df648fd4de844606365c9d4f4bd
user <user@Dmitry>
2021-12-07 15:34:01 +0300

    7.12.2021 калькулятор новая версия

--- request_calc/right_panel.php
+++ request_calc/right_panel.php
@@ -74,88 +74,157 @@ if(isset($create_request_calc_submit_class) && empty($create_request_calc_submit
     <div class="mt-3">
         <h2>Материалы&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h2>
     </div>
-    <div class="d-table w-100">
-        <div class="d-table-row">
-            <div class="d-table-cell mb-2 mt-2" style="width: 33%;">
-                <h3>Основная пленка&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
-                <div>Закупочная стоимость</div>
-                <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
-                <div>Минимальная ширина</div>
-                <div class="value mb-2">800 000 мм</div>
-                <div>Масса без приладки</div>
-                <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
-                <div>Масса с приладкой</div>
-                <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
-            </div>
-            <div class="d-table-cell" style="width: 33%;">
-                <?php if(!empty($lamination1_brand_name)): ?>
-                <div class=" mb-2 mt-2 pl-3" style=" border-left: solid 2px black;">
-                    <h3>Ламинация 1&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
-                    <div>Закупочная стоимость</div>
-                    <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
-                    <div>Минимальная ширина</div>
-                    <div class="value mb-2">800 000 мм</div>
-                    <div>Масса без приладки</div>
-                    <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
-                    <div>Масса с приладкой</div>
-                    <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
-                </div>
-                <?php endif; ?>
-            </div>
-            <div class="d-table-cell" style="width: 33%;">
-                <?php if(!empty($lamination2_brand_name)): ?>
-                <div class=" mb-2 mt-2 pl-3" style=" border-left: solid 2px black;">
-                    <h3>Ламинация 2&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
-                    <div>Закупочная стоимость</div>
-                    <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
-                    <div>Минимальная ширина</div>
-                    <div class="value mb-2">800 000 мм</div>
-                    <div>Масса без приладки</div>
-                    <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
-                    <div>Масса с приладкой</div>
-                    <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
-                </div>
-                <?php endif; ?>
-            </div>
+    <div class="row">
+        <div class="col-4">
+            <h3>Основная пленка&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
+            <div>Закупочная стоимость</div>
+            <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
+            <div>Минимальная ширина</div>
+            <div class="value mb-2">800 000 мм</div>
+            <div>Масса без приладки</div>
+            <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+            <div>Масса с приладкой</div>
+            <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
         </div>
+        <?php if(!empty($lamination1_brand_name)): ?>
+        <div class="col-4" style="border-left: solid 2px black;">
+            <h3>Ламинация 1&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
+            <div>Закупочная стоимость</div>
+            <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
+            <div>Минимальная ширина</div>
+            <div class="value mb-2">800 000 мм</div>
+            <div>Масса без приладки</div>
+            <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+            <div>Масса с приладкой</div>
+            <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
+        </div>
+        <?php endif; ?>
+        <?php if(!empty($lamination2_brand_name)): ?>
+        <div class="col-4" style="border-left: solid 2px black;">
+            <h3>Ламинация 2&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">765 <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></h3>
+            <div>Закупочная стоимость</div>
+            <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">236 &#8381; за <?=(empty($unit) || $unit == 'kg' ? "кг" : "шт") ?></span></div>
+            <div>Минимальная ширина</div>
+            <div class="value mb-2">800 000 мм</div>
+            <div>Масса без приладки</div>
+            <div class="value mb-2">7 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">172 000 м</span></div>
+            <div>Масса с приладкой</div>
+            <div class="value mb-2">8 000 кг&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">192 000 м</span></div>
+        </div>
+        <?php endif; ?>
     </div>
     <?php
     if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name) || $work_type_id == 2):
     ?>
-    <button type="button" class="btn btn-light" id="show_costs" onclick="javascript: ShowCosts();"><i class="fa fa-chevron-down"></i>&nbsp;Показать расходы</button>
-    <div id="costs" class="d-none">
-        <button type="button" class="btn btn-light" id="hide_costs" onclick="javascript: HideCosts();"><i class="fa fa-chevron-up"></i>&nbsp;Скрыть расходы</button>
-        <h2 class="mt-2">Расходы</h2>
-        <div class="d-table w-100">
-            <div class="d-table-row">
-                <div class="d-table-cell pb-2 pt-2" style="width: 33%;">
-                    <div>Отходы</div>
-                    <div class="value mb-2">1 280 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">4,5 кг</span></div>
-                    <?php if($work_type_id == 2): ?>
-                    <div>Краска</div>
-                    <div class="value mb-2">17 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">17,5 кг</span></div>
-                    <?php
-                    endif;
-                    if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
-                    ?>
-                    <div>Клей</div>
-                    <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">1,0 кг</span></div>
-                    <?php
-                    endif;
-                    if($work_type_id == 2):
-                    ?>
-                    <div>Печать тиража</div>
-                    <div class="value mb-2">470 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">6 ч. 30 мин.</span></div>
-                    <?php
-                    endif;
-                    if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
-                    ?>
-                    <div>Работа ламинатора</div>
-                    <div class="value mb-2">1 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">3 часа</span></div>
-                    <?php endif; ?>
-                </div>
-                <div class="d-table-cell"></div>
+    <div id="show_costs">
+        <div class="row">
+            <div class="col-4">
+                <button type="button" class="btn btn-light" onclick="javascript: ShowCosts();"><i class="fa fa-chevron-down"></i>&nbsp;Показать расходы</button>
             </div>
+            <?php if(!empty($lamination1_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;"></div>
+            <?php endif; ?>
+            <?php if(!empty($lamination2_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;"></div>
+            <?php endif; ?>
+        </div>
+    </div>
+    <div id="costs" class="d-none">
+        <div class="row">
+            <div class="col-4">
+                <button type="button" class="btn btn-light" id="hide_costs" onclick="javascript: HideCosts();"><i class="fa fa-chevron-up"></i>&nbsp;Скрыть расходы</button>
+                <h2 class="mt-2">Расходы</h2>
+            </div>
+            <?php if(!empty($lamination1_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;"></div>
+            <?php endif; ?>
+            <?php if(!empty($lamination2_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;"></div>
+            <?php endif; ?>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <div>Отходы</div>
+                <div class="value mb-2">1 280 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">4,5 кг</span></div>
+                <?php if($work_type_id == 2): ?>
+                <div>Краска</div>
+                <div class="value mb-2">17 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">17,5 кг</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Клей</div>
+                <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">1,0 кг</span></div>
+                <?php
+                endif;
+                if($work_type_id == 2):
+                ?>
+                <div>Печать тиража</div>
+                <div class="value mb-2">470 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">6 ч. 30 мин.</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Работа ламинатора</div>
+                <div class="value mb-2">1 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">3 часа</span></div>
+                <?php endif; ?>
+            </div>
+            <?php if(!empty($lamination1_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;">
+                <div>Отходы</div>
+                <div class="value mb-2">1 280 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">4,5 кг</span></div>
+                <?php if($work_type_id == 2): ?>
+                <div>Краска</div>
+                <div class="value mb-2">17 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">17,5 кг</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Клей</div>
+                <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">1,0 кг</span></div>
+                <?php
+                endif;
+                if($work_type_id == 2):
+                ?>
+                <div>Печать тиража</div>
+                <div class="value mb-2">470 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">6 ч. 30 мин.</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Работа ламинатора</div>
+                <div class="value mb-2">1 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">3 часа</span></div>
+                <?php endif; ?>
+            </div>
+            <?php endif; ?>
+            <?php if(!empty($lamination2_brand_name)): ?>
+            <div class="col-4" style="border-left: solid 2px black;">
+                <div>Отходы</div>
+                <div class="value mb-2">1 280 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">4,5 кг</span></div>
+                <?php if($work_type_id == 2): ?>
+                <div>Краска</div>
+                <div class="value mb-2">17 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">17,5 кг</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Клей</div>
+                <div class="value mb-2">800 000 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">1,0 кг</span></div>
+                <?php
+                endif;
+                if($work_type_id == 2):
+                ?>
+                <div>Печать тиража</div>
+                <div class="value mb-2">470 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">6 ч. 30 мин.</span></div>
+                <?php
+                endif;
+                if(!empty($lamination1_brand_name) || !empty($lamination2_brand_name)):
+                ?>
+                <div>Работа ламинатора</div>
+                <div class="value mb-2">1 500 &#8381;&nbsp;&nbsp;&nbsp;<span style="font-weight: normal;">3 часа</span></div>
+                <?php endif; ?>
+            </div>
+            <?php endif; ?>
         </div>
     </div>
     <?php
