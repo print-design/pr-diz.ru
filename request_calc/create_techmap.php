5d4a1ad0d3172876eee8ff7cdc626a8e15055714
user <user@Dmitry>
2021-12-09 10:35:55 +0300

    9.12.2021 калькулятор новая версия

--- request_calc/create_techmap.php
+++ request_calc/create_techmap.php
@@ -19,16 +19,43 @@ if(empty($id)) {
     $id = filter_input(INPUT_GET, 'id');
 }
 
-$sql = "select c.date, c.name, "
-        . "cus.name customer "
+$sql = "select c.date, c.name, c.quantity, c.unit, c.stream_width, c.brand_name, c.thickness, c.individual_brand_name, c.individual_thickness, "
+        . "c.lamination1_brand_name, c.lamination1_thickness, c.lamination1_individual_brand_name, c.lamination1_individual_thickness, "
+        . "c.lamination2_brand_name, c.lamination2_thickness, c.lamination2_individual_brand_name, c.lamination2_individual_thickness, "
+        . "cus.name customer, wt.name work_type, u.first_name, u.last_name "
         . "from request_calc c "
         . "inner join customer cus on c.customer_id=cus.id "
+        . "inner join work_type wt on c.work_type_id = wt.id "
+        . "inner join user u on c.manager_id = u.id "
         . "where c.id=$id";
 $row = (new Fetcher($sql))->Fetch();
 
 $date = $row['date'];
 $name = $row['name'];
+$quantity = $row['quantity'];
+$unit = $row['unit'];
+$stream_width = $row['stream_width'];
+
+$brand_name = $row['brand_name'];
+$thickness = $row['thickness'];
+$individual_brand_name = $row['individual_brand_name'];
+$individual_thickness = $row['individual_thickness'];
+$lamination1_brand_name = $row['lamination1_brand_name'];
+$lamination1_thickness = $row['lamination1_thickness'];
+$lamination1_individual_brand_name = $row['lamination1_individual_brand_name'];
+$lamination1_individual_thickness = $row['lamination1_individual_thickness'];
+$lamination2_brand_name = $row['lamination2_brand_name'];
+$lamination2_thickness = $row['lamination2_thickness'];
+$lamination2_individual_brand_name = $row['lamination2_individual_brand_name'];
+$lamination2_individual_thickness = $row['lamination2_individual_thickness'];
+
+$lamination1_brand_name = $row['lamination1_brand_name'];
+$lamination2_brand_name = $row['lamination2_brand_name'];
+
 $customer = $row['customer'];
+$work_type = $row['work_type'];
+$first_name = $row['first_name'];
+$last_name = $row['last_name'];
 ?>
 <!DOCTYPE html>
 <html>
@@ -36,6 +63,46 @@ $customer = $row['customer'];
         <?php
         include '../include/head.php';
         ?>
+        <style>
+            #title_qr {
+                float: left;
+                margin-right: 40px;
+            }
+            
+            #title_customer {
+                font-size: 36px;
+                font-weight: 700;
+                line-height: 44px;
+                margin-bottom: 10px;
+            }
+            
+            #title_name {
+                font-size: 24px;
+                font-weight: 700;
+                line-height: 40px;
+                margin-bottom: 10px;
+            }
+            
+            #title_date {
+                font-size: 18px;
+                font-weight: 700;
+                line-height: 32px;
+            }
+            
+            #params_top {
+                margin-top: 40px;
+                margin-bottom: 40px;
+            }
+            
+            #params_top table tr th {
+                padding-right: 20px;
+                padding-bottom: 10px;
+            }
+            
+            #params_top table tr td {
+                padding-bottom: 10px;
+            }
+        </style>
     </head>
     <body>
         <?php
@@ -49,19 +116,133 @@ $customer = $row['customer'];
             ?>
             <a class="btn btn-outline-dark backlink" href="request_calc.php?id=<?= $id ?>">Назад</a>
             <div id="title_zone">
-                <?php
-                $errorCorrectionLevel = 'M'; // 'L','M','Q','H'
-                $data = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].APPLICATION.'/request_calc/request_calc.php?id='.$id;
-                $filename = "../temp/techmap".$id."_".$current_date_time.".png";
+                <div id="title_qr">
+                    <?php
+                    $errorCorrectionLevel = 'L'; // 'L','M','Q','H'
+                    $data = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['HTTP_HOST'].APPLICATION.'/request_calc/request_calc.php?id='.$id;
+                    $filename = "../temp/techmap".$id."_".$current_date_time.".png";
                 
-                do {
-                    QRcode::png(addslashes($data), $filename, $errorCorrectionLevel, 10, 4, true);
-                } while (!file_exists($filename));
+                    do {
+                        QRcode::png(addslashes($data), $filename, $errorCorrectionLevel, 20, 0, true);
+                    } while (!file_exists($filename));
+                    ?>
+                    <img src='<?=$filename ?>' style="height: 136px; width: 136px;" />
+                    <!--img src='<?=$filename ?>' /-->
+                </div>
+                <div id="title_text">
+                    <div id="title_customer"><?=$customer ?></div>
+                    <div id="title_name"><?=$name ?></div>
+                    <div id="title_date">№<?=$id ?> от <?= DateTime::createFromFormat('Y-m-d H:i:s', $date)->format('d.m.Y') ?></div>
+                </div>
+            </div>
+            <div id="params_top">
+                <table>
+                    <tr>
+                        <th>Объем заказа</th>
+                        <td><?=$quantity.' '.($unit == 'kg' ? 'кг' : 'шт') ?></td>
+                    </tr>
+                    <tr>
+                        <th>Менеджер</th>
+                        <td><?=$first_name.' '.$last_name ?></td>
+                    </tr>
+                    <tr>
+                        <th>Тип работы</th>
+                        <td><?=$work_type ?></td>
+                    </tr>
+                    <tr>
+                        <th>Карта составлена</th>
+                        <td>
+                            <?php
+                            $current_date_time = date("d.m.Y H:i");
+                            ?>
+                            <?=$current_date_time ?>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <hr />
+            <div class="row params_main">
+                <div class="col-3">
+                    <div class="table_title">Пленка</div>
+                    <table>
+                        <tr>
+                            <th>Марка пленки</th>
+                            <td><?=($brand_name == INDIVIDUAL ? $individual_brand_name : $brand_name) ?></td>
+                        </tr>
+                        <tr>
+                            <th>Толщина</th>
+                            <td><?=($brand_name == INDIVIDUAL ? $individual_thickness : $thickness) ?> мкм</td>
+                        </tr>
+                        <tr>
+                            <th>Ширина</th>
+                            <td><?=$stream_width ?> мм</td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на приладку</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на тираж</th>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+                <?php if(!empty($lamination1_brand_name)): ?>
+                <div class="col-3">
+                    <div class="table_title">Ламинация 1</div>
+                    <table>
+                        <tr>
+                            <th>Марка пленки</th>
+                            <td><?=($lamination1_brand_name == INDIVIDUAL ? $lamination1_individual_brand_name : $lamination1_brand_name) ?></td>
+                        </tr>
+                        <tr>
+                            <th>Толщина</th>
+                            <td><?=($lamination1_brand_name == INDIVIDUAL ? $lamination1_individual_thickness : $lamination1_thickness) ?> мкм</td>
+                        </tr>
+                        <tr>
+                            <th>Ширина</th>
+                            <td><?=$stream_width ?> мм</td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на приладку</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на тираж</th>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+                <?php
+                endif;
+                if(!empty($lamination2_brand_name)):
                 ?>
-                <img src='<?=$filename ?>' style='height: 800px; width: 800px;' />
-                <p><?=$customer ?></p>
-                <p><?=$name ?></p>
-                <p>№<?=$id ?> от <?= DateTime::createFromFormat('Y-m-d H:i:s', $date)->format('d.m.Y') ?></p>
+                <div class="col-3">
+                    <div class="table_title">Ламинация 2</div>
+                    <table>
+                        <tr>
+                            <th>Марка пленки</th>
+                            <td><?=($lamination2_brand_name == INDIVIDUAL ? $lamination2_individual_brand_name : $lamination2_brand_name) ?></td>
+                        </tr>
+                        <tr>
+                            <th>Толщина</th>
+                            <td><?=($lamination2_brand_name == INDIVIDUAL ? $lamination2_individual_thickness : $lamination2_thickness) ?> мкм</td>
+                        </tr>
+                        <tr>
+                            <th>Ширина</th>
+                            <td><?=$stream_width ?> мм</td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на приладку</th>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <th>Метраж на тираж</th>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+                <?php endif; ?>
             </div>
         </div>
         <?php
