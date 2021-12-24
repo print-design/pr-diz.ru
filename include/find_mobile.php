ea0d69896d6010c7ed847f8645bf0b823f1dcfca
user <user@Dmitry>
2021-12-24 09:26:05 +0300

    24.12.2021 убираем QR-код

--- include/find_mobile.php
+++ include/find_mobile.php
@@ -49,7 +49,7 @@ elseif (IsInRole(array('cutter'))) {
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                Штрих-код / QR-код
+                Штрих-код
                 <button type="button" class="close" data-dismiss="modal" id="close_video"><i class="fas fa-times"></i></button>
             </div>
             <div class="modal-body">
