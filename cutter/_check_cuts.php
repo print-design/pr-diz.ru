e2f21612275f5bffba2dca2cfd48983f0acb490d
user <user@Dmitry>
2022-01-12 15:57:35 +0300

    12.01.2022 резчик раскрой

--- cutter/_check_cuts.php
+++ cutter/_check_cuts.php
@@ -2,20 +2,24 @@
 function CheckCuts($user_id) {
     if(!($_SERVER['PHP_SELF'] == APPLICATION.'/cutter/material.php' && !empty(filter_input(INPUT_GET, 'cutting_id')))) {
         $id = null;
-        $source_is_from_pallet = null;
-        $source_id = null;
+        $is_from_pallet = null;
+        $roll_id = null;
         
-        $sql = "select id, source_is_from_pallet, source_id from cutting where date is null and cutter_id = $user_id";
+        $sql = "select c.id, cs.is_from_pallet, cs.roll_id "
+                . "from cutting c "
+                . "left join cutting_source cs on cs.cutting_id = c.id "
+                . "where c.date is null and c.cutter_id = $user_id "
+                . "order by cs.id desc";
         $fetcher = new Fetcher($sql);
         if($row = $fetcher->Fetch()) {
             $id = $row['id'];
-            $source_is_from_pallet = $row['source_is_from_pallet'];
-            $source_id = $row['source_id'];
+            $is_from_pallet = $row['is_from_pallet'];
+            $roll_id = $row['roll_id'];
         }
         
         // Если есть незакрытая заявка и нет ни одного исходного ролика, переводим на страницу создания исходного ролика
         if(!empty($id)) {
-            if((empty($source_is_from_pallet) || empty($source_id)) && 
+            if((empty($is_from_pallet) || empty($roll_id)) && 
                     $_SERVER['PHP_SELF'] != APPLICATION.'/cutter/source.php' && 
                     $_SERVER['PHP_SELF'] != APPLICATION.'/cutter/logout.php') {
                 header("Location: ".APPLICATION.'/cutter/source.php?cutting_id='.$id);
