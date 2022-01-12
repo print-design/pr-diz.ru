2c413469c3d62a871603040167feb45176cdf823
user <user@Dmitry>
2022-01-12 11:39:27 +0300

    12.01.2022 резчик раскрой

---
+++ cutter/_check_cuts.php
@@ -0,0 +1,5 @@
+<?php
+function CheckCuts($user_id) {
+    //
+}
+?>
---
+++ cutter/index.php
@@ -0,0 +1,52 @@
+<?php
+include_once '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('technologist', 'dev', 'cutter'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+// Текущий пользователь
+$user_id = GetUserId();
+
+// Проверяем, имеются ли незакрытые нарезки
+include '_check_cuts.php';
+CheckCuts($user_id);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        include '_head.php';
+        ?>
+    </head>
+    <body>
+        <div class="container-fluid header">
+            <nav class="navbar navbar-expand-sm justify-content-between">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Склад</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown no-dropdown-arrow-after">
+                        <a class="nav-link mr-0" id="logout-submit" href="logout.php?link=<?= urlencode($_SERVER['REQUEST_URI']) ?>"><i class="fa fa-user-alt" aria-hidden="true""></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div id="topmost"></div>
+        <div class="container-fluid">
+            <?php
+            if(!empty($error_message)) {
+                echo "<div class='alert alert-danger'>$error_message</div>";
+            }
+            ?>
+            <a class="btn btn-dark w-100 mt-4" href="material.php">Приступить к раскрою</a>
+        </div>
+        <?php
+        include '_footer.php';
+        ?>
+    </body>
+</html>
