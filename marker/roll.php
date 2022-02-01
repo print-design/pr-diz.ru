c9cb340165026e8cc6769d711c2b4c3c01174198
user <user@Dmitry>
2022-02-01 16:05:29 +0300

    01.02.2022 маркиратор

--- marker/roll.php
+++ marker/roll.php
@@ -9,6 +9,9 @@ if(!IsInRole(array('technologist', 'dev', 'marker'))) {
 // Текущий пользователь
 $user_id = GetUserId();
 
+// СТАТУС "СВОБОДНЫЙ"
+const FREE_ROLL_STATUS_ID = 1;
+
 // Валидация формы
 define('ISINVALID', ' is-invalid');
 $form_valid = true;
@@ -19,9 +22,79 @@ $film_brand_id_valid = '';
 $thickness_valid = '';
 $width_valid = '';
 $radius_valid = '';
+$cell_valid = '';
 
 if(null !== filter_input(INPUT_POST, 'create-submit')) {
-    print_r($_POST);
+    $supplier_id = filter_input(INPUT_POST, 'supplier_id');
+    if(empty($supplier_id)) {
+        $supplier_id_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $film_brand_id = filter_input(INPUT_POST, 'film_brand_id');
+    if(empty($film_brand_id)) {
+        $film_brand_id_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $thickness = filter_input(INPUT_POST, 'thickness');
+    if(empty($thickness)) {
+        $thickness_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $width = filter_input(INPUT_POST, 'width');
+    if(empty($width)) {
+        $width_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $radius = filter_input(INPUT_POST, 'radius');
+    if(empty($radius)) {
+        $radius_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $cell = addslashes(filter_input(INPUT_POST, 'cell'));
+    if(empty($cell)) {
+        $cell_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
+    $length = filter_input(INPUT_POST, 'length');
+    if(empty($length)) {
+        $error_message = "Длина обязательно";
+        $form_valid = false;
+    }
+    
+    $net_weight = filter_input(INPUT_POST, 'net_weight');
+    if(empty($net_weight)) {
+        $error_message = "Масса нетто обязательно";
+        $form_valid = false;
+    }
+    
+    $id_from_supplier = rand(1000000, 9999999);
+    $comment = '';
+    $storekeeper_id = $user_id;
+    $status_id = FREE_ROLL_STATUS_ID;
+    
+    if($form_valid) {    
+        $sql = "insert into roll (supplier_id, id_from_supplier, film_brand_id, width, thickness, length, net_weight, cell, comment, storekeeper_id) "
+                . "values ($supplier_id, '$id_from_supplier', $film_brand_id, $width, $thickness, $length, $net_weight, '$cell', '$comment', '$storekeeper_id')";
+        $executer = new Executer($sql);
+        $error_message = $executer->error;
+        $roll_id = $executer->insert_id;
+        
+        if(empty($error_message)) {
+            $sql = "insert into roll_status_history (roll_id, status_id, user_id) values ($roll_id, $status_id, $user_id)";
+            $executer = new Executer($sql);
+            $error_message = $executer->error;
+            
+            if(empty($error_message)) {
+                header('Location: print.php');
+            }
+        }
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -58,6 +131,8 @@ if(null !== filter_input(INPUT_POST, 'create-submit')) {
             <div class="row">
                 <div class="col-12 col-md-6 col-lg-4">
                     <form method="post">
+                        <input type="hidden" id="length" name="length" />
+                        <input type="hidden" id="net_weight" name="net_weight" />
                         <div class="form-group">
                             <label for="supplier_id">Поставщик</label>
                             <select class="form-control<?=$supplier_id_valid ?>" id="supplier_id" name="supplier_id" required="required">
@@ -152,6 +227,11 @@ if(null !== filter_input(INPUT_POST, 'create-submit')) {
                             <input type="text" id="radius" name="radius" value="<?= filter_input(INPUT_POST, 'radius') ?>" class="form-control int-only<?=$radius_valid ?>" placeholder="Введите радиус от вала" required="required" autocomplete="off" />
                             <div class="invalid-feedback">Радиус от вала обязательно</div>
                         </div>
+                        <div class="form-group">
+                            <label type="cell">Ячейка на складе</label>
+                            <input type="text" id="cell" name="cell" value="<?= filter_input(INPUT_POST, 'cell') ?>" class="form-control<?=$cell_valid ?>" placeholder="Введите ячейку на складе" required="required" autocomplete="off" />
+                            <div class="invalid-feedback">Ячейка на складе обязательно</div>
+                        </div>
                         <div class="form-group d-none d-lg-block">
                             <button type="submit" id="create-submit" name="create-submit" class="btn btn-dark form-control mt-4">Распечатать</button>
                         </div>
@@ -201,7 +281,7 @@ if(null !== filter_input(INPUT_POST, 'create-submit')) {
     
             // Позиционируем кнопку "Далее" относительно нижнего края экрана только если она не перекроет другие элементы
             function AdjustButtons() {
-                if($('#radius').offset().top + $('#bottom_buttons').outerHeight() + 50 < $(window).height()) {
+                if($('#cell').offset().top + $('#bottom_buttons').outerHeight() + 50 < $(window).height()) {
                     $('#bottom_buttons').removeClass('sticky-top');
                     $('#bottom_buttons').addClass('fixed-bottom');
                     $('#bottom_buttons').addClass('container-fluid');
@@ -213,7 +293,43 @@ if(null !== filter_input(INPUT_POST, 'create-submit')) {
                 }
             }
             
-            $(document).ready(AdjustButtons);
+            // Все марки плёнки с их вариациями
+            var films = new Map();
+            
+            <?php
+            $sql = "SELECT fbv.film_brand_id, fbv.thickness, fbv.weight FROM film_brand_variation fbv";
+            $fetcher = new Fetcher($sql);
+            while ($row = $fetcher->Fetch()) {
+                echo "if(films.get(".$row['film_brand_id'].") == undefined) {\n";
+                echo "films.set(".$row['film_brand_id'].", new Map());\n";
+                echo "}\n";
+                echo "films.get(".$row['film_brand_id'].").set(".$row['thickness'].", ".$row['weight'].");\n";
+            }
+            ?>
+            
+            // Расчёт длины и массы плёнки по шпуле, толщине, радиусу, ширине, удельному весу
+            function CalculateByRadius() {
+                film_brand_id = $('#film_brand_id').val();
+                spool = $('#spool:checked').val();
+                thickness = $('#thickness').val();
+                width = $('#width').val();
+                radius = $('#radius').val();
+                
+                if(!isNaN(spool) && !isNaN(thickness) && !isNaN(radius) && !isNaN(width) 
+                        && spool != '' && thickness != '' && radius != '' && width != '') {
+                    density = films.get(parseInt($('#film_brand_id').val())).get(parseInt(thickness));
+                    
+                    result = GetFilmLengthWeightBySpoolThicknessRadiusWidth(spool, thickness, radius, width, density);
+                    
+                    $('#length').val(result.length.toFixed(2));
+                    $('#net_weight').val(result.weight.toFixed(2));
+                }
+            }
+            
+            $(document).ready(function() { 
+                AdjustButtons();
+                CalculateByRadius();
+            });
             
             $(window).on('resize', AdjustButtons);
         </script>
