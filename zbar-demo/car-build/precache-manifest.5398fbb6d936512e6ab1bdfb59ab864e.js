8e0c16230b34fb8d3bf451797fb04c522b414641
user <user@Dmitry>
2022-02-14 08:34:00 +0300

    14.02.2022 grafik

---
+++ zbar-demo/car-build/precache-manifest.5398fbb6d936512e6ab1bdfb59ab864e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1802d9d0009454fb6d22d8f7bb6de909",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "96c56415ed8c268de2be",
+    "url": "/demo/static/js/2.626bd5f9.chunk.js"
+  },
+  {
+    "revision": "6fb56fcdb7a4abbcc388",
+    "url": "/demo/static/js/main.9a176a25.chunk.js"
+  },
+  {
+    "revision": "291c6bec69179f1e0dca",
+    "url": "/demo/static/js/runtime-main.134dfdfb.js"
+  },
+  {
+    "revision": "5f90df2200eb4175f234046e0f31df9a",
+    "url": "/demo/static/media/zbar.wasm.5f90df22.bin"
+  }
+]);
