397e4299d016f772f1eefefd05ed9d6d49c0c0f8
user <user@Dmitry>
2022-02-21 09:13:18 +0300

    21.02.2022 grafik class

--- grafik/include/grafik_print_shift.php
+++ grafik/include/grafik_print_shift.php
@@ -3,7 +3,7 @@
     <td class="<?=$top ?>" rowspan="2"><?=$GLOBALS['weekdays'][$this->date->format('w')] ?></td>
     <td class="<?=$top ?>" rowspan="2"><?=$this->date->format('d.m.Y') ?></td>
     <?php endif; ?>
-    <td class="<?=$top ?>"><?=($dateshift['shift'] == 'day' ? 'День' : 'Ночь') ?></td>
+    <td class="<?=$top.' '.$this->shift ?>"><?=($this->shift == 'day' ? 'День' : 'Ночь') ?></td>
     
     <!-- Работник №1 -->
     <?php if($this->machine->user1Name != ''): ?>
@@ -18,4 +18,28 @@
         <?php echo (array_key_exists('u2_fio', $this->shift_data) ? $this->shift_data['u2_fio'] : ''); ?>
     </td>
     <?php endif; ?>
+    
+    <!-- Заказчик -->
+    <?php if($this->machine->hasOrganization): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Наименование -->
+    <?php if($this->machine->hasEdition): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Метраж -->
+    <?php if($this->machine->hasLength): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Вал -->
+    <?php if($this->machine->hasRoller): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Ламинация -->
+    <?php if($this->machine->hasLamination): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Красочность -->
+    <?php if($this->machine->hasColoring): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Менеджер -->
+    <?php if($this->machine->hasManager): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
+    
+    <!-- Комментарий -->
+    <?php if($this->machine->hasComment): ?><td class="<?=$top.' '.$this->shift ?>"></td><?php endif; ?>
 </tr>
