71b8c7d349f921edffbb269f32cd54e8f5814ec0
user <user@Dmitry>
2022-02-21 11:00:18 +0300

    21.02.2022 grafik class

--- grafik/include/grafik_select_user1.php
+++ grafik/include/grafik_select_user1.php
@@ -1,4 +1,4 @@
-<select id='user1_id' name='user1_id' style='width:100px;' onchange='javascript: EditUser1($(this))' data-id='<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>' data-date='<?=$this->date->format('Y-m-d') ?>' data-shift='<?=$this->shift ?>' data-machine='<?=$this->machine->machineId ?>' data-from='<?=$this->machine->dateFrom->format('Y-m-d') ?>' data-to='<?=$this->machine->dateTo->format('Y-m-d') ?>'>
+<select id='user1_id' name='user1_id' style='width:100px;' onchange='javascript: EditUser1($(this))' data-id='<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>' data-date='<?=$this->date->format('Y-m-d') ?>' data-shift='<?=$this->shift ?>' data-machine='<?=$this->timetable->machineId ?>' data-from='<?=$this->timetable->dateFrom->format('Y-m-d') ?>' data-to='<?=$this->timetable->dateTo->format('Y-m-d') ?>'>
     <optgroup>
         <option value="">...</option>
         <?php
@@ -16,6 +16,6 @@
         
 <div class="input-group d-none">
     <input type="text" id="user1" name="user1" value="" class="editable" />
-    <div class="input-group-append"><button type="button" class="btn btn-outline-dark" onclick="javascript: CreateUser1($(this));" data-id="<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>" role_id="<?=$this->machine->userRole ?>" data-date="<?=$this->date->format('Y-m-d') ?>" data-shift="<?=$this->shift ?>" data-machine="<?=$this->machine->machineId ?>" data-from="<?=$this->machine->dateFrom->format('Y-m-d') ?>" data-to="<?=$this->machine->dateTo->format('Y-m-d') ?>"><i class="fas fa-save"></i></button></div>
+    <div class="input-group-append"><button type="button" class="btn btn-outline-dark" onclick="javascript: CreateUser1($(this));" data-id="<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>" role_id="<?=$this->timetable->userRole ?>" data-date="<?=$this->date->format('Y-m-d') ?>" data-shift="<?=$this->shift ?>" data-machine="<?=$this->timetable->machineId ?>" data-from="<?=$this->timetable->dateFrom->format('Y-m-d') ?>" data-to="<?=$this->timetable->dateTo->format('Y-m-d') ?>"><i class="fas fa-save"></i></button></div>
     <div class="input-group-append"><button type="button" class="btn btn-outline-dark" data-user1="<?=(isset($this->shift_data['u1_id']) ? $this->shift_data['u1_id'] : '') ?>" onclick="javascript: CancelCreateUser1($(this));"><i class="fas fa-window-close"></i></button></div>
 </div>
--- grafik/include/grafik_select_user2.php
+++ grafik/include/grafik_select_user2.php
@@ -1,4 +1,4 @@
-<select id='user2_id' name='user2_id' style='width:100px;' onchange='javascript: EditUser2($(this))' data-id='<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>' data-date='<?=$this->date->format('Y-m-d') ?>' data-shift='<?=$this->shift ?>' data-machine='<?=$this->machine->machineId ?>' data-from='<?=$this->machine->dateFrom->format('Y-m-d') ?>' data-to='<?=$this->machine->dateTo->format('Y-m-d') ?>'>
+<select id='user2_id' name='user2_id' style='width:100px;' onchange='javascript: EditUser2($(this))' data-id='<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>' data-date='<?=$this->date->format('Y-m-d') ?>' data-shift='<?=$this->shift ?>' data-machine='<?=$this->timetable->machineId ?>' data-from='<?=$this->timetable->dateFrom->format('Y-m-d') ?>' data-to='<?=$this->timetable->dateTo->format('Y-m-d') ?>'>
     <optgroup>
         <option value="">...</option>
         <?php
@@ -16,6 +16,6 @@
                             
 <div class="input-group d-none">
     <input type="text" id="user2" name="user2" value="" class="editable" />
-    <div class="input-group-append"><button type="button" class="btn btn-outline-dark" onclick="javascript: CreateUser2($(this));" data-id="<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>" role_id="<?=$this->machine->userRole ?>" data-date="<?=$this->date->format('Y-m-d') ?>" data-shift="<?=$this->shift ?>" data-machine="<?=$this->machine->machineId ?>" data-from="<?=$this->machine->dateFrom->format('Y-m-d') ?>" data-to="<?=$this->machine->dateTo->format('Y-m-d') ?>"><i class="fas fa-save"></i></button></div>
+    <div class="input-group-append"><button type="button" class="btn btn-outline-dark" onclick="javascript: CreateUser2($(this));" data-id="<?=(isset($this->shift_data['id']) ? $this->shift_data['id'] : '') ?>" role_id="<?=$this->timetable->userRole ?>" data-date="<?=$this->date->format('Y-m-d') ?>" data-shift="<?=$this->shift ?>" data-machine="<?=$this->timetable->machineId ?>" data-from="<?=$this->timetable->dateFrom->format('Y-m-d') ?>" data-to="<?=$this->timetable->dateTo->format('Y-m-d') ?>"><i class="fas fa-save"></i></button></div>
     <div class="input-group-append"><button type="button" class="btn btn-outline-dark" data-user2="<?=(isset($this->shift_data['u2_id']) ? $this->shift_data['u2_id'] : '') ?>" onclick="javascript: CancelCreateUser2($(this));"><i class="fas fa-window-close"></i></button></div>
 </div>
