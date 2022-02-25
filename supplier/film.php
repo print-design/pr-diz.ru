0bf70d6179931fb041b2ed401d6cf60ed49cd48c
user <user@Dmitry>
2022-02-25 14:54:27 +0300

    25.02.2022 марка пленки

--- supplier/film.php
+++ supplier/film.php
@@ -21,6 +21,7 @@ function OrderLink($param) {
         <?php
         include '../include/head.php';
         ?>
+        <link href="<?=APPLICATION ?>/css/select2.min.css" rel="stylesheet"/>
         <style>
             h2 {
                 margin-top: 20px;
@@ -49,24 +50,26 @@ function OrderLink($param) {
         <?php
         include '../include/header_admin.php';
         ?>
-        <div id="create_film_variation" class="modal fade show">
+        <div id="create_film_brand_variation" class="modal fade show">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <form method="post">
                         <div class="modal-header">
                             <p class="font-weight-bold" style="font-size: x-large;">Добавить пленку</p>
-                            <button type="button" class="close user_change_password_dismiss" data-dismiss="modal"><i class="fas fa-times"></i></button>
+                            <button type="button" class="close create_film_brand_variation_dismiss" data-dismiss="modal"><i class="fas fa-times" style="color: #EC3A7A;"></i></button>
                         </div>
                         <div class="modal-body">
                             <div class="form-group">
-                                <select name="film_brand" class="form-control">
+                                <select class="form-control" name="film_brand" id="film_brand">
                                     <option value="" hidden="hidden">Марка пленки</option>
                                     <option>HDPL</option>
                                     <option>HMIL.M</option>
                                     <option>HOHL</option>
                                     <option>HWHL</option>
                                     <option>LOBA</option>
-                                    <option>+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Новая марка</option>
+                                    <optgroup>
+                                        <option value="+">+&nbsp;Новая марка</option>
+                                    </optgroup>
                                 </select>
                             </div>
                             <div class="row">
@@ -84,7 +87,28 @@ function OrderLink($param) {
                         </div>
                         <div class="modal-footer" style="justify-content: flex-start;">
                             <button type="submit" class="btn btn-dark" id="create_film_variation_submit" name="create_film_variation_submit">Добавить</button>
-                            <button type="button" class="btn btn-light user_change_password_dismiss" data-dismiss="modal">Отменить</button>
+                            <button type="button" class="btn btn-light create_film_brand_variation_dismiss" data-dismiss="modal">Отменить</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div id="create_film_brand" class="modal fade show">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <form method="post">
+                        <div class="modal-header">
+                            <p class="font-weight-bold" style="font-size: x-large;">Введите марку пленки</p>
+                            <button type="button" class="close create_film_brand_dismiss" data-dismiss="modal"><i class="fas fa-times" style="color: #EC3A7A;"></i></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <input type="text" name="film_brand" id="film_brand" class="form-control" placeholder="Марка пленки" />
+                            </div>
+                        </div>
+                        <div class="modal-footer" style="justify-content: flex-start;">
+                            <button type="submit" class="btn btn-dark" id="create_film_brand" name="create_film_brand">Добавить</button>
+                            <button type="button" class="btn btn-light create_film_brand_dismiss" data-dismiss="modal">Отменить</button>
                         </div>
                     </form>
                 </div>
@@ -101,7 +125,7 @@ function OrderLink($param) {
                     <h1>Пленка</h1>
                 </div>
                 <div class="p-0">
-                    <button class="btn btn-dark" data-toggle="modal" data-target="#create_film_variation">
+                    <button class="btn btn-dark" data-toggle="modal" data-target="#create_film_brand_variation">
                         <i class="fas fa-plus"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Добавить тип пленки
                     </button>
                 </div>
@@ -179,5 +203,27 @@ function OrderLink($param) {
         <?php
         include '../include/footer.php';
         ?>
+        <script>
+            $('#film_brand').change(function() {
+                if($(this).val() == '+') {
+                    $('#create_film_brand_variation').modal('hide');
+                }
+            });
+            
+            $('#create_film_brand').on('hidden.bs.modal', function() {
+                $('#film_brand').val('');
+                $('#create_film_brand_variation').modal('show');
+            });
+            
+            $('#create_film_brand_variation').on('hidden.bs.modal', function() {
+                if($('#film_brand').val() == '+') {
+                    $('#create_film_brand').modal('show');
+                }
+            });
+            
+            $('#create_film_brand').on('shown.bs.modal', function() {
+                $('input:text:visible:first').focus();
+            });
+        </script>
     </body>
 </html>
