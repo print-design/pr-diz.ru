1b4951c9e945ec57517bba43d8646a3bbe36a961
user <user@Dmitry>
2022-03-03 14:32:38 +0300

    03.03.2022 миграция

--- ajax/thickness.php
+++ ajax/thickness.php
@@ -6,12 +6,13 @@ $film_id = filter_input(INPUT_GET, 'film_id');
 
 if(!empty($film_id)) {
     echo "<option value='' hidden='hidden' selected='selected'>Выберите толщину</option>";
-    $grabber = (new Grabber("select thickness, weight from film_variation where film_id = $film_id order by thickness"))->result;
+    $grabber = (new Grabber("select id, thickness, weight from film_variation where film_id = $film_id order by thickness"))->result;
     
     foreach ($grabber as $row) {
+        $film_variation_id = intval($row['id']);
         $thickness = intval($row['thickness']);
         $weight = floatval($row['weight']);
-        echo "<option value='$thickness'>$thickness мкм $weight г/м<sup>2</sup></option>";
+        echo "<option value='$film_variation_id'>$thickness мкм $weight г/м<sup>2</sup></option>";
     }
 }
 
@@ -34,9 +35,10 @@ $film_name = addslashes(filter_input(INPUT_GET, 'film_name'));
 
 if(!empty($film_name)) {
     echo "<option value='' hidden='hidden' selected='selected'>Толщина...</option>";
-    $grabber = (new Grabber("select distinct fv.thickness, fv.weight from film_variation fv inner join film f on fv.film_id = f.id where f.name='$film_name' order by thickness"))->result;
+    $grabber = (new Grabber("select distinct fv.id, fv.thickness, fv.weight from film_variation fv inner join film f on fv.film_id = f.id where f.name='$film_name' order by thickness"))->result;
     
     foreach ($grabber as $row) {
+        $film_variation_id = intval($row['id']);
         $thickness = intval($row['thickness']);
         $weight = floatval($row['weight']);
         echo "<option value='$thickness'>$thickness мкм $weight г/м<sup>2</sup></option>";
