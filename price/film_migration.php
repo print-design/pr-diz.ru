69de2c20c94b9daab472cb0dceb82e845a633164
user <user@Dmitry>
2022-03-03 11:02:52 +0300

    03.03.2022 миграция

---
+++ price/film_migration.php
@@ -0,0 +1,72 @@
+<?php
+include '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array('technologist', 'dev', 'storekeeper', 'manager', 'administrator'))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+
+// Общего количества рулонов
+$total_count = 0;
+$sql = "select count(id) from film_price";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $total_count = $row[0];
+}
+
+// Количество мигрированных рулонов
+$ok_count = 0;
+$sql = "select count(p.id) "
+        . "from film_price p "
+        . "left join film_variation fv on p.film_variation_id = fv.id "
+        . "left join film f on fv.film_id = f.id "
+        . "where p.brand_name = f.name and p.thickness = fv.thickness";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $ok_count = $row[0];
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+        <style>
+            #progress {
+                font-family: sans-serif;
+                font-weight: bold;
+                font-size: 100px;
+            }
+        </style>
+    </head>
+    <body>
+        <?php
+        include '../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <h1>Миграция (цены)</h1>
+            <div id="progress"><?=$ok_count ?> из <?=$total_count ?></div>
+            <br /><br /><br />
+            <button type="button" class="btn btn-lg btn-primary" id="start_btn" onclick="javascript: Start(); $(this).hide();">Запуск</button>
+        </div>
+        <?php
+        include '../include/footer.php';
+        ?>
+        <script>
+            function Start() {
+                $.ajax({ url: 'film_migration_ajax.php' })
+                        .done(function(data) {
+                            $('#progress').text(data + ' из <?=$total_count ?>');
+                    
+                            if(data > 0) {
+                                Start();
+                            }
+                        })
+                        .fail(function() {
+                            $('$progress').text("Ошибка");
+                        });
+            }
+        </script>
+    </body>
+</html>
