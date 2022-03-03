caad6c23ca1ed96366de623f7a89698ab9f54fef
user <user@Dmitry>
2022-03-03 11:42:24 +0300

    03.03.2022 миграция

--- request_calc/film_migration.php
+++ request_calc/film_migration.php
@@ -14,6 +14,20 @@ if($row = $fetcher->Fetch()) {
     $total_count = $row[0];
 }
 
+$total_count_1 = 0;
+$sql = "select count(id) from calculation where lamination1_brand_name != 'other' and lamination1_brand_name is not null and lamination1_brand_name != ''";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher ->Fetch()) {
+    $total_count_1 = $row[0];
+}
+
+$total_count_2 = 0;
+$sql = "select count(id) from calculation where lamination2_brand_name != 'other' and lamination2_brand_name is not null and lamination2_brand_name != ''";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $total_count_2 = $row[0];
+}
+
 // Количество мигрированных рулонов
 $ok_count = 0;
 $sql = "select count(c.id) "
@@ -25,6 +39,28 @@ $fetcher = new Fetcher($sql);
 if($row = $fetcher->Fetch()) {
     $ok_count = $row[0];
 }
+
+$ok_count_1 = 0;
+$sql = "select count(c.id) "
+        . "from calculation c "
+        . "left join film_variation fv on c.lamination1_film_variation_id = fv.id "
+        . "left join film f on fv.film_id = f.id "
+        . "where c.lamination1_brand_name = f.name and c.lamination1_thickness = fv.thickness";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $ok_count_1 = $row[0];
+}
+
+$ok_count_2 = 0;
+$sql = "select count(c.id) "
+        . "from calculation c "
+        . "left join film_variation fv on c.lamination2_film_variation_id = fv.id "
+        . "left join film f on fv.film_id = f.id "
+        . "where c.lamination2_brand_name = f.name and lamination2_thickness = fv.thickness";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $ok_count_2 = $row[0];
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -33,7 +69,7 @@ if($row = $fetcher->Fetch()) {
         include '../include/head.php';
         ?>
         <style>
-            #progress {
+            #progress, #progress_1, #progress_2 {
                 font-family: sans-serif;
                 font-weight: bold;
                 font-size: 100px;
@@ -48,7 +84,15 @@ if($row = $fetcher->Fetch()) {
             <h1>Миграция (расчёты)</h1>
             <div id="progress"><?=$ok_count ?> из <?=$total_count ?></div>
             <br /><br /><br />
-            <button type="button" class="btn btn-lg btn-primary" id="start_btn" onclick="javascript: Start(); $(this).hide();">Запуск</button>
+            <button type="button" class="btn btn-primary" onclick="javascript: Start(); $(this).hide();">Запуск</button>
+            <br /><br /><br />
+            <div id="progress_1"><?=$ok_count_1 ?> из <?=$total_count_1 ?></div>
+            <br /><br /><br />
+            <button type="button" class="btn btn-primary" onclick="javascript: Start_1(); $(this).hide();">Запуск</button>
+            <br /><br /><br />
+            <div id="progress_2"><?=$ok_count_2 ?> из <?=$total_count_2 ?></div>
+            <br /><br /><br />
+            <button type="button" class="btn btn-primary" onclick="javascript: Start_2(); $(this).hide();">Запуск</button>
         </div>
         <?php
         include '../include/footer.php';
@@ -65,6 +109,34 @@ if($row = $fetcher->Fetch()) {
                         })
                         .fail(function() {
                             $('$progress').text("Ошибка");
+                        });
+            }
+            
+            function Start_1() {
+                $.ajax({ url: 'film_migration_ajax_1.php' })
+                        .done(function(data) {
+                            $('#progress_1').text(data + ' из <?=$total_count_1 ?>');
+                    
+                            if(data > 0) {
+                                Start_1();
+                            }
+                        })
+                        .fail(function() {
+                            $('$progress_1').text("Ошибка");
+                        });
+            }
+            
+            function Start_2() {
+                $.ajax({ url: 'film_migration_ajax_2.php' })
+                        .done(function(data) {
+                            $('#progress_2').text(data + ' из <?=$total_count_2 ?>');
+                    
+                            if(data > 0) {
+                                Start_2();
+                            }
+                        })
+                        .fail(function() {
+                            $('$progress_2').text("Ошибка");
                         });
             }
         </script>
--- request_calc/film_migration_index.php
+++ request_calc/film_migration_index.php
@@ -18,33 +18,97 @@ if(!IsInRole(array('technologist', 'dev', 'storekeeper', 'manager', 'administrat
         include '../include/header_sklad.php';
         ?>
         <div class="container-fluid">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Марка</th>
-                        <th>Марка старая</th>
-                        <th>Толщина</th>
-                        <th>Толщина старая</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $sql = "select c.id, f.name film, c.brand_name film_old, fv.thickness, c.thickness thickness_old "
-                            . "from calculation c "
-                            . "left join film_variation fv on c.film_variation_id = fv.id "
-                            . "left join film f on fv.film_id = f.id where c.brand_name != 'other'";
-                    $fetcher = new Fetcher($sql);
-                    while ($row = $fetcher->Fetch()):
-                    ?>
-                    <tr>
-                        <td><?=$row['film'] ?></td>
-                        <td><?=$row['film_old'] ?></td>
-                        <td><?=$row['thickness'] ?></td>
-                        <td><?=$row['thickness_old'] ?></td>
-                    </tr>
-                    <?php endwhile; ?>
-                </tbody>
-            </table>
+            <div class="row">
+                <div class="col-4">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Марка</th>
+                                <th>Марка старая</th>
+                                <th>Толщина</th>
+                                <th>Толщина старая</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "select c.id, f.name film, c.brand_name film_old, fv.thickness, c.thickness thickness_old "
+                                    . "from calculation c "
+                                    . "left join film_variation fv on c.film_variation_id = fv.id "
+                                    . "left join film f on fv.film_id = f.id where c.brand_name != 'other'";
+                            $fetcher = new Fetcher($sql);
+                            while ($row = $fetcher->Fetch()):
+                            ?>
+                            <tr>
+                                <td><?=$row['film'] ?></td>
+                                <td><?=$row['film_old'] ?></td>
+                                <td><?=$row['thickness'] ?></td>
+                                <td><?=$row['thickness_old'] ?></td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-4">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Марка</th>
+                                <th>Марка старая</th>
+                                <th>Толщина</th>
+                                <th>Толщина старая</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "select c.id, f.name film, c.lamination1_brand_name film_old, fv.thickness, c.lamination1_thickness thickness_old "
+                                    . "from calculation c "
+                                    . "left join film_variation fv on c.lamination1_film_variation_id = fv.id "
+                                    . "left join film f on fv.film_id = f.id "
+                                    . "where c.lamination1_brand_name != 'other' and lamination1_brand_name is not null and lamination1_brand_name != ''";
+                            $fetcher = new Fetcher($sql);
+                            while ($row = $fetcher->Fetch()):
+                            ?>
+                            <tr>
+                                <td><?=$row['film'] ?></td>
+                                <td><?=$row['film_old'] ?></td>
+                                <td><?=$row['thickness'] ?></td>
+                                <td><?=$row['thickness_old'] ?></td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-4">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>Марка</th>
+                                <th>Марка старая</th>
+                                <th>Толщина</th>
+                                <th>Толщина старая</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $sql = "select c.id, f.name film, c.lamination2_brand_name film_old, fv.thickness, c.lamination2_thickness thickness_old "
+                                    . "from calculation c "
+                                    . "left join film_variation fv on c.lamination2_film_variation_id = fv.id "
+                                    . "left join film f on fv.film_id = f.id "
+                                    . "where c.brand_name != 'other' and lamination2_brand_name is not null and lamination2_brand_name != ''";
+                            $fetcher = new Fetcher($sql);
+                            while ($row = $fetcher->Fetch()):
+                            ?>
+                            <tr>
+                                <td><?=$row['film'] ?></td>
+                                <td><?=$row['film_old'] ?></td>
+                                <td><?=$row['thickness'] ?></td>
+                                <td><?=$row['thickness_old'] ?></td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
         <?php
         include '../include/footer.php';
