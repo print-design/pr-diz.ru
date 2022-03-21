aa52c13e69e449983e9b4ca4ef57487919477abc
user <user@Dmitry>
2022-03-21 09:42:34 +0300

    21.03.2022 калькулятор

--- ajax/film_price.php
+++ ajax/film_price.php
@@ -1,30 +1,32 @@
 <?php
 include '../include/topscripts.php';
 
-$brand_name = filter_input(INPUT_GET, 'brand_name');
-$thickness = filter_input(INPUT_GET, 'thickness');
-$price = filter_input(INPUT_GET, 'price');
-$currency = filter_input(INPUT_GET, 'currency');
+$film_variation_id = filter_input(INPUT_GET, 'film_variation_id');
+$result = array("text" => "Нет данных", "currency" => "");
 
-if(!empty($brand_name) && !empty($thickness) && !empty($price) && !empty($currency)) {
-    $sql = "insert into film_price (brand_name, thickness, price, currency) values ('$brand_name', $thickness, $price, '$currency')";
-    $executer = new Executer($sql);
-    if(empty($executer->error)) {
-        $sql = "select price from film_price where brand_name='$brand_name' and thickness=$thickness order by id desc limit 1";
-        $fetcher = new Fetcher($sql);
+if(!empty($film_variation_id)) {
+    $sql = "select price, currency from film_price where film_variation_id=$film_variation_id order by id desc limit 1";
+    $fetcher = new Fetcher($sql);
             
-        if($row = $fetcher->Fetch()) {
-            echo $row[0];
+    if($row = $fetcher->Fetch()) {
+        $price_final = rtrim(rtrim(number_format($row['price'], 2, ",", " "), "0"), ",");
+        $currency_final = "";
+        switch($row['currency']) {
+            case 'rub':
+                $currency_final = "руб";
+                break;
+            
+            case 'usd':
+                $currency_final = "USD";
+                break;
+            
+            case 'euro':
+                $currency_final = "EUR";
+                break;
         }
-        else {
-            echo 0;
-        }
-    }
-    else {
-        echo 0;
+        $result = array("text" => "($price_final&nbsp;$currency_final&nbsp;&nbsp;&nbsp;34&nbsp;кг&nbsp;&nbsp;&nbsp;&nbsp;23&nbsp;000&nbsp;м)", "currency" => $row['currency']);
     }
 }
-else {
-    echo 0;
-}
+
+echo json_encode($result);
 ?>
