5c185972ce69ab2e4a9b6a842c27aa1be034d8dd
user <user@Dmitry>
2022-03-24 08:43:45 +0300

    24.03.2022 расчет

--- ajax/raport.php
+++ ajax/raport.php
@@ -6,7 +6,7 @@ $machine_id = filter_input(INPUT_GET, 'machine_id');
 echo "<option value='' hidden='hidden'>Рапорт...</option>";
 
 if(!empty($machine_id)) {
-    $sql = "select value from raport where active = 1 and machine_id = $machine_id";
+    $sql = "select value from raport where active = 1 and machine_id = $machine_id order by value";
     $grabber = new Grabber($sql);
     $result = $grabber->result;
     
