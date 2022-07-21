602383ba98489f0bb1da3296547966bd08dbace1
user <user@Dmitry>
2022-07-21 14:57:41 +0300

    21.07.2022 ширина 1 лыжи

--- admin/gap.php
+++ admin/gap.php
@@ -23,6 +23,7 @@ $error_message = '';
 
 $gap_raport_valid = '';
 $gap_stream_valid = '';
+$ski_valid = '';
 
 // Сохранение введённых значений
 if(null !== filter_input(INPUT_POST, 'norm_gap_submit')) {
@@ -36,26 +37,34 @@ if(null !== filter_input(INPUT_POST, 'norm_gap_submit')) {
         $form_valid = false;
     }
     
+    if(empty(filter_input(INPUT_POST, 'ski'))) {
+        $ski_valid = ISINVALID;
+        $form_valid = false;
+    }
+    
     if($form_valid) {
         // Старый объект
         $old_gap_raport = '';
         $old_gap_stream = '';
+        $old_ski = '';
         
-        $sql = "select gap_raport, gap_stream from norm_gap where machine_id = $machine_id order by date desc limit 1";
+        $sql = "select gap_raport, gap_stream, ski from norm_gap where machine_id = $machine_id order by date desc limit 1";
         $fetcher = new Fetcher($sql);
         $error_message = $fetcher->error;
         
         if($row = $fetcher->Fetch()) {
             $old_gap_raport = $row['gap_raport'];
             $old_gap_stream = $row['gap_stream'];
+            $old_ski = $row['ski'];
         }
         
         // Новый объект
         $new_gap_raport = filter_input(INPUT_POST, 'gap_raport');
         $new_gap_stream = filter_input(INPUT_POST, 'gap_stream');
+        $new_ski = filter_input(INPUT_POST, 'ski');
         
-        if($old_gap_raport != $new_gap_raport || $old_gap_stream != $new_gap_stream) {
-            $sql = "insert into norm_gap (machine_id, gap_raport, gap_stream) values ($machine_id, $new_gap_raport, $new_gap_stream)";
+        if($old_gap_raport != $new_gap_raport || $old_gap_stream != $new_gap_stream || $old_ski != $new_ski) {
+            $sql = "insert into norm_gap (machine_id, gap_raport, gap_stream, ski) values ($machine_id, $new_gap_raport, $new_gap_stream, $new_ski)";
             $executer = new Executer($sql);
             $error_message = $executer->error;
         }
@@ -68,8 +77,9 @@ if(null !== filter_input(INPUT_POST, 'norm_gap_submit')) {
 // Получение объекта
 $gap_raport = '';
 $gap_stream = '';
+$ski = '';
 
-$sql = "select gap_raport, gap_stream from norm_gap where machine_id = $machine_id order by date desc limit 1";
+$sql = "select gap_raport, gap_stream, ski from norm_gap where machine_id = $machine_id order by date desc limit 1";
 $fetcher = new Fetcher($sql);
 if(empty($error_message)) {
     $error_message = $fetcher->error;
@@ -78,6 +88,7 @@ if(empty($error_message)) {
 if($row = $fetcher->Fetch()) {
     $gap_raport = $row['gap_raport'];
     $gap_stream = $row['gap_stream'];
+    $ski = $row['ski'];
 }
 ?>
 <!DOCTYPE html>
@@ -112,7 +123,7 @@ if($row = $fetcher->Fetch()) {
                     <form method="post">
                         <input type="hidden" id="machine_id" name="machine_id" value="<?= filter_input(INPUT_GET, 'machine_id') ?>" />
                         <div class="form-group">
-                            <label for="gap_raport">ЗазорРапорт (минимальное расстояние между этикетками, мм)</label>
+                            <label for="gap_raport">ЗазорРапорт (минимальное расстояние между этикетками), мм</label>
                             <input type="text" 
                                    class="form-control float-only" 
                                    id="gap_raport" 
@@ -128,7 +139,7 @@ if($row = $fetcher->Fetch()) {
                             <div class="invalid-feedback">ЗазорРапорт обязательно</div>
                         </div>
                         <div class="form-group">
-                            <label for="gap_stream">ЗазорРучей (минимальное расстояние между ручьями, мм)</label>
+                            <label for="gap_stream">ЗазорРучей (минимальное расстояние между ручьями), мм</label>
                             <input type="text" 
                                    class="form-control float-only" 
                                    id="gap_stream" 
@@ -142,6 +153,22 @@ if($row = $fetcher->Fetch()) {
                                    onkeyup="javascript: $(this).attr('id', 'gap_stream'); $(this).attr('name', 'gap_stream'); $(this).attr('placeholder', 'ЗазорРучей, мм');" 
                                    onfocusout="javascript: $(this).attr('id', 'gap_stream'); $(this).attr('name', 'gap_stream'); $(this).attr('placeholder', 'ЗазорРучей, мм');" />
                             <div class="invalid-feedback">ЗазорРучей обязательно</div>
+                        </div>
+                        <div class="form-group">
+                            <label for="ski">Ширина 1 лыжи, мм</label>
+                            <input type="text" 
+                                   class="form-control float-only" 
+                                   id="ski" 
+                                   name="ski" 
+                                   value="<?= empty($ski) ? "" : floatval($ski) ?>" 
+                                   placeholder="Ширина 1 лыжи, мм" 
+                                   required="required" 
+                                   onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
+                                   onmouseup="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" 
+                                   onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder'); }" 
+                                   onkeyup="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" 
+                                   onfocusout="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" />
+                            <div class="invalid-feedback">Ширина 1 лыжи обязательно</div>
                         </div>
                         <button type="submit" id="norm_gap_submit" name="norm_gap_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
                     </form>
