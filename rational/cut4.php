e4881acb986f2ab2fcf3ae6a72004a0914f90014
user <user@Dmitry>
2022-08-09 11:58:41 +0300

    09.08.2022 рациональный раскрой

--- rational/cut4.php
+++ rational/cut4.php
@@ -1,3 +1,6 @@
+<?php
+include '../include/topscripts.php';
+?>
 <html>
     <body>
         <a href="cut4.php" title="Очистить">Очистить</a>
@@ -7,9 +10,9 @@
                 <tr>
                     <td>
                         <label for="source_width">Ширина исходного ролика, мм</label><br />
-                        <input type="number" min="1" name="source_width" value="<?= filter_input(INPUT_POST, 'source_width') ?>1500" required="required" /><br /><br />
+                        <input type="number" min="1" id="source_width" name="source_width" value="<?= filter_input(INPUT_POST, 'source_width') ?>1500" required="required" /><br /><br />
                         <label for="cut_length">Длина одного съёма, м</label><br />
-                        <input type="number" min="1" name="cut_length" value="<?= filter_input(INPUT_POST, 'cut_length') ?>2000" required="required" /><br /><br />
+                        <input type="number" min="1" id="cut_length" name="cut_length" value="<?= filter_input(INPUT_POST, 'cut_length') ?>2000" required="required" /><br /><br />
                     </td>
                     <td>
                         <?php
@@ -22,46 +25,48 @@
                             if($key_exists) array_push ($post_roll_keys, $post_roll_key);
                         } while ($key_exists);
                         ?>
-                        <table style="display: none;">
+                        <?php if(false): ?>
+                        <table>
                             <tr>
                                 <th>Ширина, мм</th>
                                 <?php foreach($post_roll_keys as $item): ?>
-                                <td><input type="number" min="1" name="width_<?=$item ?>" value="<?= filter_input(INPUT_POST, "width_$item") ?>" style="width: 70px;" /></td>
+                                <td><input type="number" min="1" id="width_<?=$item ?>" name="width_<?=$item ?>" value="<?= filter_input(INPUT_POST, "width_$item") ?>" style="width: 70px;" /></td>
                                 <?php endforeach; ?>
                                 <?php if(null !== filter_input(INPUT_POST, 'add_submit') || count($post_roll_keys) == 0): ?>
-                                <td><input type="number" min="1" name="width_<?=$post_roll_key ?>" style="width: 70px;" /></td>
+                                <td><input type="number" min="1" id="width_<?=$post_roll_key ?>" name="width_<?=$post_roll_key ?>" style="width: 70px;" /></td>
                                 <?php endif; ?>
                                 <td><button type="submit" name="add_submit">Добавить</button></td>
                             </tr>
                             <tr>
                                 <th>Длина, м</th>
                                 <?php foreach($post_roll_keys as $item): ?>
-                                <td><input type="number" min="1" name="length_<?=$item ?>" value="<?= filter_input(INPUT_POST, "length_$item") ?>" style="width: 70px;" /></td>
+                                <td><input type="number" min="1" id="length_<?=$item ?>" name="length_<?=$item ?>" value="<?= filter_input(INPUT_POST, "length_$item") ?>" style="width: 70px;" /></td>
                                 <?php endforeach; ?>
                                 <?php if(null !== filter_input(INPUT_POST, 'add_submit') || count($post_roll_keys) == 0): ?>
-                                <td><input type="number" min="1" name="length_<?=$post_roll_key ?>" style="width: 70px;" /></td>
+                                <td><input type="number" min="1" id="length_<?=$post_roll_key ?>" name="length_<?=$post_roll_key ?>" style="width: 70px;" /></td>
                                 <?php endif; ?>
                                 <td>&nbsp;</td>
                             </tr>
                         </table>
+                        <?php endif; ?>
                         <table>
                             <tr>
                                 <th>Ширина, мм</th>
-                                <td><input type="number" min="1" name="width_1" style="width: 70px;" value="120" /></td>
-                                <td><input type="number" min="1" name="width_2" style="width: 70px;" value="140" /></td>
-                                <td><input type="number" min="1" name="width_3" style="width: 70px;" value="150" /></td>
-                                <td><input type="number" min="1" name="width_4" style="width: 70px;" value="260" /></td>
-                                <td><input type="number" min="1" name="width_5" style="width: 70px;" value="200" /></td>
-                                <td><input type="number" min="1" name="width_6" style="width: 70px;" value="205" /></td>
+                                <td><input type="number" min="1" id="width_1" name="width_1" style="width: 70px;" value="120" /></td>
+                                <td><input type="number" min="1" id="width_2" name="width_2" style="width: 70px;" value="140" /></td>
+                                <td><input type="number" min="1" id="width_3" name="width_3" style="width: 70px;" value="150" /></td>
+                                <td><input type="number" min="1" id="width_4" name="width_4" style="width: 70px;" value="260" /></td>
+                                <td><input type="number" min="1" id="width_5" name="width_5" style="width: 70px;" value="200" /></td>
+                                <td><input type="number" min="1" id="width_6" name="width_6" style="width: 70px;" value="205" /></td>
                             </tr>
                             <tr>
                                 <th>Длина, м</th>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="10000" /></td>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="2000" /></td>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="18000" /></td>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="4000" /></td>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="2000" /></td>
-                                <td><input type="number" min="1" name="length_1" style="width: 70px;" value="4000" /></td>
+                                <td><input type="number" min="1" id="length_1" name="length_1" style="width: 70px;" value="10000" /></td>
+                                <td><input type="number" min="1" id="length_2" name="length_1" style="width: 70px;" value="2000" /></td>
+                                <td><input type="number" min="1" id="length_3" name="length_1" style="width: 70px;" value="18000" /></td>
+                                <td><input type="number" min="1" id="length_4" name="length_1" style="width: 70px;" value="4000" /></td>
+                                <td><input type="number" min="1" id="length_5" name="length_1" style="width: 70px;" value="2000" /></td>
+                                <td><input type="number" min="1" id="length_6" name="length_1" style="width: 70px;" value="4000" /></td>
                             </tr>
                         </table>
                     </td>
@@ -70,9 +75,23 @@
             <button type="button" name="cut_sumbit" onclick="javascript: Start();">Рассчитать</button>
         </form>
     </body>
+    <script src='<?=APPLICATION ?>/js/jquery-3.5.1.min.js'></script>
     <script>
         function Start() {
-            alert('STARTING...');
+            if($('#source_width').val() === '' ||
+                    $('#cut_length').val() === '' ||
+                    $('#width_1').val() === '' || $('#width_1').val() === undefined ||
+                    $('#length_1').val() === '' || $('#length_1').val() === undefined) {
+                alert('Введите данные');
+                return;
+            }
+            
+            var source_width = $('#source_width').val();
+            var cut_length = $('#cut_length').val();
+            var width = $('#width_1').val();
+            var length = $('#length_1').val();
+            
+            alert('<?=APPLICATION ?> ' + source_width + ' ' + cut_length + ' ' + width + ' ' + length);
         }
     </script>
 </html>
