7d616339a100bd6affa20d381859d79a800f83e0
user <user@Dmitry>
2022-08-23 14:47:19 +0300

    23.08.2022 чтобы не исчезала отгрузочная стоимость ПФ при установки галочки "включать ПФ в себестоимость"

--- calculation/right_panel_self_adhesive.php
+++ calculation/right_panel_self_adhesive.php
@@ -507,10 +507,8 @@ if(!empty($id)) {
             <h3>Отгрузочная стоимость</h3>
             <div>Отгрузочная стоимость <?=$cliche_in_price == 1 ? 'с' : 'без' ?> ПФ</div>
             <div class="value"><?= CalculationBase::Display(floatval($shipping_cost), 0) ?> &#8381;&nbsp;&nbsp;&nbsp;<span class="font-weight-normal"><?= CalculationBase::Display(floatval($shipping_cost_per_unit), 3) ?> &#8381; за шт</span></div>
-            <?php if($cliche_in_price != 1): ?>
             <div class="mt-2">Отгрузочная стоимость ПФ</div>
             <div class="value"><?= CalculationBase::Display(floatval($shipping_cliche_cost), 0) ?> &#8381;</div>
-            <?php endif; ?>
         </div>
         <div class="col-4">
             <h3>Прибыль</h3>
