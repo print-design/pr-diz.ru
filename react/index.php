ce2524f408587f03001db86f19014a3a259c6c46
user <user@Dmitry>
2022-09-14 16:06:26 +0300

    14.09.2022 react

---
+++ react/index.php
@@ -0,0 +1,19 @@
+<?php
+include '../include/topscripts.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1>Здесь будет React</h1>
+        </div>
+    </body>
+    <?php
+    include '../include/footer.php';
+    ?>
+</html>
