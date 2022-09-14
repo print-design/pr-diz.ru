28df11b3251a8868e33462103f447a4c3b6df514
user <user@Dmitry>
2022-09-14 11:24:35 +0300

    14.09.2022 китайский язык

--- chinese/index.php
+++ chinese/index.php
@@ -1,14 +1,51 @@
 <?php
 include '../include/topscripts.php';
+include './database_chinese.php';
 ?>
 <html>
     <head>
         <?php
         include '../include/head.php';
         ?>
-        <link href="<?=APPLICATION ?>/css/select2.min.css" rel="stylesheet"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+            .container {
+                font-size: x-large;
+            }
+        </style>
     </head>
     <body>
-        <h1>Китайский язык</h1>
+        <div class="container">
+            <div class="text-center">
+                <a class="btn btn-dark mt-2" href="./">Новое слово</a>
+            </div>
+            <hr />
+            <?php
+            $sql = "select word, transcription, translation from words limit 1";
+            $fetcher = new FetcherChinese($sql);
+            if($row = $fetcher->Fetch()):
+            ?>
+            <h1><?=$row['word'] ?></h1>
+            <div id="showtranscription" class="mt-4"><a class="btn btn-outline-dark" href="javascript:void(0);" id="btn_showtranscription">Показать транскрипцию</a></div>
+            <div id="transcription" class="mt-4 d-none"><?=$row['transcription'] ?></div>
+            <div id="showtranslation" class="mt-4"><a class="btn btn-outline-dark" href="javascript:void(0);" id="btn_showtranslation">Показать перевод</a></div>
+            <div id="translation" class="mt-4 d-none"><?=$row['translation'] ?></div>
+            <?php
+            endif;
+            ?>
+        </div>
     </body>
+    <script src='<?=APPLICATION ?>/js/jquery-3.5.1.min.js'></script>
+    <script src='<?=APPLICATION ?>/js/bootstrap.min.js'></script>
+    <script>
+        $('#btn_showtranscription').click(function() {
+            $('#showtranscription').addClass('d-none');
+            $('#transcription').removeClass('d-none');
+        });
+        
+        $('#btn_showtranslation').click(function() {
+            $('#showtranslation').addClass('d-none');
+            $('#translation').removeClass('d-none');
+        });
+    </script>
 </html>
