9d229012f0651d43e162b66d58ed1e278bc7cf9b
user <user@Dmitry>
2022-09-14 16:59:04 +0300

    14.09.2022 react

--- react/index.php
+++ react/index.php
@@ -11,7 +11,15 @@ include '../include/topscripts.php';
     <body>
         <div class="container-fluid">
             <h1>Здесь будет React</h1>
+            <div id="like_button_container"></div>
         </div>
+        <!-- Загрузим React. -->
+        <!-- Примечание: при деплое на продакшен замените «development.js» на «production.min.js». -->
+        <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
+        <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
+        
+        <!-- Загрузим наш React-компонент. -->
+        <script src="like_button.js"></script>
     </body>
     <?php
     include '../include/footer.php';
