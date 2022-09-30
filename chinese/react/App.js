7ac9e1299c00c30533815c604276a9e1f601abe9
user <user@Dmitry>
2022-09-30 16:32:13 +0300

    30.09.2022 react

--- chinese/react/App.js
+++ chinese/react/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import axios from 'axios';
@@ -29,21 +28,18 @@ class DictionaryValue extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { word: '', transcription: '', translation: '' };
+    this.state = { dictionaryItem: [] };
     this.transcription = React.createRef();
     this.translation = React.createRef();
   }
 
   setnewword = () => {
-    axios.get('https://pr-diz.ru/chinese/word.php')
+    axios.get(`https://pr-diz.ru/chinese/word.php`)
     .then(res => {
-      const dictionaryValue = res.data;
-      this.setState({ word: res.data });
-      //this.setState({ dictionaryValue });
-      //this.setState({ word: 'ASd', transcription: 'ASD', translation: 'NJU' });
+      const dictionaryItem = res.data;
+      this.setState({ dictionaryItem });
+      
     })
-    // this.setState({ word: "CDE" });
-    //this.setState({"word":"\u7167\u5f20\u76f8","transcription":"zh\u00e0o zh\u0101ng xi\u00e0ng","translation":"\u0441\u0434\u0435\u043b\u0430\u0442\u044c \u0441\u043d\u0438\u043c\u043e\u043a, c\u0444\u043e\u0442\u043e\u0433\u0440\u0430\u0444\u0438\u0440\u043e\u0432\u0430\u0442\u044c "});
     
     this.transcription.current.reset();
     this.translation.current.reset();
@@ -63,9 +59,9 @@ class App extends React.Component {
       )}
     </div>
     <hr />
-    <h1>{ this.state.word }</h1>
-    <DictionaryValue ref={this.transcription} value={this.state.transcription} button="Показать транскрипцию" />
-    <DictionaryValue ref={this.translation} value={this.state.translation} button="Показать перевод" />
+    <h1>{ this.state.dictionaryItem.word }</h1>
+    <DictionaryValue ref={this.transcription} value={ this.state.dictionaryItem.transcription } button="Показать транскрипцию" />
+    <DictionaryValue ref={this.translation} value={ this.state.dictionaryItem.translation } button="Показать перевод" />
   </div>
   }
 }
