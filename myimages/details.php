3194cd144d70225263498e58269411a84539e924
user <user@Dmitry>
2022-10-13 11:40:25 +0300

    13.10.2022 image

--- myimages/details.php
+++ myimages/details.php
@@ -23,6 +23,33 @@ else {
     $error_message = "Ошибка при получении имени файла";
 }
 
+function hex2rgb($hex) {
+  $color = str_replace('#','',$hex);
+  $rgb = array('r' => hexdec(substr($color,0,2)),
+               'g' => hexdec(substr($color,2,2)),
+               'b' => hexdec(substr($color,4,2)));
+  return $rgb;
+}
+
+function rgb2cmyk($var1,$g=0,$b=0) {
+   if(is_array($var1)) {
+      $r = $var1['r'];
+      $g = $var1['g'];
+      $b = $var1['b'];
+   }
+   else $r=$var1;
+   $cyan    = 255 - $r;
+   $magenta = 255 - $g;
+   $yellow  = 255 - $b;
+   $black   = min($cyan, $magenta, $yellow);
+   $cyan    = @(($cyan    - $black) / (255 - $black)) * 255;
+   $magenta = @(($magenta - $black) / (255 - $black)) * 255;
+   $yellow  = @(($yellow  - $black) / (255 - $black)) * 255;
+   return array('c' => $cyan / 255,
+                'm' => $magenta / 255,
+                'y' => $yellow / 255,
+                'k' => $black / 255);
+}
 ?>
 <html>
     <head>
@@ -74,8 +101,12 @@ else {
                 for($y = 0; $y < $imagesize[1]; $y++) {
                     echo "<tr>";
                     for($x = 0; $x < $imagesize[0]; $x++) {
-                        echo "<td>";
-                        echo imagecolorat($gdimage, $x, $y);
+                        echo "<td class='text-nowrap pr-3'>";
+                        $dec = imagecolorat($gdimage, $x, $y);
+                        $hex = dechex($dec);
+                        $rgb = hex2rgb($hex);
+                        $cmyk = rgb2cmyk($rgb);
+                        echo "C ". number_format($cmyk['c'] * 100, 2, ",", "")."% - M ". number_format($cmyk['m'] * 100, 2, ",", "")."% - Y ". number_format($cmyk['y'] * 100, 2, ",", "")."% - K ". number_format($cmyk['k'] * 100, 2, ",", "")."%";
                         echo "</td>";
                     }
                     echo "</tr>";
