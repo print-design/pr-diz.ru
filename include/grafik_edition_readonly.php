094af0a4dbd9ec03a0816fc0d71fc99a9886aa01
user <user@Dmitry>
2023-03-07 12:22:19 +0300

    07.03.2023 график - материал, толщина, ширина

--- include/grafik_edition_readonly.php
+++ include/grafik_edition_readonly.php
@@ -1,7 +1,7 @@
 <tr>
     <?php if($this->shift == 'day' && $this->edition_key == 0): ?>
-    <td class="<?=$top ?>" rowspan="<?=$this->date_editions_count ?>"><?=$GLOBALS['weekdays'][$this->date->format('w')] ?></td>
-    <td class="<?=$top ?>" rowspan="<?=$this->date_editions_count ?>"><?=$this->date->format('d.m').".".$this->date->format('Y') ?></td>
+    <td class="<?=$top ?>" rowspan="<?=$this->date_editions_count ?>" style="padding-left: 7px; padding-right: 7px;"><?=$GLOBALS['weekdays'][$this->date->format('w')] ?></td>
+    <td class="<?=$top ?>" rowspan="<?=$this->date_editions_count ?>" style="padding-left: 7px; padding-right: 7px;"><?=$this->date->format('d.m').".".$this->date->format('Y') ?></td>
     <?php endif; ?>
     <?php if($this->edition_key == 0): ?>
     <td class="<?=$top.' '.$this->shift ?>" rowspan="<?=$this->shift_editions_count ?>"><?=($this->shift == 'day' ? 'День' : 'Ночь') ?></td>
