bbafbcba8d459b5244c656f035c209c9981604c9
user <user@Dmitry>
2023-03-13 08:37:48 +0300

    10.03.2023 нужно подготовить

--- ajax/laminator_roller.php
+++ ajax/laminator_roller.php
@@ -5,7 +5,7 @@ $laminator_id = filter_input(INPUT_GET, 'laminator_id');
 
 echo "<option value='' hidden='hidden'>Ширина ламинирующего вала...</option>";
 
-$sql = "select value from norm_laminator_roller where laminator_id = $laminator_id and active = 1";
+$sql = "select value from norm_laminator_roller where laminator_id = $laminator_id and active = 1 order by value";
 $fetcher = new Fetcher($sql);
 while($row = $fetcher->Fetch()):
 ?>
