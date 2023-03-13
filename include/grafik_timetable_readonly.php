e82d7df12e868d4b857b5bb1a8e54942dd002186
user <user@Dmitry>
2023-03-13 16:00:57 +0300

    13.03.2023 автовыбор ширины ламинирующего вала

--- include/grafik_timetable_readonly.php
+++ include/grafik_timetable_readonly.php
@@ -19,6 +19,29 @@
                 <a href="?id=<?= filter_input(INPUT_GET, 'id') ?>" class="btn btn-outline-dark">Сбросить</a>
             </div>
         </form>
+        <?php elseif(IsInRole(array('storekeeper'))): ?>
+        <form class="form-inline" action="<?=APPLICATION ?>/grafik/print.php" target="_blank" method="post">
+            <input type="hidden" id="from" name="from" value="<?= $this->dateFrom->format('Y-m-d') ?>" class="print_from" />
+            <input type="hidden" id="to" name="to" value="<?= filter_input(INPUT_GET, 'to') ?>" class="print_to" />
+            <input type="hidden" id="machine" name="machine" value="<?= $this->machineId ?>"/>
+            <input type="hidden" id="name" name="name" value="<?= $this->name ?>"/>
+            <input type="hidden" id="user1Name" name="user1Name" value="<?= $this->user1Name ?>"/>
+            <input type="hidden" id="user2Name" name="user2Name" value="<?= $this->user2Name ?>"/>
+            <input type="hidden" id="userRole" name="userRole" value="<?= $this->userRole ?>"/>
+            <input type="hidden" id="hasOrganization" name="hasOrganization" value="<?= $this->hasOrganization ?>"/>
+            <input type="hidden" id="hasEdition" name="hasEdition" value="<?= $this->hasEdition ?>"/>
+            <input type="hidden" id="hasMaterial" name="hasMaterial" value="<?=$this->hasMaterial ?>"/>
+            <input type="hidden" id="hasThickness" name="hasThickness" value="<?=$this->hasThickness ?>"/>
+            <input type="hidden" id="hasWidth" name="hasWidth" value="<?=$this->hasWidth ?>"/>
+            <input type="hidden" id="hasLength" name="hasLength" value="<?= $this->hasLength ?>"/>
+            <input type="hidden" id="hasStatus" name="hasStatus" value="<?= $this->hasStatus ?>"/>
+            <input type="hidden" id="hasRoller" name="hasRoller" value="<?= $this->hasRoller ?>"/>
+            <input type="hidden" id="hasLamination" name="hasLamination" value="<?= $this->hasLamination ?>"/>
+            <input type="hidden" id="hasColoring" name="hasColoring" value="<?= $this->hasColoring ?>"/>
+            <input type="hidden" id="hasManager" name="hasManager" value="<?= $this->hasManager ?>"/>
+            <input type="hidden" id="hasComment" name="hasComment" value="<?= $this->hasComment ?>"/>
+            <button type="submit" class="form-control btn btn-outline-dark" id="print_submit" name="print_submit">Печать&nbsp;<i class="fas fa-print"></i></button>
+        </form>
         <?php endif; ?>
     </div>
 </div>
