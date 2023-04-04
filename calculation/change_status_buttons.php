443379b7a0832152e8ee08c1a6f4d4467317a155
user <user@Dmitry>
2023-04-04 13:56:51 +0300

    04.04.2023 статусы: ждём одобрения, ждём постановки в план, отклонено, в плане

--- calculation/change_status_buttons.php
+++ calculation/change_status_buttons.php
@@ -34,6 +34,19 @@
         </form>
     </div>
 </div>
-<?php elseif($status_id != DRAFT && $status_id != CALCULATION && $status_id != TRASH): ?>
+<?php elseif ($status_id == WAITING && IsInRole(array("manager-senior", "technologist"))): ?>
+<div class="d-flex justify-content-start">
+    <form method="post">
+        <input type="hidden" name="id" value="<?= filter_input(INPUT_GET, 'id') ?>" />
+        <input type="hidden" name="status_id" value="<?=CONFIRMED ?>" />
+        <button type="submit" name="change-status-submit" class="btn btn-outline-dark mt-3" style="width: 200px;">Одобрить</button>
+    </form>
+    <form method="post" class="ml-4">
+        <input type="hidden" name="id" value="<?= filter_input(INPUT_GET, 'id') ?>" />
+        <input type="hidden" name="status_id" value="<?=REJECTED ?>" />
+        <button type="submit" name="change-status-submit" class="btn btn-outline-dark draft mt-3" style="width: 200px;">Отклонить</button>
+    </form>
+</div>
+<?php else: ?>
 <a href="techmap.php?id=<?=$id ?>" class="btn btn-outline-dark mt-3 mr-2" style="width: 200px;">Посмотреть тех. карту</a>
 <?php endif; ?>
--- calculation/status_ids.php
+++ calculation/status_ids.php
@@ -9,7 +9,7 @@ const TECHMAP = 6; // Составлена технологическая кар
 const TRASH = 7; // В корзине
 const PLAN = 8; // В плане
 
-$status_names = array(DRAFT => "Черновик", CALCULATION => "Сделан расчёт", WAITING => "Ждём подтверждения", CONFIRMED => "Одобрено", REJECTED => "Отклонено", TECHMAP => "Составлена тех. карта", TRASH => "В корзине", PLAN => "В плане");
+$status_names = array(DRAFT => "Черновик", CALCULATION => "Сделан расчёт", WAITING => "Ждём подтверждения", CONFIRMED => "Ждём постановки в план", REJECTED => "Отклонено", TECHMAP => "Составлена тех. карта", TRASH => "В корзине", PLAN => "В плане");
 $status_colors = array(DRAFT => "gray", CALCULATION => "steelblue", WAITING => "goldenrod", CONFIRMED => "mediumseagreen", REJECTED => "indianred", TECHMAP => "saddlebrown", TRASH => "black", PLAN => "purple");
 $status_icons = array(DRAFT => "fas fa-edit", CALCULATION => "fas fa-check", WAITING => "fas fa-clock", CONFIRMED => "fas fa-check-double", REJECTED => "fas fa-times-circle", TECHMAP => "fas fa-file", TRASH => "fas fa-trash-alt", PLAN => "fas fa-list-ol");
 ?>
