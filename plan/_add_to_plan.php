9640c7e13866121502a16ca6b997f496834ab83c
user <user@Dmitry>
2023-04-13 16:52:43 +0300

    12.04.2023 план

--- plan/_add_to_plan.php
+++ plan/_add_to_plan.php
@@ -9,6 +9,15 @@ $from = filter_input(INPUT_GET, 'from');
 $before = filter_input(INPUT_GET, 'before');
 $error = '';
 
+class Edition {
+    public $Timespan;
+    public $Position;
+    public $Date;
+    public $Shift;
+}
+
+$editions = array();
+
 // Определяем размер расчёта
 $work_time_1 = '';
 
@@ -22,37 +31,50 @@ if($row = $fetcher->Fetch()) {
     echo $work_time_1."<br />";
 }
 
-// Определяем, сколько места в нужной смене
-$position = 0;
-$timespans = array();
-
 // Если не указываем следующий расчёт, то размер смены равен 12 минус сумма всех расчётов данной смены
 // Если указываем следующий расчёт, то размер смены равен 12 минус сумма всех расчётов смены, кроме тех, у кого position меньше, чем position следующего расчёта
+$sql = "";
 if(empty($before)) {
-    $sql = "select 12 - sum(e.timespan) timespan1, max(position) position "
+    $sql = "select sum(e.timespan) timespan1, max(position) position "
             . "from plan_edition e "
             . "inner join calculation c on e.calculation_id = c.id "
             . "where c.machine_id = $machine_id and e.date = '$date' and e.shift = '$shift'";
-    $fetcher = new Fetcher($sql);
-    if($row = $fetcher->Fetch()) {
-        $position = $row['position'];
-        array_push($timespans, round($row['timespan1'], 2));
-    }
 }
 else {
-    $sql = "select 12 - sum(e.timespan) timespan1, max(position) position "
+    $sql = "select sum(e.timespan) timespan1, max(position) position "
             . "from plan_edition e "
             . "inner join calculation c on e.calculation_id = c.id "
-            . "where c.machine_id = $machine_id and e.date = '$date' and e.shift = '$shift'";
-    $fetcher = new Fetcher($sql);
-    if($row = $fetcher->Fetch()) {
-        $position = $row['position'];
-        array_push($timespans, round($row['timespan1'], 2));
+            . "where c.machine_id = $machine_id and e.date = '$date' and e.shift = '$shift' "
+            . "and e.position < "
+            . "(select min(position) "
+            . "from plan_edition "
+            . "where calculation_id = $before and machine_id = $machine_id and date='$date' and shift = '$shift')";
+}
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $position = $row['position'];
+    $edition = new Edition();
+    if(empty($row['position'])) {
+        $edition->Position = 1;
     }
+    else {
+        $edition->Position = $row['position'] + 1;
+    }
+    $edition->Timespan = 12 - round($row['timespan1'], 2);
+    array_push($editions, $edition);
 }
 
-echo $position."<br />";
-print_r($timespans); echo "<br />";
+// Определяем следующие смены
+$sum_timespans = $editions[0]->Timespan;
+while ($sum_timespans < $work_time_1) {
+    $edition = new Edition();
+    $edition->Timespan = min(12, $work_time_1 - $sum_timespans);
+    $edition->Position = 1;
+    $sum_timespans += $edition->Timespan;
+    array_push($editions, $edition);
+}
+
+print_r($editions); echo "<br />";
 
 echo json_encode(array('machine_id' => $machine_id, 'from' => $from, 'error' => $error));
 ?>
