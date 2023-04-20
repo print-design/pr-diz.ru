19244935ada6b7ea37e3afed1fa7a919d79cb536
user <user@Dmitry>
2023-04-20 08:58:26 +0300

    19.04.2023 план упрощение

--- plan/_queue_view.php
+++ plan/_queue_view.php
@@ -3,13 +3,13 @@
         <div class="d-flex justify-content-start">
             <div style="padding-top: 10px; padding-right: 10px;" data-id="<?=$row['id'] ?>" draggable="true" ondragstart="DragQueue(event);"><img src="../images/icons/double-vertical-dots.svg" draggable="false" /></div>
             <div>
-                <div style="font-weight: bold; font-size: large;"><a href='../calculation/techmap.php?id=<?=$row['id'] ?>'><?=$row['calculation'] ?></a></div>
+                <div style="font-weight: bold; font-size: large; line-height: 20px; margin-bottom: 8px;"><a href='../calculation/techmap.php?id=<?=$row['id'] ?>'><?=$row['calculation'] ?></a></div>
                 <?=$row['customer'] ?>
             </div>
         </div>
         <div>
             <div class="d-flex justify-content-end" style="padding-top: 10px;">
-                <div><img src="../images/icons/vertical-dots1.svg" /></div>
+                <div><a href='../calculation/techmap.php?id=<?=$row['id'] ?>'><img src="../images/icons/vertical-dots1.svg" /></a></div>
                 <div style="padding-left: 10px;"><img src="../images/icons/right-arrow.svg" /></div>
             </div>
         </div>
