993b5170a32539b67e37b3bea43f389ad64b9058
user <user@Dmitry>
2023-04-21 10:54:33 +0300

    21.04.2023 план события

--- plan/print.php
+++ plan/print.php
@@ -12,42 +12,10 @@ if(!IsInRole(array('technologist', 'dev', 'administrator', 'manager-senior'))) {
 if(null !== filter_input(INPUT_POST, 'add_event_submit')) {
     $machine_id = filter_input(INPUT_POST, 'machine_id');
     $text = addslashes(filter_input(INPUT_POST, 'text'));
-    $date = filter_input(INPUT_POST, 'date');
-    $shift = filter_input(INPUT_POST, 'shift');
     $worktime = filter_input(INPUT_POST, 'worktime');
     $in_plan = filter_input(INPUT_POST, 'in_plan');
     
-    $max_edition = 0;
-    $max_event = 0;
-    
-    $sql = "select max(e.position) "
-            . "from plan_edition e "
-            . "inner join calculation c on e.calculation_id = c.id "
-            . "where c.machine_id = $machine_id and e.date = '$date' and e.shift = '$shift'";
-    $fetcher = new Fetcher($sql);
-    $row = $fetcher->Fetch();
-    if(!$row) {
-        $error = "Ошибка при определении позиции тиража";
-        echo json_encode(array('error' => $error));
-        exit();
-    }
-    $max_edition = $row[0];
-    
-    $sql = "select max(position) "
-            . "from plan_event "
-            . "where in_plan = 1 and machine_id = $machine_id and date = '$date' and shift = '$shift'";
-    $fetcher = new Fetcher($sql);
-    $row = $fetcher->Fetch();
-    if(!$row) {
-        $error = "Ошибка при определении позиции события";
-        echo json_encode(array('error' => $error));
-        exit();
-    }
-    $max_event = $row[0];
-    
-    $position = max($max_edition, $max_event) + 1;
-    
-    $sql = "insert into plan_event (machine_id, text, date, shift, position, worktime, in_plan) values ($machine_id, '$text', '$date', '$shift', $position, $worktime, $in_plan)";
+    $sql = "insert into plan_event (machine_id, text, worktime, in_plan) values ($machine_id, '$text', $worktime, $in_plan)";
     $executer = new Executer($sql);
     $error_message = $executer->error;
 }
@@ -176,45 +144,27 @@ if(null !== filter_input(INPUT_POST, 'delete_event_submit')) {
                 <div class="modal-content">
                     <form method="post">
                         <input type="hidden" name="machine_id" value="<?= filter_input(INPUT_GET, 'id') ?>" />
-                        <input type="hidden" name="in_plan" value="1" />
+                        <input type="hidden" name="in_plan" value="0" />
                         <input type="hidden" name="scroll" />
                         <div class="modal-header">
                             <p class="font-weight-bold" style="font-size: x-large;">Добавить событие</p>
                             <button type="button" class="close add_event_dismiss" data-dismiss="modal"><i class="fas fa-times" style="color: #EC3A7A"></i></button>
                         </div>
                         <div class="modal-body">
-                            <div class="row">
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="date">Дата</label>
-                                        <input type="date" name="date" class="form-control" required="required" />
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="worktime">Кол-во часов</label>
-                                        <input type="text" 
-                                               name="worktime" 
-                                               class="form-control float-only" 
-                                               required="required" 
-                                               autocomplete="off"
-                                               onmousedown="javascript: $(this).removeAttr('name');" 
-                                               onfocus="javascript: $(this).removeAttr('name');" 
-                                               onmouseup="javascript: $(this).attr('name', 'worktime');" 
-                                               onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('name'); }" 
-                                               onkeyup="javascript: $(this).attr('name', 'worktime');" 
-                                               onfocusout="javascript: $(this).attr('name', 'worktime');" />
-                                    </div>
-                                </div>
-                                <div class="col-4">
-                                    <div class="form-group">
-                                        <label for="shift">Начало события</label>
-                                        <select name="shift" class="form-control" required="required">
-                                            <option hidden="hidden" value="">...</option>
-                                            <option value="day">День</option>
-                                            <option value="night">Ночь</option>
-                                        </select>
-                                    </div>
+                            <div class="d-flex justify-content-between">
+                                <div class="text-nowrap w-25"><label>Кол-во часов</label></div>
+                                <div class="w-75">
+                                    <input type="text" 
+                                           name="worktime" 
+                                           class="form-control float-only" 
+                                           required="required" 
+                                           autocomplete="off"
+                                           onmousedown="javascript: $(this).removeAttr('name');" 
+                                           onfocus="javascript: $(this).removeAttr('name');" 
+                                           onmouseup="javascript: $(this).attr('name', 'worktime');" 
+                                           onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('name'); }" 
+                                           onkeyup="javascript: $(this).attr('name', 'worktime');" 
+                                           onfocusout="javascript: $(this).attr('name', 'worktime');" />
                                 </div>
                             </div>
                             <div class="form-group">
@@ -601,14 +551,6 @@ if(null !== filter_input(INPUT_POST, 'delete_event_submit')) {
                     return;
                 }
             }
-            
-            $('#timetable').on('scroll', function() {
-                $('input[name="scroll"]').val($('#timetable').scrollTop());
-            });
-            
-            <?php if(!empty($_REQUEST['scroll'])): ?>
-                $('#timetable').scrollTop(<?= intval($_REQUEST['scroll']) ?>);
-            <?php endif; ?>
         </script>
     </body>
 </html>
