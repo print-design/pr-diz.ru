af0572235965ce9ce0b8d3314413ca37c3dee54e
user <user@Dmitry>
2023-04-27 14:42:29 +0300

    27.04.2023 план события

--- plan/_add_continuation.php
+++ plan/_add_continuation.php
@@ -36,11 +36,39 @@ function GetNextDateShift($date, $shift) {
     }
     else {
         $datetime = DateTime::createFromFormat('Y-m-d', $date);
-        return new DateShift($datetime->add(new DateInterval('P1D')), 'day');
+        return new DateShift($datetime->add(new DateInterval('P1D'))->format('Y-m-d'), 'day');
     }
 }
 
 $plan_continuations = array();
+
+// Проверяем, точно ли у этого тиража нет допечатки
+$sql = "select worktime_continued from plan_edition where id = $id";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    if(!empty($row[0])) {
+        echo json_encode(array('error' => 'Сначала удалите все допечатки'));
+        exit();
+    }
+}
+
+// Получаем данные по тиражу
+$date = '';
+$shift = '';
+$worktime = 0;
+$machine_id = 0;
+
+$sql = "select pe.date, pe.shift, pe.worktime, c.machine_id "
+        . "from plan_edition pe "
+        . "inner join calculation c on pe.calculation_id = c.id "
+        . "where pe.id = $id";
+$fetcher = new Fetcher($sql);
+if($row = $fetcher->Fetch()) {
+    $date = $row['date'];
+    $shift = $row['shift'];
+    $worktime = $row['worktime'];
+    $machine_id = $row['machine_id'];
+}
 
 // Вычисляем, сколько времени остаётся в текущей смене
 $start_time = 0;
@@ -48,45 +76,48 @@ $start_time = 0;
 $sql = "select 12 - sum(pe.worktime) "
         . "from plan_edition pe "
         . "inner join calculation c on pe.calculation_id = c.id "
-        . "where pe.id <> $id "
-        . "and pe.date = (select date from plan_edition where id = $id) "
-        . "and pe.shift = (select shift from plan_edition where id = $id) "
-        . "and c.machine_id = (select machine_id from calculation where id in (select calculation_id from plan_edition where id = $id))";
+        . "where pe.id <> $id and pe.date = '$date' and pe.shift = '$shift' and c.machine_id = $machine_id";
 $fetcher = new Fetcher($sql);
 if($row = $fetcher->Fetch()) {
     $start_time = $row[0];
     
-    if($start_time < 0) {
+    if(empty($start_time) || $start_time < 0) {
         $start_time = 0;
     }
 }
 
 // Указываем оставшееся в текущей смене время для этого тиража
-echo $start_time."<br />";
+$sql = "update plan_edition set worktime_continued = $start_time where id = $id";
+$executer = new Executer($sql);
+$error = $executer->error;
 
 // Вычисляем, сколько времени нужно для допечаток
-$continuation_time = 0;
-$date = '';
-$shift = '';
-
-$sql = "select worktime - $start_time as continuation_time, date, shift from plan_edition where id = $id";
-$fetcher = new Fetcher($sql);
-if($row = $fetcher->Fetch()) {
-    $continuation_time = $row['continuation_time'];
-    $date = $row['date'];
-    $shift = $row['shift'];
-}
+$continuation_time = $worktime - $start_time;
 
 while($continuation_time > 0) {
     $next_date_shift = GetNextDateShift($date, $shift);
-    $worktime = min(12, $continuation_time);
-    $has_continuation = $continuation_time > $worktime ? 1 : 0;
-    $plan_continuation = new PlanContinuation($next_date_shift->date, $next_date_shift->shift, $id, $worktime, $has_continuation);
+    $next_worktime = min(12, $continuation_time);
+    $has_continuation = $continuation_time > $next_worktime ? 1 : 0;
+    $plan_continuation = new PlanContinuation($next_date_shift->date, $next_date_shift->shift, $id, $next_worktime, $has_continuation);
     
-    // Увеличиваем position у всех тиражей и событий данной смены
+    // Увеличиваем position у всех тиражей данной смены
+    $sql = "update plan_edition pe inner join calculation c on pe.calculation_id = c.id "
+            . "set pe.position = ifnull(pe.position, 1) + 1 "
+            . "where pe.date = '".$plan_continuation->date."' and pe.shift = '".$plan_continuation->shift."' and c.machine_id = $machine_id";
+    $executer = new Executer($sql);
+    $error = $executer->error;
+    
+    // Увеличиваем position у всех событий данной смены
+    $sql = "update plan_event set position = ifnull(position, 1) + 1 "
+            . "where date = '".$plan_continuation->date."' and shift = '".$plan_continuation->shift."' and machine_id = $machine_id";
+    $executer = new Executer($sql);
+    $error = $executer->error;
     
     // Создаём допечатку
-    echo $plan_continuation->date.' -- '.$plan_continuation->shift.' -- '.$plan_continuation->plan_edition_id.' -- '.$plan_continuation->worktime.' -- '.$plan_continuation->has_continuation."<br />";
+    $sql = "insert into plan_continuation (date, shift, plan_edition_id, worktime, has_continuation) "
+            . "values ('".$plan_continuation->date."', '".$plan_continuation->shift."', ".$plan_continuation->plan_edition_id.", ".$plan_continuation->worktime.", ".$plan_continuation->has_continuation.")";
+    $executer = new Executer($sql);
+    $error = $executer->error;
     
     $continuation_time -= 12;
 }
