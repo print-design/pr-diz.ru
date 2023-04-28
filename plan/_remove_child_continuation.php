54a589260ffdd2ee77a67197bdebb25aa3d6e7e4
user <user@Dmitry>
2023-04-28 14:57:54 +0300

    28.04.2023 план события

--- plan/_remove_child_continuation.php
+++ plan/_remove_child_continuation.php
@@ -4,12 +4,34 @@ require_once '../include/topscripts.php';
 $id = filter_input(INPUT_GET, 'id');
 $error = "";
 
-$sql = "update plan_continuation set has_continuation = 0, worktime = worktime + (select sum(worktime) from plan_continuation where plan_edition_id = (select plan_edition_id from plan_continuation where id = $id) and id > $id) where id = $id";
-$executer = new Executer($sql);
-$error = $executer->error;
+$plan_edition_id = 0;
+
+$sql = "select plan_edition_id from plan_continuation where id = $id";
+$fetcher = new Fetcher($sql);
+$error = $fetcher->error;
+if($row = $fetcher->Fetch()) {
+    $plan_edition_id = $row[0];
+}
+
+$sum_worktime = 0;
 
 if(empty($error)) {
-    $sql = "delete from plan_continuation where plan_edition_id = (select plan_edition_id from plan_continuation where id = $id) and id > $id";
+    $sql = "select sum(worktime) from plan_continuation where plan_edition_id = $plan_edition_id and id > $id";
+    $fetcher = new Fetcher($sql);
+    $error = $fetcher->error;
+    if($row = $fetcher->Fetch()) {
+        $sum_worktime = $row[0];
+    }
+}
+
+if(empty($error)) {
+    $sql = "update plan_continuation set has_continuation = 0, worktime = worktime + $sum_worktime where id = $id";
+    $executer = new Executer($sql);
+    $error = $executer->error;
+}
+
+if(empty($error)) {
+    $sql = "delete from plan_continuation where plan_edition_id = $plan_edition_id and id > $id";
     $executer = new Executer($sql);
     $error = $executer->error;
 }
