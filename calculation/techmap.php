d58fe72a18149dd5b9f0c208444fc3670cdd94bc
user <user@Dmitry>
2023-06-01 14:23:18 +0300

    01.06.2023 тестирование

--- calculation/techmap.php
+++ calculation/techmap.php
@@ -470,6 +470,7 @@ if(!empty($waste2) && $waste2 != $waste1) $waste = WASTE_KAGAT;
 if(!empty($waste3) && $waste3 != $waste2) $waste = WASTE_KAGAT;
 
 $machine_coeff = null;
+
 if(!empty($machine_id)) {
     $machine_coeff = GetMachineCoeff(PRINTER_SHORTNAMES[$machine_id]);
 }
