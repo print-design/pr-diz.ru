e11d78a73dff03401bc4273c023bc44b082f2f1e
user <user@Dmitry>
2023-06-22 11:37:49 +0300

    22.06.2023 тестирование

--- include/constants.php
+++ include/constants.php
@@ -45,7 +45,7 @@ const CUTTER_SOMA = 102;
 
 const CUTTERS = array(CUTTER_1, CUTTER_2, CUTTER_3, CUTTER_4, CUTTER_SOMA, CUTTER_ATLAS);
 const CUTTER_NAMES = array(CUTTER_1 => "Резка 1", CUTTER_2 => "Резка 2", CUTTER_3 => "Резка 3", CUTTER_4 => "Резка 4", CUTTER_ATLAS => "Резка &laquo;Атлас&raquo;", CUTTER_SOMA => "Резка &laquo;Сома&raquo;");
-const CUTTER_SPEEDS = array(CUTTER_1 => 70, CUTTER_2 => 70, CUTTER_3 => 70, CUTTER_4 => 120, CUTTER_ATLAS => 70, CUTTER_SOMA => 120);
+const CUTTER_SPEEDS = array(CUTTER_1 => 35, CUTTER_2 => 35, CUTTER_3 => 35, CUTTER_4 => 80, CUTTER_ATLAS => 35, CUTTER_SOMA => 120);
 
 // Типы работы
 const WORK_TYPE_NOPRINT = 1;
