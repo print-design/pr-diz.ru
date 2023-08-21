41edc94c3be08b5bd42e17773e50303ca8ea90d7
user <user@Dmitry>
2023-08-21 15:37:48 +0300

    21.08.2023 autocomplete off

--- admin/gap.php
+++ admin/gap.php
@@ -119,50 +119,17 @@ if($row = $fetcher->Fetch()) {
                         <input type="hidden" id="machine_id" name="machine_id" value="<?= filter_input(INPUT_GET, 'machine_id') ?>" />
                         <div class="form-group">
                             <label for="gap_raport">ЗазорРапорт (минимальное расстояние между этикетками), мм</label>
-                            <input type="text" 
-                                   class="form-control float-only<?=$gap_raport_valid ?>" 
-                                   id="gap_raport" 
-                                   name="gap_raport" 
-                                   value="<?= empty($gap_raport) ? "" : floatval($gap_raport) ?>" 
-                                   placeholder="ЗазорРапорт, мм" 
-                                   required="required" 
-                                   onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
-                                   onmouseup="javascript: $(this).attr('id', 'gap_raport'); $(this).attr('name', 'gap_raport'); $(this).attr('placeholder', 'ЗазорРапорт, мм');" 
-                                   onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder'); }" 
-                                   onkeyup="javascript: $(this).attr('id', 'gap_raport'); $(this).attr('name', 'gap_raport'); $(this).attr('placeholder', 'ЗазорРапорт, мм');" 
-                                   onfocusout="javascript: $(this).attr('id', 'gap_raport'); $(this).attr('name', 'gap_raport'); $(this).attr('placeholder', 'ЗазорРапорт, мм');" />
+                            <input type="text" class="form-control float-only<?=$gap_raport_valid ?>" id="gap_raport" name="gap_raport" value="<?= empty($gap_raport) ? "" : floatval($gap_raport) ?>" placeholder="ЗазорРапорт, мм" required="required" autocomplete="off" />
                             <div class="invalid-feedback">ЗазорРапорт обязательно</div>
                         </div>
                         <div class="form-group">
                             <label for="gap_stream">ЗазорРучей (минимальное расстояние между ручьями), мм</label>
-                            <input type="text" 
-                                   class="form-control float-only<?=$gap_stream_valid ?>" 
-                                   id="gap_stream" 
-                                   name="gap_stream" 
-                                   value="<?= empty($gap_stream) ? "" : floatval($gap_stream) ?>" 
-                                   placeholder="ЗазорРучей, мм" 
-                                   required="required" 
-                                   onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
-                                   onmouseup="javascript: $(this).attr('id', 'gap_stream'); $(this).attr('name', 'gap_stream'); $(this).attr('placeholder', 'ЗазорРучей, мм');" 
-                                   onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder'); }" 
-                                   onkeyup="javascript: $(this).attr('id', 'gap_stream'); $(this).attr('name', 'gap_stream'); $(this).attr('placeholder', 'ЗазорРучей, мм');" 
-                                   onfocusout="javascript: $(this).attr('id', 'gap_stream'); $(this).attr('name', 'gap_stream'); $(this).attr('placeholder', 'ЗазорРучей, мм');" />
+                            <input type="text" class="form-control float-only<?=$gap_stream_valid ?>" id="gap_stream" name="gap_stream" value="<?= empty($gap_stream) ? "" : floatval($gap_stream) ?>" placeholder="ЗазорРучей, мм" required="required" autocomplete="off" />
                             <div class="invalid-feedback">ЗазорРучей обязательно</div>
                         </div>
                         <div class="form-group">
                             <label for="ski">Ширина 1 лыжи, мм</label>
-                            <input type="text" 
-                                   class="form-control float-only<?=$ski_valid ?>" 
-                                   id="ski" 
-                                   name="ski" 
-                                   value="<?= empty($ski) ? "" : floatval($ski) ?>" 
-                                   placeholder="Ширина 1 лыжи, мм" 
-                                   required="required" 
-                                   onmousedown="javascript: $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder');" 
-                                   onmouseup="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" 
-                                   onkeydown="javascript: if(event.which != 10 && event.which != 13) { $(this).removeAttr('id'); $(this).removeAttr('name'); $(this).removeAttr('placeholder'); }" 
-                                   onkeyup="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" 
-                                   onfocusout="javascript: $(this).attr('id', 'ski'); $(this).attr('name', 'ski'); $(this).attr('placeholder', 'Ширина 1 лыжи, мм');" />
+                            <input type="text" class="form-control float-only<?=$ski_valid ?>" id="ski" name="ski" value="<?= empty($ski) ? "" : floatval($ski) ?>" placeholder="Ширина 1 лыжи, мм" required="required" autocomplete="off" />
                             <div class="invalid-feedback">Ширина 1 лыжи обязательно</div>
                         </div>
                         <button type="submit" id="norm_gap_submit" name="norm_gap_submit" class="btn btn-dark w-100 mt-5">Сохранить</button>
