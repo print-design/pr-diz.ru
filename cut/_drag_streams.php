a36e742d84e6dd573e03a0dfa5619b497536e254
user <user@Dmitry>
2023-11-23 11:32:01 +0300

    23.11.2023 съёмы

--- cut/_drag_streams.php
+++ cut/_drag_streams.php
@@ -4,33 +4,37 @@ $source_id = filter_input(INPUT_GET, 'source_id');
 $target_id = filter_input(INPUT_GET, 'target_id');
 $error = 'Ошибка при перетаскивании ручьёв';
 
+$source_calculation_id = 0;
 $source_position = 0;
-$target_position = 0;
 
-$sql = "select position from calculation_stream where id = $source_id";
+$sql = "select calculation_id, position from calculation_stream where id = $source_id";
 $fetcher = new Fetcher($sql);
 if($row = $fetcher->Fetch()) {
-    $source_position = $row[0];
+    $source_calculation_id = $row['calculation_id'];
+    $source_position = $row['position'];
 }
 
-$sql = "select position from calculation_stream where id = $target_id";
+$target_calculation_id = 0;
+$target_position = 0;
+
+$sql = "select calculation_id, position from calculation_stream where id = $target_id";
 $fetcher = new Fetcher($sql);
 if($row = $fetcher->Fetch()) {
-    $target_position = $row[0];
+    $target_calculation_id = $row['calculation_id'];
+    $target_position = $row['position'];
 }
 
 if($source_position < $target_position) {
     $sql = "update calculation_stream set position = position - 1 "
-            . "where calculation_id = (select calculation_id from calculation_stream where id = $source_id) "
-            . "and calculation_id = (select calculation_id from calculation_stream where id = $target_id) "
+            . "where calculation_id = $source_calculation_id "
+            . "and calculation_id = $target_calculation_id "
             . "and position > $source_position "
             . "and position < $target_position";
     $executer = new Executer($sql);
     $error = $executer->error;
     
     if(empty($error)) {
-        $sql = "update calculation_stream set position = $target_position - 1 "
-                . "where id = $source_id";
+        $sql = "update calculation_stream set position = $target_position - 1 where id = $source_id";
         $executer = new Executer($sql);
         $error = $executer->error;
     }
@@ -38,16 +42,15 @@ if($source_position < $target_position) {
 
 if($source_position > $target_position) {
     $sql = "update calculation_stream set position = position + 1 "
-            . "where calculation_id = (select calculation_id from calculation_stream where id = $source_id) "
-            . "and calculation_id = (select calculation_id from calculation_stream where id = $target_id) "
+            . "where calculation_id = $source_calculation_id "
+            . "and calculation_id = $target_calculation_id "
             . "and position >= $target_position "
             . "and position < $source_position";
     $executer = new Executer($sql);
     $error = $executer->error;
     
     if(empty($error)) {
-        $sql = "update calculation_stream set position = $target_position "
-                . "where id = $source_id";
+        $sql = "update calculation_stream set position = $target_position where id = $source_id";
         $executer = new Executer($sql);
         $error = $executer->error;
     }
