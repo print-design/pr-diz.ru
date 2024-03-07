7f812a1db65faa0831b4807ca9b064ef335c6960
user <user@Dmitry>
2024-03-07 12:16:59 +0300

    2024.03.07 - тестирование

--- personal/index.php
+++ personal/index.php
@@ -26,17 +26,21 @@ if($row = $fetcher->Fetch()) {
 if(IsInRole(CUTTER_USERS)) {
     $current_time = new DateTime();
     $current_time->setTimezone(new DateTimeZone('Europe/Moscow'));
-    $current_date_format = $current_time->format('d-m-Y');
     
     $current_hour = intval($current_time->format('G'));
     $current_shift = 'day';
-    if($current_hour < 8 || $current_hour > 19) {
+    $working_current_time = clone $current_time;
+    if($current_hour > 19 && $current_hour < 24) {
         $current_shift = 'night';
+    }
+    elseif ($current_hour >= 0 && $current_hour < 8) {
+        $current_shift = 'night';
+        $working_current_time->modify("-1 day");
     }
     
     $sql = "select pe.last_name, pe.first_name, pe.phone "
             . "from plan_workshift1 pw inner join plan_employee pe on pw.employee1_id = pe.id "
-            . "where date_format(pw.date, '%d-%m-%Y') = '$current_date_format' and pw.shift = '$current_shift' and pw.work_id = ".WORK_CUTTING.' and pw.machine_id = '. GetUserId();
+            . "where date_format(pw.date, '%d-%m-%Y') = '".$working_current_time->format('d-m-Y')."' and pw.shift = '$current_shift' and pw.work_id = ".WORK_CUTTING.' and pw.machine_id = '. GetUserId();
     $fetcher = new Fetcher($sql);
     if($row = $fetcher->Fetch()) {
         $last_name = $row['last_name'];
