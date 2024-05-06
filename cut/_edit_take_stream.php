6a88087aa5e9c439b4f30dcf3ff77062e804940f
user <user@Dmitry>
2024-05-06 08:58:29 +0300

    2024.05.06 - ТЗ 15 корректировка массы катушки

--- cut/_edit_take_stream.php
+++ cut/_edit_take_stream.php
@@ -23,12 +23,21 @@ if(null !== filter_input(INPUT_POST, 'edit_take_stream_submit')) {
     $location_get['take_stream_id'] = $id;
     $location_get['scroll'] = filter_input(INPUT_POST, 'scroll');
     
-    $sql = "update calculation_take_stream set weight = $weight, length = $length, radius = $radius where id = $id";
-    $executer = new Executer($sql);
-    $error_message = $executer->error;
+    $sql = "select calculation_take_id from calculation_take_stream where id = $id";
+    $fetcher = new Fetcher($sql);
     
-    if(empty($error_message)) {
-        header('Location: '.$location."?". http_build_query($location_get));
+    if($row = $fetcher->Fetch()) {
+        $location_get['take_id'] = $row['calculation_take_id'];
+        $sql = "update calculation_take_stream set weight = $weight, length = $length, radius = $radius where id = $id";
+        $executer = new Executer($sql);
+        $error_message = $executer->error;
+        
+        if(empty($error_message)) {
+            header('Location: '.$location."?". http_build_query($location_get));
+        }
+        else {
+            header('Location: '.APPLICATION.'/cut/');
+        }
     }
     else {
         header('Location: '.APPLICATION.'/cut/');
