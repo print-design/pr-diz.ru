d8d2f6f7b74a8c45ceb6e4f02f36bf0149e70cd2
user <user@Dmitry>
2024-05-07 16:09:13 +0300

    2024.05.07 - ТЗ 15 список валов ламинации

--- calculation/create.php
+++ calculation/create.php
@@ -2044,6 +2044,10 @@ if((!empty($lamination1_film_id) || !empty($lamination1_individual_film_name)) &
                                         <?php
                                         if(!empty($laminator_id)):
                                             $sql = "select value from norm_laminator_roller where laminator_id = $laminator_id and active = 1 ";
+                                            if(!empty($stream_width) && !empty($streams_number)) {
+                                                $min_width = $stream_width * $streams_number;
+                                                $sql .= "and value >= $min_width ";
+                                            }
                                             if(!empty($lamination_roller_width)) {
                                                 $sql .= "union select value from norm_laminator_roller where laminator_id = $laminator_id and active = 0 and value = $lamination_roller_width ";
                                             }
