cc179f5235dd640aec65957f8cf6396f87c03f7c
user <user@Dmitry>
2024-05-13 14:36:51 +0300

    2024.05.13 - ТЗ 15 список валов ламинации

--- plan/_plan_timetable_print.php
+++ plan/_plan_timetable_print.php
@@ -8,7 +8,7 @@
         <th class="storekeeper_hidden">Метраж</th>
         <th class="cutting_hidden lamination_hidden storekeeper_hidden planner_hidden colorist_hidden">Кол-во образцов</th>
         <th class="cutting_hidden lamination_hidden storekeeper_hidden colorist_hidden">Вал</th>
-        <th class="cutting_hidden text-nowrap">Лам-й</th>
+        <th class="cutting_hidden">Лам-й</th>
         <th class="cutting_hidden lamination_hidden storekeeper_hidden">Кр-ть</th>
         <th class="not_colorist_hidden">Краски</th>
         <th class="storekeeper_hidden colorist_hidden">Время</th>
