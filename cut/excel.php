65f4689ec587937eb99a9edcfa22ef9d62557107
Дмитрий <@DIMA>
2025-08-19 14:26:21 +0300

    2025-08-15 не точно считается выработка резчиков

--- cut/excel.php
+++ cut/excel.php
@@ -47,21 +47,23 @@ foreach($cutters as $cutter) {
     $sheet->getColumnDimension('G')->setAutoSize(true);
     $sheet->getColumnDimension('H')->setAutoSize(true);
     $sheet->getColumnDimension('I')->setAutoSize(true);
+    $sheet->getColumnDimension('J')->setAutoSize(true);
         
     $rowindex = 1;
-        
+    
     $sheet->setCellValue('A'.$rowindex, "Дата");
-    $sheet->setCellValue('B'.$rowindex, "День/Ночь");
-    $sheet->setCellValue('C'.$rowindex, "ФИО резчика");
-    $sheet->setCellValue('D'.$rowindex, "ID заказа");
-    $sheet->setCellValue('E'.$rowindex, "Заказчик");
-    $sheet->setCellValue('F'.$rowindex, "Заказ");
-    $sheet->setCellValue('G'.$rowindex, "Кг/Шт");
-    $sheet->setCellValue('H'.$rowindex, "Выполненный метраж");
-    $sheet->setCellValue('I'.$rowindex, "Выполненная масса");
+    $sheet->setCellValue('B'.$rowindex, "В плане");
+    $sheet->setCellValue('C'.$rowindex, "День/Ночь");
+    $sheet->setCellValue('D'.$rowindex, "ФИО резчика");
+    $sheet->setCellValue('E'.$rowindex, "ID заказа");
+    $sheet->setCellValue('F'.$rowindex, "Заказчик");
+    $sheet->setCellValue('G'.$rowindex, "Заказ");
+    $sheet->setCellValue('H'.$rowindex, "Кг/Шт");
+    $sheet->setCellValue('I'.$rowindex, "Выполненный метраж");
+    $sheet->setCellValue('J'.$rowindex, "Выполненная масса");
     
     // Тиражи
-    $sql = "select distinct date(cts.printed) printed, ped.date, ped.shift, ped.position, ". PLAN_TYPE_EDITION." as type, pem.last_name, pem.first_name, c.customer_id, "
+    $sql = "select distinct date(cts.printed) printed, ped.date, ped.shift, ped.position, pem.last_name, pem.first_name, c.customer_id, "
             . "(select count(id) from calculation where customer_id = c.customer_id and id <= c.id) as num_for_customer, "
             . "cus.name customer, c.name calculation, c.unit, "
             . "(select sum(length) from calculation_take_stream where printed between '".$date_from->format('Y-m-d')."' and '".((clone $date_to)->add($diff1Day))->format('Y-m-d')."' and plan_employee_id = cts.plan_employee_id and date(printed) = date(cts.printed) and calculation_stream_id in (select id from calculation_stream where calculation_id = c.id)) length_cut, "
@@ -78,7 +80,7 @@ foreach($cutters as $cutter) {
     }
     $sql .= " and ped.id not in (select plan_edition_id from plan_continuation) "
             . "union "
-            . "select distinct date(cts.printed) printed, pc.date, pc.shift, 1 as position, ". PLAN_TYPE_CONTINUATION." as type, pem.last_name, pem.first_name, c.customer_id, "
+            . "select distinct date(cts.printed) printed, pc.date, pc.shift, 1 as position, pem.last_name, pem.first_name, c.customer_id, "
             . "(select count(id) from calculation where customer_id = c.customer_id and id <= c.id) as num_for_customer, "
             . "cus.name customer, c.name calculation, c.unit, "
             . "(select sum(length) from calculation_take_stream where printed between '".$date_from->format('Y-m-d')."' and '".((clone $date_to)->add($diff1Day))->format('Y-m-d')."' and plan_employee_id = cts.plan_employee_id and date(printed) = date(cts.printed) and calculation_stream_id in (select id from calculation_stream where calculation_id = c.id)) length_cut, "
@@ -95,43 +97,21 @@ foreach($cutters as $cutter) {
         $sql .= " and ped.machine_id = ".$cutter;
     }
     $sql .= " and pc.has_continuation = false "
-            . "order by printed";
+            . "order by printed, last_name, first_name";
     $fetcher = new Fetcher($sql);
     while ($row = $fetcher->Fetch()) {
-        $rowindex++;
-        
-        $sheet->setCellValue('A'.$rowindex, DateTime::createFromFormat("Y-m-d", $row['date'])->format('d.m.Y'));
-        $sheet->setCellValue('B'.$rowindex, $row['shift'] == "day" ? "День" : "Ночь");
-        $sheet->setCellValue('C'.$rowindex, $row['last_name'].' '.(empty($row['first_name']) ? '' : mb_substr($row['first_name'], 0, 1).'.'));
-        $sheet->setCellValue('D'.$rowindex, $row['customer_id'].'-'.$row['num_for_customer']);
-        $sheet->setCellValue('E'.$rowindex, $row['customer']);
-        $sheet->setCellValue('F'.$rowindex, $row['calculation'].($row['type'] == PLAN_TYPE_CONTINUATION ? ' (дорезка)' : ''));
-        $sheet->setCellValue('G'.$rowindex, $row['unit'] == 'kg' ? "Кг" : "Шт");
-        $sheet->setCellValue('H'.$rowindex, $row['length_cut']);
-        $sheet->setCellValue('I'.$rowindex, $row['weight_cut']);
-        
-        // Подсчёт суммы
-        /*if($cutter == CUTTERS_ALL) {
-            if($row['unit'] == KG) {
-                if(key_exists($key, $workshifts) && key_exists($workshifts[$key], $employees)) {
-                    if(!key_exists(KG, $employees[$workshifts[$key]])) {
-                        $employees[$workshifts[$key]][KG] = 0.0;
-                    }
-                    
-                    $employees[$workshifts[$key]][KG] = floatval($employees[$workshifts[$key]][KG]) + (strval(floatval($row['weight_cut']) * floatval($row['worktime']) / floatval($row['worktime_cut'])) / 1000);
-                }
-            }
-            
-            if($row['unit'] == PIECES) {
-                if(key_exists($key, $workshifts) && key_exists($workshifts[$key], $employees)) {
-                    if(!key_exists(PIECES, $employees[$workshifts[$key]])) {
-                        $employees[$workshifts[$key]][PIECES] = 0.0;
-                    }
-                    
-                    $employees[$workshifts[$key]][PIECES] = floatval($employees[$workshifts[$key]][PIECES]) + (strval($length_cut * floatval($row['worktime']) / floatval($row['worktime_cut'])) / 1000);
-                }
-            }
-        }*/
+        $sheet->setCellValue('A'.(++$rowindex), DateTime::createFromFormat("Y-m-d", $row['printed'])->format('d.n.Y'));
+        $sheet->setCellValue('B'.$rowindex, DateTime::createFromFormat("Y-m-d", $row['date'])->format('d.m.Y'));
+        $sheet->setCellValue('C'.$rowindex, $row['shift'] == "day" ? "День" : "Ночь");
+        $sheet->setCellValue('D'.$rowindex, $row['last_name'].' '.(empty($row['first_name']) ? '' : mb_substr($row['first_name'], 0, 1).'.'));
+        $sheet->setCellValue('E'.$rowindex, $row['customer_id'].'-'.$row['num_for_customer']);
+        $sheet->setCellValue('F'.$rowindex, $row['customer']);
+        $sheet->setCellValue('G'.$rowindex, $row['calculation']);
+        $sheet->setCellValue('H'.$rowindex, $row['unit'] == 'kg' ? "Кг" : "Шт");
+        $sheet->getStyle('I'.$rowindex)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER);
+        $sheet->setCellValue('I'.$rowindex, $row['length_cut']);
+        $sheet->getStyle('J'.$rowindex)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+        $sheet->setCellValue('J'.$rowindex, $row['weight_cut']);
     }
     
     $activeSheetIndex++;
@@ -157,28 +137,33 @@ $sheet->getStyle('C2')->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NU
 $sheet->setCellValue('C2', '0');
 $sheet->setCellValue('D3', "Итого ₽");
 
-$row_number = 4;
+$rowindex = 3;
 
-/*foreach($employees_sorted as $employee_id) {
-    if(key_exists($employee_id, $employees) && in_array($employee_id, $workshifts)) {
-        $sheet->setCellValue('A'.$row_number, $employees[$employee_id]['last_name'].' '.$employees[$employee_id]['first_name']);
-        
-        if(key_exists(KG, $employees[$employee_id]) && !empty($employees[$employee_id][KG])) {
-            $sheet->getStyle('B'.$row_number)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
-            $sheet->setCellValue('B'.$row_number, strval($employees[$employee_id][KG]));
-        }
-        
-        if(key_exists(PIECES, $employees[$employee_id]) && !empty($employees[$employee_id][PIECES])) {
-            $sheet->getStyle('C'.$row_number)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
-            $sheet->setCellValue('C'.$row_number, strval($employees[$employee_id][PIECES]));
-        }
-        
-        $sheet->getStyle('D'.$row_number)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
-        $sheet->setCellValue('D'.$row_number, '=PRODUCT(B2,B'.$row_number.')+PRODUCT(C2,C'.$row_number.')');
-        
-        $row_number++;
-    }
-}*/
+$sql = "select distinct pem.id, pem.last_name, pem.first_name, "
+        . "(select sum(cts1.weight) / 1000 "
+        . "from calculation_take_stream cts1 "
+        . "inner join calculation_stream cs1 on cts1.calculation_stream_id = cs1.id "
+        . "inner join calculation c1 on cs1.calculation_id = c1.id "
+        . "where cts1.printed between '".$date_from->format('Y-m-d')."' and '".((clone $date_to)->add($diff1Day))->format('Y-m-d')."' and cts1.plan_employee_id = pem.id and c1.unit = '".KG."') weight, "
+        . "(select sum(cts1.length) / 1000 "
+        . "from calculation_take_stream cts1 "
+        . "inner join calculation_stream cs1 on cts1.calculation_stream_id = cs1.id "
+        . "inner join calculation c1 on cs1.calculation_id = c1.id "
+        . "where cts1.printed between '".$date_from->format('Y-m-d')."' and '".((clone $date_to)->add($diff1Day))->format('Y-m-d')."' and cts1.plan_employee_id = pem.id and c1.unit = '".PIECES."') length "
+        . "from calculation_take_stream cts "
+        . "left join plan_employee pem on cts.plan_employee_id = pem.id "
+        . "where cts.printed between '".$date_from->format('Y-m-d')."' and '".((clone $date_to)->add($diff1Day))->format('Y-m-d')."' "
+        . "order by pem.last_name, pem.first_name";
+$fetcher = new Fetcher($sql);
+while($row = $fetcher->Fetch()) {
+    $sheet->setCellValue('A'.(++$rowindex), $row['last_name'].' '.(empty($row['first_name']) ? '' : mb_substr($row['first_name'], 0, 1).'.'));
+    $sheet->getStyle('B'.$rowindex)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+    $sheet->setCellValue('B'.$rowindex, $row['weight']);
+    $sheet->getStyle('C'.$rowindex)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+    $sheet->setCellValue('C'.$rowindex, $row['length']);
+    $sheet->getStyle('D'.$rowindex)->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1);
+    $sheet->setCellValue('D'.$rowindex, '=(B2*B'.$rowindex.')+(C2*C'.$rowindex.')');
+}
 
 // Сохранение
 $filename = "Резчики_".$date_from->format('Y-m-d')."_".$date_to->format('Y-m-d').".xlsx";
