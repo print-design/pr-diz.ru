1a105cc9f46b9c0ad64fe30754be2626561914ed
Дмитрий <@DMITRY>
2025-11-25 16:05:41 +0300

    25.11.2025 фильтр у упаковщицы

--- pack/index.php
+++ pack/index.php
@@ -48,46 +48,53 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
         <?php
         include '../include/header_pack.php';
         include '../include/status_track.php';
+        include '../include/pager_top.php';
+        $rowcounter = 0;
         ?>
         <div class="container-fluid">
             <?php
             if(!empty($error_message)) {
                 echo "<div class='alert alert-danger'>$error_message</div>";
             }
-            
-            include '../include/pager_top.php';
-            $rowcounter = 0;
-            
-            // Фильтр
-            $filter = '';
-            
-            $find = trim(filter_input(INPUT_GET, 'find') ?? '');
-            if(!empty($find)) {
-                $find_substrings = explode('-', $find);
-                if(count($find_substrings) != 2 || intval($find_substrings[0]) == 0 || intval($find_substrings[1]) == 0) {
-                    $filter .= " and false";
-                }
-                else {
-                    $filter .= " and c.customer_id = ". intval($find_substrings[0])." and (select count(id) from calculation where customer_id = c.customer_id and id <= c.id) = ". intval($find_substrings[1]);
-                }
-            }
-            
-            // Общее количество работ для установления количества страниц в постраничном выводе
-            $sql = "select count(c.id) "
-                    . "from calculation c "
-                    . "inner join plan_edition e on e.calculation_id = c.id "
-                    . "inner join (select calculation_id, max(timestamp) as time from calculation_take group by calculation_id) ct on ct.calculation_id = c.id "
-                    . "where (select status_id from calculation_status_history where calculation_id = c.id order by date desc limit 1) = $status_id and e.work_id = ".WORK_CUTTING.$filter;
-            $fetcher = new Fetcher($sql);
-            
-            if($row = $fetcher->Fetch()) {
-                $pager_total_count = $row[0];
-            }
             ?>
-            <h1>Готовые резки&nbsp;&nbsp;<span style="font-size: smaller; color: #999999;"><?=$pager_total_count ?></span></h1>
+            <div class="d-flex justify-content-between mb-auto">
+                <div class="text-nowrap">
+                    <h1>Готовые резки&nbsp;&nbsp;<span style="font-size: smaller; color: #999999;"><?=$pager_total_count ?></span></h1>
+                    <?php
+                    // Фильтр
+                    $filter = '';
+                    
+                    $find = trim(filter_input(INPUT_GET, 'find') ?? '');
+                    if(!empty($find)) {
+                        $find_substrings = explode('-', $find);
+                        if(count($find_substrings) != 2 || intval($find_substrings[0]) == 0 || intval($find_substrings[1]) == 0) {
+                            $filter .= " and false";
+                        }
+                        else {
+                            $filter .= " and c.customer_id = ". intval($find_substrings[0])." and (select count(id) from calculation where customer_id = c.customer_id and id <= c.id) = ". intval($find_substrings[1]);
+                        }
+                    }
+                    
+                    // Общее количество работ для установления количества страниц в постраничном выводе
+                    $sql = "select count(c.id) "
+                            . "from calculation c "
+                            . "inner join plan_edition e on e.calculation_id = c.id "
+                            . "inner join (select calculation_id, max(timestamp) as time from calculation_take group by calculation_id) ct on ct.calculation_id = c.id "                    . "where (select status_id from calculation_status_history where calculation_id = c.id order by date desc limit 1) = $status_id and e.work_id = ".WORK_CUTTING.$filter;
+                    $fetcher = new Fetcher($sql);
+                    
+                    if($row = $fetcher->Fetch()) {
+                        $pager_total_count = $row[0];
+                    }
+                    ?>
+                </div>
+                <div></div>
+            </div>            
             <table class="table typography">
                 <tr>
                     <th class="text-nowrap">Дата<i class='fas fa-arrow-down ml-2' style="color: #BBBBBB;"></i></th>
+                    <?php if($status_id == ORDER_STATUS_SHIPPED): ?>
+                    <th>Дата отгрузки</th>
+                    <?php endif; ?>
                     <th>№</th>
                     <th>Заказ</th>
                     <th>Метраж</th>
@@ -99,6 +106,8 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
                 </tr>
             <?php
             $sql = "select distinct c.id, ct.time, c.customer_id, e.machine_id, e.comment, pc.comment as continuation_comment, cus.name as customer, c.name as calculation, cr.length_pure_1, concat(u.last_name, ' ', left(first_name, 1), '.') as manager, c.raport, c.length, c.unit, c.quantity, "
+                    . ($status_id == ORDER_STATUS_SHIPPED ? "ss.shipping_date, " : "")
+                    //. "cq.quantity_sum, "
                     . "(select sum(quantity) from calculation_quantity where calculation_id = c.id) quantity_sum, "
                     . "(select sum(weight) from calculation_take_stream where calculation_take_id in (select id from calculation_take where calculation_id = c.id)) as weight, "
                     . "(select gap_raport from norm_gap where date <= c.date order by id desc limit 1) as gap_raport, "
@@ -115,7 +124,9 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
                     . "inner join calculation_result cr on cr.calculation_id = c.id "
                     . "inner join user u on c.manager_id = u.id "
                     . "inner join (select calculation_id, max(timestamp) as time from calculation_take group by calculation_id) ct on ct.calculation_id = c.id "
+                    . ($status_id == ORDER_STATUS_SHIPPED ? "left join (select calculation_id, max(date) as shipping_date from calculation_status_history where status_id = ". ORDER_STATUS_SHIPPED." group by calculation_id) ss on ss.calculation_id = c.id " : "")
                     . "left join plan_continuation pc on pc.plan_edition_id = e.id "
+                    . "left join (select calculation_id, sum(quantity) as quantity_sum from calculation_quantity group by calculation_id) cq on cq.calculation_id = c.id "
                     . "where (select status_id from calculation_status_history where calculation_id = c.id order by date desc limit 1) = $status_id and e.work_id = ".WORK_CUTTING.$filter
                     . " order by ct.time desc limit $pager_skip, $pager_take";
             $fetcher = new Fetcher($sql);
@@ -124,6 +135,12 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
             ?>
                 <tr>
                     <td><?=$datetime->format('d.m') ?><br /><span style="font-size: smaller;"><?=$datetime->format('H:i') ?></span></td>
+                    <?php
+                    if($status_id == ORDER_STATUS_SHIPPED):
+                        $shipping_date = DateTime::createFromFormat('Y-m-d H:i:s', $row['shipping_date']);
+                    ?>
+                    <td><?=$shipping_date->format('d.m') ?><br /><span style="font-size: smaller;"><?=$shipping_date->format("H:i") ?></span></td>
+                    <?php endif; ?>
                     <td><?=$row['customer_id'].'-'.$row['num_for_customer'] ?></td>
                     <td><?=$row['calculation'] ?><br /><span style="font-size: smaller;"><?=$row['customer'] ?></span></td>
                     <td class="text-nowrap"><?= DisplayNumber(floatval($row['length_pure_1']), 0) ?> м</td>
