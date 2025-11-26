66b290680b89fcab0c23caa35a2fd48efb4b88e8
Дмитрий <@DMITRY>
2025-11-26 16:51:08 +0300

    25.11.2025 фильтр у упаковщицы

--- pack/index.php
+++ pack/index.php
@@ -29,6 +29,7 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
         <?php
         include '../include/head.php';
         ?>
+        <link href="<?=APPLICATION ?>/css/select2.min.css" rel="stylesheet"/>
         <style>
             table.typography {
                 border-radius: 15px;
@@ -58,8 +59,8 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
             }
             ?>
             <div class="d-flex justify-content-between mb-auto">
-                <div class="text-nowrap">
-                    <h1>Готовые резки&nbsp;&nbsp;<span style="font-size: smaller; color: #999999;"><?=$pager_total_count ?></span></h1>
+                <div class="p-1 text-nowrap">
+                    <h1 class="d-inline">Готовые резки</h1>
                     <?php
                     // Фильтр
                     $filter = '';
@@ -71,7 +72,7 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
                     
                     $work_type = filter_input(INPUT_GET, 'work_type');
                     if(!empty($work_type)) {
-                        $filter .= " and c.work_type = $work_type";
+                        $filter .= " and c.work_type_id = $work_type";
                     }
                     
                     $manager = filter_input(INPUT_GET, 'manager');
@@ -113,8 +114,76 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
                         $pager_total_count = $row[0];
                     }
                     ?>
+                    <div class="d-inline ml-3" style="color: gray; font-size: x-large;"><?=$pager_total_count ?></div>
                 </div>
-                <div></div>
+                <div class="p-1 text-nowrap">
+                    <?php
+                    $status_id = filter_input(INPUT_GET, 'status_id');
+                    ?>
+                    <form class="form-inline d-inline" method="get">
+                        <?php if(null !== $status_id): ?>
+                        <input type="hidden" name="status_id" value="<?=$status_id ?>" />
+                        <?php endif; ?>
+                        <select id="unit" name="unit" class="form-control" multiple="multiple" onchange="javascript: this.form.submit();">
+                            <option value="">Шт/кг...</option>
+                            <option value="<?= PIECES ?>"<?= filter_input(INPUT_GET, 'unit') == PIECES ? " selected='selected'" : "" ?>>Шт</option>
+                            <option value="<?= KG ?>"<?= filter_input(INPUT_GET, 'unit') == KG ? " selected='selected'" : "" ?>>Кг</option>
+                        </select>
+                        <select id="work_type" name="work_type" class="form-control" multiple="multiple" onchange="javascript: this.form.submit();">
+                            <option value="">Тип работы...</option>
+                            <?php foreach(WORK_TYPES as $item): ?>
+                            <option value="<?=$item ?>"<?= ($item == filter_input(INPUT_GET, 'work_type') ? " selected='selected'" : "") ?>><?= WORK_TYPE_NAMES[$item] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <select id="manager" name="manager" class="form-control" multiple="multiple" onchange="javascript: this.form.submit();">
+                            <option value="">Менеджер...</option>
+                            <?php
+                            $sql = "select distinct u.id, u.last_name, u.first_name from calculation c inner join user u on c.manager_id = u.id order by u.last_name, u.first_name";
+                            $fetcher = new Fetcher($sql);
+                            while ($row = $fetcher->Fetch()):
+                            ?>
+                            <option value="<?=$row['id'] ?>"<?=$row['id'] == filter_input(INPUT_GET, 'manager') ? " selected='selected'" : "" ?>><?=(mb_strlen($row['first_name']) == 0 ? '' : mb_substr($row['first_name'], 0, 1).'. ').$row['last_name'] ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                        <select id="customer" name="customer" class="form-control" multiple="multiple" onchange="javascript: this.form.submit();">
+                            <option value="">Заказчик...</option>
+                            <?php
+                            $sql = "select distinct cus.id, cus.name from calculation c inner join customer cus on c.customer_id = cus.id order by cus.name";
+                            $fetcher = new Fetcher($sql);
+                            while($row = $fetcher->Fetch()):
+                            ?>
+                            <option value="<?=$row['id'] ?>"<?=$row['id'] == filter_input(INPUT_GET, 'customer') ? " selected='selected'" : "" ?>><?=$row['name'] ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                        <select id="name" name="name" class="form-control" multiple="multiple" onchange="javascript: this.form.submit();">
+                            <option value="">Наименование...</option>
+                            <?php
+                            $name_where = "";
+                            
+                            if(!empty($status_id)) {
+                                $name_where = "where (select status_id from calculation_status_history where calculation_id = c.id order by date desc limit 1) = $status_id";
+                            }
+                            
+                            $customer = filter_input(INPUT_GET, 'customer');
+                            if(!empty($customer)) {
+                                $name_where .= " and c.customer_id = $customer";
+                                
+                                $customer_manager = filter_input(INPUT_GET, 'manager');
+                                
+                                if(!empty($customer_manager)) {
+                                    $name_where .= " and c.manager_id = $customer_manager";
+                                }
+                            }
+                            
+                            $sql = "select distinct trim(c.name) name from calculation c $name_where order by trim(c.name)";
+                            $fetcher = new Fetcher($sql);
+                            while($row = $fetcher->Fetch()):
+                            ?>
+                            <option<?=$row['name'] == filter_input(INPUT_GET, 'name') ? " selected='selected'" : "" ?>><?=$row['name'] ?></option>
+                            <?php endwhile; ?>
+                        </select>
+                    </form>
+                </div>
             </div>            
             <table class="table table-hover typography">
                 <tr>
@@ -186,4 +255,49 @@ function ShowOrderStatus($status_id, $length_cut, $weight_cut, $quantity_sum, $q
         include '../include/footer.php';
         ?>
     </body>
+    <script src="<?= APPLICATION ?>/js/select2.min.js"></script>
+    <script src="<?= APPLICATION ?>/js/i18n.js"></script>
+    <script>
+        // Список с поиском
+        $('#unit').select2({
+            placeholder: "Шт/кг...",
+            maximumStatusLength: 1,
+            language: "ru",
+            width: '4rem'
+        });
+        
+        $('#status').select2({
+            placeholder: "Статус...",
+            maximumSelectionLength: 1,
+            language: "ru"
+        });
+        
+        $('#work_type').select2({
+            placeholder: "Тип работы...",
+            maximumSelectionLength: 1,
+            language: "ru",
+            width: '8rem'
+        });
+        
+        $('#manager').select2({
+            placeholder: "Менеджер...",
+            maximumSelectionLength: 1,
+            language: "ru",
+            width: "8rem"
+        });
+        
+        $('#customer').select2({
+            placeholder: "Заказчик...",
+            maximumSelectionLength: 1,
+            language: "ru",
+            width: "15rem"
+        });
+        
+        $('#name').select2({
+            placeholder: "Наименование...",
+            maximumSelectionLength: 1,
+            language: "ru",
+            width: "15rem"
+        });
+    </script>
 </html>
