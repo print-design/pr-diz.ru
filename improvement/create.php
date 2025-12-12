36fa78270376a0788296f98894794425a8f314e1
DMITRY <DMITRY@DMITRY>
2025-12-12 15:26:25 +0300

    improvement

---
+++ improvement/create.php
@@ -0,0 +1,50 @@
+<?php
+include '../include/topscripts.php';
+
+// Авторизация
+if(!IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_CUTTER]))) {
+    header('Location: '.APPLICATION.'/unauthorized.php');
+}
+?>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <?php
+        include '../include/style_mobile.php';
+        ?>
+    </head>
+    <body>
+        <div class="container-fluid header">
+            <nav class="navbar navbar-expand-sm justify-content-end">
+                <?php if(!empty(filter_input(INPUT_COOKIE, USERNAME))): ?>
+                <ul class="navbar-nav">
+                    <li class="nav-item dropdown no-dropdown-arrow-after">
+                        <a class="nav-link mr-0" href="<?=APPLICATION ?>/user_mobile.php?link=<?= urlencode($_SERVER['REQUEST_URI']) ?>"><i class="fa fa-cog" aria-hidden="true"></i></a>
+                    </li>
+                </ul>
+                <?php endif; ?>
+            </nav>
+        </div>
+        <div id="topmost"></div>
+        <div class="container-fluid">
+            <h1>Предложение по улучшению</h1>
+            <form method="post">
+                <div class="form-group">
+                    <label for="title">Заголовок</label>
+                    <input type="text" class="form-control" name="title" />
+                </div>
+                <div class="form-group">
+                    <label for="body">Текст предложения</label>
+                    <textarea class="form-control" name="body" rows="4"></textarea>
+                </div>
+            </form>
+        </div>
+        <?php
+        include '../include/footer.php';
+        include '../include/footer_mobile.php';
+        ?>
+    </body>
+</html>
