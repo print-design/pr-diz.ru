994a22e57ad12c83c9754cc1acc91df65591e959
DMITRY <DMITRY@DMITRY>
2025-12-18 10:30:19 +0300

    improvement

---
+++ misc/qrs.php
@@ -0,0 +1,91 @@
+<?php
+require '../vendor/autoload.php';
+use chillerlan\QRCode\QRCode;
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>50684 (676-31)</h1>
+        <?php
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        
+        $data = "1234567890";
+        $qrcode = (new QRCode)->render($data);
+        echo "<img src='$qrcode' alt='QR Code' />";
+        ?>
+    </body>
+</html>
