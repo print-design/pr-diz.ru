52f9a191861496810980e7170ef755d0d651aabe
DMITRY <DMITRY@DMITRY>
2025-12-22 15:12:15 +0300

    improvement

--- misc/qrs.php
+++ misc/qrs.php
@@ -5,9 +5,42 @@ use chillerlan\QRCode\QRCode;
 <!DOCTYPE html>
 <html>
     <body>
-        <h1>50684 (676-31)</h1>
+        <h1>Заказ 50684 (676-31)</h1>
+        <p><strong>Пример:</strong> Артикул (ТУ-2245-003-70398464-2016) + Дата резки (22.12.2025) + Номер партии плёнки (1241237) + ID ролика (24615)</p>
+        <hr />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524615"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524614"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524613"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524612"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524611"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+        
+        <?php $data = "ТУ-2245-003-70398464-201622.12.202524610"; ?>
+        <p><?=$data ?></p>
+        <?php $qrcode = (new QRCode)->render($data); ?>
+        <img src="<?=$qrcode ?>" alt="<?=$data ?>" width="400" height="400" />
+
         <?php
-        $data = "1234567890";
+        /*$data = "1234567890";
         $qrcode = (new QRCode)->render($data);
         echo "<img src='$qrcode' alt='QR Code' />";
         
@@ -85,7 +118,7 @@ use chillerlan\QRCode\QRCode;
         
         $data = "1234567890";
         $qrcode = (new QRCode)->render($data);
-        echo "<img src='$qrcode' alt='QR Code' />";
+        echo "<img src='$qrcode' alt='QR Code' />";*/
         ?>
     </body>
 </html>
