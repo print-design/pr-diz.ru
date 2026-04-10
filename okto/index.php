7c28cc88ef092101274c7f71b9550941cbfed55b
DMITRY <DMITRY@DMITRY>
2026-04-10 15:43:45 +0300

    Octopus

--- okto/index.php
+++ okto/index.php
@@ -58,11 +58,20 @@ if(!LoggedIn()) {
                     <div id="contacts" style="overflow: auto; position: absolute; top: 0px; bottom: 0; left: 0; right: 15px;">
                         <?php
                         $me = GetUserId();
-                        $sql = "select id, last_name, first_name from user where active = 1 and id <> $me order by last_name, first_name";
+                        $sql = "select u.id, u.last_name, u.first_name, "
+                                . "(select count(id) from dialog where user_id_from = u.id and user_id_to = $me and viewed = 0) unviewed "
+                                . "from user u where u.active = 1 and u.id <> $me "
+                                . "order by u.last_name, u.first_name";
                         $fetcher = new Fetcher($sql);
                         while($row = $fetcher->Fetch()):
                         ?>
-                        <button type="button" class="btn btn-light w-100 mt-1 mb-1 btn_contact" style="text-align: left;" data-id="<?=$row['id'] ?>"><?=$row['last_name'].' '.$row['first_name'] ?></button>
+                        <button type="button" class="btn btn-light w-100 mt-1 mb-1 btn_contact" style="text-align: left;" data-id="<?=$row['id'] ?>">
+                            <div class="d-flex justify-content-between">
+                                <div><?=$row['last_name'].' '.$row['first_name'] ?></div>
+                                <div><?=($row['unviewed'] > 0 ? $row['unviewed'] : '') ?></div>
+                            </div>
+                                
+                        </button>
                         <?php endwhile; ?>
                     </div>
                 </nav>
