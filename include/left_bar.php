926e04bb11c262c02e917a090f4cf63782d28512
DMITRY <DMITRY@DMITRY>
2026-05-27 16:19:10 +0300

    flexim left_bar.php

--- include/left_bar.php
+++ include/left_bar.php
@@ -46,28 +46,25 @@ elseif($folder == "okto") {
 
 ?>
 <aside class="flexim-nav-rail" aria-label="Левое меню">
-    <span class="flexim-logo__mark" aria-hidden="true"></span>
+    <a class="flexim-logo__mark" href="<?= APPLICATION ?>"></a>
     <nav class="flexim-nav-rail__tabs">
     <?php
     // Расчёты
     if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$zakaz_class ?>" aria-label="Заказы"><span data-flexim-icon="time" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/calculation/<?= BuildQueryAddRemoveArray("status", ORDER_STATUS_SHIPPED, array("page", "order")) ?>" class="left_bar_item ui_tooltip right<?=$zakaz_class ?>" title="Заказы"><img src="<?=APPLICATION ?>/images/nav_clock.svg" /></a>
+    <a href="<?=APPLICATION ?>/calculation/<?= BuildQueryAddRemoveArray("status", ORDER_STATUS_SHIPPED, array("page", "order")) ?>" type="button" class="flexim-nav-rail__tab<?=$zakaz_class ?>" aria-label="Заказы"><span data-flexim-icon="time" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     // Склад
     if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_STOREKEEPER], ROLE_NAMES[ROLE_MANAGER]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$sklad_class ?>" aria-label="Склад"><span data-flexim-icon="box" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/roll/" class="left_bar_item ui_tooltip right<?=$sklad_class ?>" title="Склад"><img src="<?=APPLICATION ?>/images/nav_sklad.svg" /></a>
+    <a href="<?=APPLICATION ?>/roll/" type="button" class="flexim-nav-rail__tab<?=$sklad_class ?>" aria-label="Склад"><span data-flexim-icon="box" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     // План
     if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE_NAMES[ROLE_SCHEDULER], ROLE_NAMES[ROLE_LAM_HEAD], ROLE_NAMES[ROLE_FLEXOPRINT_HEAD], ROLE_NAMES[ROLE_STOREKEEPER], ROLE_NAMES[ROLE_PACKER], ROLE_NAMES[ROLE_ACCOUNTANT], ROLE_NAMES[ROLE_COLORIST]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$plan_class ?>" aria-label="План"><span data-flexim-icon="calendar" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/plan/" class="left_bar_item ui_tooltip right<?=$plan_class ?>" title="План"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
+    <a href="<?=APPLICATION ?>/plan/" type="button" class="flexim-nav-rail__tab<?=$plan_class ?>" aria-label="План"><span data-flexim-icon="calendar" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     
@@ -75,8 +72,7 @@ elseif($folder == "okto") {
     /*ВРЕМЕННО*/
     if(GetUserId() == CUTTER_SOMA):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$plan_class ?>" aria-label="План"><span data-flexim-icon="calendar" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/plan/<?= BuildQueryAddRemoveArray("work_id", WORK_CUTTING, array("page", "order")) ?>" class="left_bar_item ui_tooltip right<?=$plan_class ?>" title="План"><img src="<?=APPLICATION ?>/images/nav_grafik.svg" /></a>
+    <a href="<?=APPLICATION ?>/plan/<?= BuildQueryAddRemoveArray("work_id", WORK_CUTTING, array("page", "order")) ?>" type="button" class="flexim-nav-rail__tab<?=$plan_class ?>" aria-label="План"><span data-flexim-icon="calendar" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     /*ВРЕМЕННО*/
@@ -84,43 +80,36 @@ elseif($folder == "okto") {
     // Резка
     if(IsInRole(CUTTER_USERS) || IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_LAM_HEAD]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$cut_class ?>" aria-label="Резка"><span data-flexim-icon="factory" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/cut/<?= IsInRole(CUTTER_USERS) ? "" : "?machine_id=".CUTTER_1 ?>" class="left_bar_item ui_tooltip right<?=$cut_class ?>" title="Резка"><img src="<?=APPLICATION ?>/images/icons/factory.svg" /></a>
+    <a href="<?=APPLICATION ?>/cut/<?= IsInRole(CUTTER_USERS) ? "" : "?machine_id=".CUTTER_1 ?>" type="button" class="flexim-nav-rail__tab<?=$cut_class ?>" aria-label="Резка"><span data-flexim-icon="factory" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     // Упаковка
     if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_PACKER], ROLE_NAMES[ROLE_ACCOUNTANT]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$pack_class ?>" aria-label="Упаковка"><span data-flexim-icon="loader-machine" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/pack/" class="left_bar_item ui_tooltip right<?=$pack_class ?>" title="Упаковка"><img src="<?=APPLICATION ?>/images/icons/loader_machine.svg" /></a>
+    <a href="<?=APPLICATION ?>/pack/" type="button" class="flexim-nav-rail__tab<?=$pack_class ?>" aria-label="Упаковка"><span data-flexim-icon="loader-machine" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     // Админка
     if(IsInRole(ROLE_NAMES[ROLE_TECHNOLOGIST])):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/user/" class="left_bar_item ui_tooltip right<?=$admin_class ?>" title="Админка"><img src="<?=APPLICATION ?>/images/nav_admin.svg" /></a>
+    <a href="<?=APPLICATION ?>/user/" type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></a>
     <?php
     elseif(IsInRole(array(ROLE_NAMES[ROLE_SCHEDULER], ROLE_NAMES[ROLE_LAM_HEAD]))):
     ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/admin/plan_employees.php" class="left_bar_item ui_tooltip right<?=$admin_class ?>" title="Админка"><img src="<?=APPLICATION ?>/images/nav_admin.svg" /></a>
+    <a href="<?=APPLICATION ?>/admin/plan_employees.php" type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></a>
     <?php
     endif;
     ?>
     
     <!-- Старший менеджер может редактировать константы -->
     <?php if(IsInRole(ROLE_NAMES[ROLE_MANAGER_SENIOR])): ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?=APPLICATION ?>/supplier/film.php" class="left_bar_item ui_tooltip right<?=$admin_class ?>" title="Админка"><img src="<?=APPLICATION ?>/images/nav_admin.svg" /></a>
+    <a href="<?=APPLICATION ?>/supplier/film.php" type="button" class="flexim-nav-rail__tab<?=$admin_class ?>" aria-label="Админка"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></a>
     <?php endif; ?>
     
     <!-- Предложения по улучшению -->
     <?php if(IsInRole(ROLE_NAMES[ROLE_MANAGER_SENIOR]) || IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_SCHEDULER], ROLE_NAMES[ROLE_LAM_HEAD], ROLE_NAMES[ROLE_FLEXOPRINT_HEAD]))): ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$improvement_class ?>" aria-label="Предложения по улучшению"><span data-flexim-icon="like" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?= APPLICATION ?>/improvement/" class="left_bar_item ui_tooltip right<?=$improvement_class ?>" title="Предложения по улучшению"><i class="far fa-thumbs-up" style="font-size: 1.3rem;"></i></a>
+    <a href="<?= APPLICATION ?>/improvement/" type="button" class="flexim-nav-rail__tab<?=$improvement_class ?>" aria-label="Предложения по улучшению"><span data-flexim-icon="like" data-size="24" aria-hidden="true"></span></a>
     <?php endif; ?>
-    <button type="button" class="flexim-nav-rail__tab<?=$okto_class ?>" aria-label="Octopus"><span data-flexim-icon="settings" data-size="24" aria-hidden="true"></span></button>
-    <a href="<?= APPLICATION ?>/okto/" class="left_bar_item ui_tooltip right<?=$okto_class ?>" title="Octopus"><i class="far fa-comments" style="font-size: 1.3rem;"></i></a>
+    <a href="<?= APPLICATION ?>/okto/" type="button" class="flexim-nav-rail__tab<?=$okto_class ?>" aria-label="Octopus"><i class="far fa-comments"></i></a>
     </nav>
 </aside>
