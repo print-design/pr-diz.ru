62b949e5a9c508c872aebce6c2bf3905386aae65
DMITRY <DMITRY@DMITRY>
2026-05-28 16:09:09 +0300

    flexim Страница логина

--- include/header_zakaz.php
+++ include/header_zakaz.php
@@ -26,34 +26,45 @@ if(empty($status_id) && !empty($calculation)) {
 
 if($folder == 'calculation') {
     if($status_id == ORDER_STATUS_TRASH) {
-        $trash_status = ' disabled';
+        $trash_status = ' flexim-header-menu__item--active';
     }
     elseif($status_id == ORDER_STATUS_DRAFT) {
-        $draft_status = ' disabled';
+        $draft_status = ' flexim-header-menu__item--active';
     }
     elseif(in_array ($status_id, ORDER_STATUSES_NOT_IN_WORK) || $status_id == ORDER_STATUS_NOT_IN_WORK) {
-        $not_in_work_status = ' disabled';
+        $not_in_work_status = ' flexim-header-menu__item--active';
     }
     elseif(in_array($status_id, ORDER_STATUSES_IN_PRODUCTION) || $status_id == ORDER_STATUS_IN_PRODUCTION) {
-        $production_status = ' disabled';
+        $production_status = ' flexim-header-menu__item--active';
     }
     elseif($status_id == ORDER_STATUS_SHIP_READY) {
-        $ship_ready_status = ' disabled';
+        $ship_ready_status = ' flexim-header-menu__item--active';
     }
     elseif($status_id == ORDER_STATUS_SHIPPED) {
-        $shipped_status = ' disabled';
+        $shipped_status = ' flexim-header-menu__item--active';
     }
     else {
-        $calculation_status = ' disabled';
+        $calculation_status = ' flexim-header-menu__item--active';
     }
 }
 ?>
-<div class="container-fluid header">
-    <nav class="navbar navbar-expand-sm justify-content-end">
-        <ul class="navbar-nav">
-            <?php
-            if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE_NAMES[ROLE_MANAGER_SENIOR]))):
-            ?>
+<div class="app-topbar">
+    <nav class="flexim-header-menu__nav">
+        <?php
+        if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE_NAMES[ROLE_MANAGER_SENIOR]))):
+        ?>
+        <button type="button" class="flexim-header-menu__item<?=$shipped_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_SHIPPED] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$ship_ready_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_SHIP_READY] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$production_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_IN_PRODUCTION] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$calculation_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_IN_WORK] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$not_in_work_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_NOT_IN_WORK] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$draft_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_DRAFT] ?></button>
+        <button type="button" class="flexim-header-menu__item<?=$trash_status ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_TRASH] ?></button>
+        <?php endif; ?>
+    </nav>
+        <?php
+        if(IsInRole(array(ROLE_NAMES[ROLE_TECHNOLOGIST], ROLE_NAMES[ROLE_MANAGER], ROLE_NAMES[ROLE_MANAGER_SENIOR]))):
+        ?>
             <li class="nav-item">
                 <a class="nav-link<?=$shipped_status ?> text-nowrap" href="<?=APPLICATION ?>/calculation/<?= BuildQueryAddRemoveArray("status", ORDER_STATUS_SHIPPED, array("page", "order")) ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_SHIPPED] ?></a>
             </li>
@@ -76,7 +87,6 @@ if($folder == 'calculation') {
                 <a class="nav-link<?=$trash_status ?>" href="<?=APPLICATION ?>/calculation/<?= BuildQueryAddRemoveArray("status", ORDER_STATUS_TRASH, array("page", "order")) ?>"><?=ORDER_STATUS_TITLES[ORDER_STATUS_TRASH] ?></a>
             </li>
             <?php endif; ?>
-        </ul>
         <?php
         if(file_exists('find.php')) {
             include 'find.php';
@@ -87,6 +97,6 @@ if($folder == 'calculation') {
         
         include 'header_right.php';
         ?>
-    </nav>
+    
 </div>
 <div id="topmost"></div>
