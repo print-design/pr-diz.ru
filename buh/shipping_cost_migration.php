6f2d44b142241557500164100ed19c8029d85b70
DMITRY <DMITRY@DMITRY>
2026-08-04 15:13:52 +0300

    Бухгалтерия.

---
+++ buh/shipping_cost_migration.php
@@ -0,0 +1,51 @@
+<?php
+include '../include/topscripts.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include '../include/head.php';
+        ?>
+    </head>
+    <body>
+        <?php
+        include '../include/header.php';
+        ?>
+        <div class="container-fluid">
+            <h2>Дублирование общей стоимости</h2>
+            <?php
+            $result = 0;
+            $sql = "select count(distinct c.id) "
+                    . "from calculation_take_stream cts "
+                    . "inner join calculation_take ct on cts.calculation_take_id = ct.id "
+                    . "inner join calculation c on ct.calculation_id = c.id "
+                    . "where c.duplicate_shipping_cost = 0 and cts.weight > 0 and cts.length > 0";
+            $fetcher = new Fetcher($sql);
+            if($row = $fetcher->Fetch()) {
+                $result = $row[0];
+            }
+            ?>
+            <div id="result" style="font-size: xx-large;"><?=$result ?></div>
+            <button type="button" class="btn" onclick="javascript: Migrate();">Старт</button>
+        </div>
+    </body>
+    <?php
+    include '../include/footer.php';
+    ?>
+    <script>
+        function Migrate() {
+            $.ajax({ url: 'shipping_cost_migration_ajax.php' })
+                    .done(function(data) {
+                        $('#result').text(data);
+                
+                        if(data != 0 && data != '0') {
+                            Migrate();
+                        }
+                    })
+                    .fail(function() {
+                        $('#result').text('ERROR');
+                    });
+        }
+    </script>
+</html>
---
+++ buh/shipping_cost_migration_ajax.php
@@ -0,0 +1,56 @@
+<?php
+include '../include/topscripts.php';
+include '../calculation/calculation.php';
+include '../calculation/calculation_result.php';
+
+$id = 0;
+$result = 0;
+$error_message = '';
+
+$sql = "select c.id "
+        . "from calculation_take_stream cts "
+        . "inner join calculation_take ct on cts.calculation_take_id = ct.id "
+        . "inner join calculation c on ct.calculation_id = c.id "
+        . "where c.duplicate_shipping_cost = 0 and cts.weight > 0 and cts.length > 0";
+$fetcher = new Fetcher($sql);
+
+if($row = $fetcher->Fetch()) {
+    $id = $row[0];
+}
+
+if($id > 0) {
+    $calculation = CalculationBase::Create($id);
+    $calculation_result = CalculationResult::Create($id);
+    
+    // Стоимость заказа
+    $shipping_order_cost = 0;
+    
+    if($calculation->unit == KG) {
+        $shipping_order_cost = round($calculation->weight_cut, 2) * round($calculation_result->shipping_cost_per_unit, 3);
+    }
+    else {
+        $shipping_order_cost = floor($calculation->length_cut * $calculation->number_in_meter) * round($calculation_result->shipping_cost_per_unit, 3);
+    }
+    
+    // Общая стоимость
+    $shipping_cost = $shipping_order_cost + $calculation_result->shipping_cliche_cost + $calculation_result->shipping_knife_cost;
+    
+    // Заполняем дублирующееся поле
+    $sql = "update calculation set duplicate_shipping_cost = $shipping_cost where id = $id";
+    $executer = new Executer($sql);
+    $error_message = $executer->error;
+}
+
+if(empty($error_message)) {
+    $sql = "select count(distinct c.id) "
+            . "from calculation_take_stream cts "
+            . "inner join calculation_take ct on cts.calculation_take_id = ct.id "
+            . "inner join calculation c on ct.calculation_id = c.id "
+            . "where c.duplicate_shipping_cost = 0 and cts.weight > 0 and cts.length > 0";
+    $fetcher = new Fetcher($sql);
+    if($row = $fetcher->Fetch()) {
+        $result = $row[0];
+    }
+}
+echo $result;
+?>
