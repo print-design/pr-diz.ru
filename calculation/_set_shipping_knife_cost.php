8ed7ccd925fc93d09a56e02b57896dba645d4657
DMITRY <DMITRY@DMITRY>
2026-08-25 14:19:52 +0300

    Claude AI

--- calculation/_set_shipping_knife_cost.php
+++ calculation/_set_shipping_knife_cost.php
@@ -13,15 +13,15 @@ elseif($shipping_knife_cost === null || $shipping_knife_cost === '') {
     $result['error'] = "Не указана отгрузочная стоимость ножа";
 }
 else {
-    $sql = "update calculation_result set shipping_knife_cost = $shipping_knife_cost where calculation_id = $id";
-    $executer = new Executer($sql);
+    $sql = "update calculation_result set shipping_knife_cost = ? where calculation_id = ?";
+    $executer = new Executer($sql, [$shipping_knife_cost, $id]);
     $error_message = $executer->error;
     
     $knife_cost = 0;
     
     if(empty($error_message)) {
-        $sql = "select knife_cost from calculation_result where calculation_id = $id";
-        $fetcher = new Fetcher($sql);
+        $sql = "select knife_cost from calculation_result where calculation_id = ?";
+        $fetcher = new Fetcher($sql, [$id]);
         $error_message = $fetcher->error;
         if($row = $fetcher->Fetch()) {
             $knife_cost = $row['knife_cost'];
@@ -36,8 +36,8 @@ else {
     }
     
     if(empty($error_message)) {
-        $sql = "update calculation c inner join calculation_result cr on c.id = cr.calculation_id set c.extracharge_knife = (100 * ($shipping_knife_cost - $knife_cost) / ($knife_cost + $uk_costknife)) where c.id = $id";
-        $executer = new Executer($sql);
+        $sql = "update calculation c inner join calculation_result cr on c.id = cr.calculation_id set c.extracharge_knife = (100 * (? - ?) / (? + $uk_costknife)) where c.id = ?";
+        $executer = new Executer($sql, [$shipping_knife_cost, $knife_cost, $knife_cost, $id]);
         $error_message = $executer->error;
     }
     
@@ -55,14 +55,14 @@ else {
     }
     
     if(empty($error_message)) {
-        $sql = "update calculation_result set income_knife = $income_knife where calculation_id = $id";
-        $executer = new Executer($sql);
+        $sql = "update calculation_result set income_knife = ? where calculation_id = ?";
+        $executer = new Executer($sql, [$income_knife, $id]);
         $error_message = $executer->error;
     }
     
     if(empty($error_message)) {
-        $sql = "select c.extracharge_knife, cr.shipping_knife_cost, cr.income, cr.income_per_unit, cr.income_cliche, cr.income_knife from calculation_result cr inner join calculation c on cr.calculation_id = c.id where c.id = $id order by cr.id desc limit 1";
-        $fetcher = new Fetcher($sql);
+        $sql = "select c.extracharge_knife, cr.shipping_knife_cost, cr.income, cr.income_per_unit, cr.income_cliche, cr.income_knife from calculation_result cr inner join calculation c on cr.calculation_id = c.id where c.id = ? order by cr.id desc limit 1";
+        $fetcher = new Fetcher($sql, [$id]);
         $error_message = $fetcher->error;
         
         if($row = $fetcher->Fetch()) {
