a51d9c678450205d4df08768ad92a8113b1f8544
DMITRY <DMITRY@DMITRY>
2026-09-03 09:38:43 +0300

    Claude AI prepared statement

--- include/pallet_shipping_form.php
+++ include/pallet_shipping_form.php
@@ -33,25 +33,25 @@ $pallet_fields_disabled = !empty($pallet_shared_with_id);
         </div>
         <div class="d-flex justify-content-between">
             <div class="form-group mr-1">
-                    <label for="gross_weight_<?=$id ?>">Вес брутто, кг</label>
-                    <input type="text" id="gross_weight_<?=$id ?>" name="gross_weight" class="form-control float-only float-format pallet-required-field<?=$gross_weight_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($gross_weight, 0) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
-                </div>
+                <label for="gross_weight_<?=$id ?>">Вес брутто, кг</label>
+                <input type="text" id="gross_weight_<?=$id ?>" name="gross_weight" class="form-control float-only float-format pallet-required-field<?=$gross_weight_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($gross_weight, 0) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
+            </div>
             <div class="form-group mr-1 ml-1">
-                    <label for="pallet_count_<?=$id ?>">Кол-во паллетов</label>
-                    <input type="text" id="pallet_count_<?=$id ?>" name="pallet_count" class="form-control int-only pallet-required-field<?=$pallet_count_valid ?>" value="<?= $pallet_fields_disabled ? '' : $pallet_count ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
-                </div>
+                <label for="pallet_count_<?=$id ?>">Кол-во паллетов</label>
+                <input type="text" id="pallet_count_<?=$id ?>" name="pallet_count" class="form-control int-only pallet-required-field<?=$pallet_count_valid ?>" value="<?= $pallet_fields_disabled ? '' : $pallet_count ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
+            </div>
             <div class="form-group mr-1 ml-1">
-                    <label for="pallet_length_<?=$id ?>">Длина, м</label>
-                    <input type="text" id="pallet_length_<?=$id ?>" name="pallet_length" class="form-control float-only float-format pallet-required-field<?=$pallet_length_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_length, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
-                </div>
+                <label for="pallet_length_<?=$id ?>">Длина, м</label>
+                <input type="text" id="pallet_length_<?=$id ?>" name="pallet_length" class="form-control float-only float-format pallet-required-field<?=$pallet_length_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_length, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
+            </div>
             <div class="form-group mr-1 ml-1">
-                    <label for="pallet_width_<?=$id ?>">Ширина, м</label>
-                    <input type="text" id="pallet_width_<?=$id ?>" name="pallet_width" class="form-control float-only float-format pallet-required-field<?=$pallet_width_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_width, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
-                </div>
+                <label for="pallet_width_<?=$id ?>">Ширина, м</label>
+                <input type="text" id="pallet_width_<?=$id ?>" name="pallet_width" class="form-control float-only float-format pallet-required-field<?=$pallet_width_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_width, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
+            </div>
             <div class="form-group ml-1">
-                    <label for="pallet_height_<?=$id ?>">Высота, м</label>
-                    <input type="text" id="pallet_height_<?=$id ?>" name="pallet_height" class="form-control float-only float-format pallet-required-field<?=$pallet_height_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_height, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
-                </div>
+                <label for="pallet_height_<?=$id ?>">Высота, м</label>
+                <input type="text" id="pallet_height_<?=$id ?>" name="pallet_height" class="form-control float-only float-format pallet-required-field<?=$pallet_height_valid ?>" value="<?= $pallet_fields_disabled ? '' : DisplayNumber($pallet_height, 2) ?>"<?= $pallet_fields_disabled ? '' : ' required="required"' ?> autocomplete="off" />
+            </div>
         </div>
         <div class="d-flex justify-content-end">
             <button type="submit" name="save_pallet_data_submit" class="btn btn-outline-dark mb-3"><i class="fas fa-save mr-2"></i>Сохранить</button>
