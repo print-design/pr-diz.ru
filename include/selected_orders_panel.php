f22c205ebbc6f479945bdb45f679fc7673ebbe2e
DMITRY <DMITRY@DMITRY>
2026-09-04 10:10:42 +0300

    Claude AI prepared statement

--- include/selected_orders_panel.php
+++ include/selected_orders_panel.php
@@ -76,13 +76,13 @@
         return parseFloat(value || 0).toLocaleString('ru-RU', {minimumFractionDigits: decimals, maximumFractionDigits: decimals});
     }
     
-    // Пересчёт нижней таблицы (суммы и максимум по габаритам) по тем строкам разбивки,
+    // Пересчёт нижней таблицы (суммы и габариты самого крупного паллета) по тем строкам разбивки,
     // у которых флажок остался отмеченным
     function RecalculateSelectedOrdersBreakdown() {
         var grossWeightSum = 0;
         var palletCountSum = 0;
         var maxVolume = -1;
-        var maxVolumeOrderNumber = null;
+        var maxVolumeDimensions = null;
         
         $('.selected_orders_breakdown_checkbox:checked').each(function() {
             grossWeightSum += parseFloat($(this).data('gross-weight')) || 0;
@@ -91,13 +91,16 @@
             var volume = parseFloat($(this).data('pallet-volume')) || 0;
             if(volume > maxVolume) {
                 maxVolume = volume;
-                maxVolumeOrderNumber = $(this).data('order-number');
+                var length = parseFloat($(this).data('pallet-length')) || 0;
+                var width = parseFloat($(this).data('pallet-width')) || 0;
+                var height = parseFloat($(this).data('pallet-height')) || 0;
+                maxVolumeDimensions = FormatNumberRu(length, 2) + '\u00d7' + FormatNumberRu(width, 2) + '\u00d7' + FormatNumberRu(height, 2) + ' м';
             }
         });
         
         $('#selected_orders_gross_weight_sum').text(FormatNumberRu(grossWeightSum, 0) + ' кг');
         $('#selected_orders_pallet_count_sum').text(palletCountSum);
-        $('#selected_orders_max_volume_order').text(maxVolumeOrderNumber !== null ? maxVolumeOrderNumber : '\u2014');
+        $('#selected_orders_max_volume_order').text(maxVolumeDimensions !== null ? maxVolumeDimensions : '\u2014');
     }
     
     function UpdateSelectedOrdersPanel() {
@@ -120,7 +123,7 @@
                         var grossWeightText = order.gross_weight !== null ? FormatNumberRu(order.gross_weight, 0) + ' кг' : '&mdash;';
                         var palletCountText = order.pallet_count !== null ? order.pallet_count : '&mdash;';
                         rowsHtml += '<tr>'
-                                + '<td><input type="checkbox" class="selected_orders_breakdown_checkbox" checked="checked" data-gross-weight="' + (order.gross_weight || 0) + '" data-pallet-count="' + (order.pallet_count || 0) + '" data-pallet-volume="' + (order.pallet_volume || 0) + '" data-order-number="' + order.customer_id + '-' + order.num_for_customer + '" /></td>'
+                                + '<td><input type="checkbox" class="selected_orders_breakdown_checkbox" checked="checked" data-gross-weight="' + (order.gross_weight || 0) + '" data-pallet-count="' + (order.pallet_count || 0) + '" data-pallet-volume="' + (order.pallet_volume || 0) + '" data-pallet-length="' + (order.pallet_length || 0) + '" data-pallet-width="' + (order.pallet_width || 0) + '" data-pallet-height="' + (order.pallet_height || 0) + '" /></td>'
                                 + '<td>' + order.customer_id + '-' + order.num_for_customer + '</td>'
                                 + '<td class="text-right">' + grossWeightText + '</td>'
                                 + '<td class="text-right">' + palletCountText + '</td>'
